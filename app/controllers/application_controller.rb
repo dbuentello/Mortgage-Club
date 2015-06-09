245707fdf39a5d92edc0f0cce5365b82cc972862
Hoang Le <lehoang1417@gmail.com>
2015-06-09 16:04:05 +0700

    reindent for application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,26 +3,28 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-private
-  def bootstrap(data={})
-    @bootstrap_data = {
-      currentUser: current_user.present? ? {
-        id: current_user.id,
-        firstName: current_user.first_name,
-        lastName: current_user.last_name
-      } : {}
-    }.merge!(data)
-  end
-
-  def current_user
-    User.where(id: session[:user_id]).first
-  end
-
-  def redirect_if_auth
-    if current_user
-      redirect_to new_loan_path
-    end
-  end
-
   helper_method :current_user
+
+  private
+
+    def bootstrap(data={})
+      @bootstrap_data = {
+        currentUser: current_user.present? ? {
+          id: current_user.id,
+          firstName: current_user.first_name,
+          lastName: current_user.last_name
+        } : {}
+      }.merge!(data)
+    end
+
+    def current_user
+      User.where(id: session[:user_id]).first
+    end
+
+    def redirect_if_auth
+      if current_user
+        redirect_to new_loan_path
+      end
+    end
+
 end
