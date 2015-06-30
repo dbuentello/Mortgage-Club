703346f58e86ede8435b7d9bc0f6dda9db6af9f2
Hoang Le <lehoang1417@gmail.com>
2015-06-30 13:53:53 +0700

    update send remote file by io instead of local path

--- app/services/docusign.rb
+++ app/services/docusign.rb
@@ -1,21 +1,19 @@
+require 'open-uri'
+
 class Docusign
 
   def self.create_envelope_from_document(document = nil)
     document ||= Documents::FirstW2.first
     file_url = document.attachment.s3_object.url_for(:read, :secure => true, :expires => 3.minutes).to_s
 
-    # file_url = URI.parse(file_url)
-    file_url = "/Users/hoangle/projects/homieo/public/examples/sample.png"
+    # file_url = "/Users/hoangle/projects/homieo/public/examples/sample.png"
+    file_io = open(file_url)
 
-    # file = open(file_url)
-
-    # do something
     client = DocusignRest::Client.new
-
     document_envelope_response = client.create_envelope_from_document(
       email: {
-        subject: "test email subject",
-        body: "this is the email body and it's large!"
+        subject: "Test from Hoang",
+        body: "let sign it guys"
       },
       # If embedded is set to true  in the signers array below, emails
       # don't go out to the signers and you can embed the signature page in an
@@ -36,9 +34,12 @@ class Docusign
         }
       ],
       files: [
-        {path: file_url, name: 'sample.png'}
+        # {path: file_url, name: 'sample.png'}
+        {io: file_io, name: document.attachment.instance.attachment_file_name}
       ],
       status: 'sent'
     )
+
+    # document_envelope_response["envelopeId"]
   end
 end
