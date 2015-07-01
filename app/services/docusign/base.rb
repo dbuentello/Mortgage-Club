9eb48ccf5cd8d6504140b266ba3b63a0e5760a4a
Hoang Le <lehoang1417@gmail.com>
2015-07-01 17:36:11 +0700

    implement auto detect label names to fill info

---
+++ app/services/docusign/base.rb
@@ -0,0 +1,80 @@
+require 'open-uri'
+
+module Docusign
+  class Base
+    def initialize(args = nil)
+      @client = DocusignRest::Client.new
+    end
+
+    # POST /accounts/#{acct_id}/envelopes
+    # PARAMS:
+    # => template_id / template_name
+    # => signers
+    # => email_subject, email_body
+    # => user object
+    # => values hash contain auto filled info
+    def create_envelope_from_template(options = {})
+      if options[:template_id].blank? && options[:template_name].blank?
+        puts "Error: don't have enough params"
+        return
+      end
+
+      helper = Docusign::Helper.new
+      if options[:template_id].blank?
+        options[:template_id] = helper.find_template_id_from_name(options[:template_name])
+      end
+
+      options[:email_subject] ||= "The test email subject envelope"
+      options[:email_body] ||= "Envelope body content here"
+
+      # Set values to tab labels
+      # NOTE: need to map 2 names carefully (for example "Text" will take value from :text)
+      values = {
+        "Text" => options[:values][:text]
+      }
+
+      # Map data from databse to signers
+      tabs = helper.get_tabs_from_template(template_id: options[:template_id], values: values)
+
+      signers = []
+      signer = {
+        name: options[:user][:name],
+        email: options[:user][:email],
+        role_name: 'Normal'
+      }
+      signer = signer.merge(tabs)
+      signers << signer
+      ap signers
+
+      # Get the corresponding document to send over with the achieved signers
+      if options[:document]
+        # options[:document] ||= Documents::FirstW2.first
+        file_url = options[:document].attachment.s3_object.url_for(:read, :secure => true, :expires => 3.minutes).to_s
+        file_io = open(file_url)
+      else
+        file_url = "/Users/hoangle/projects/homieo/public/examples/Loan Estimation.pdf"
+      end
+
+      envelope_response = @client.create_envelope_from_document(
+        status: 'sent',
+        email: {
+          subject: options[:email_subject],
+          body: options[:email_body]
+        },
+        template_id: options[:template_id],
+        signers: signers,
+        files: [
+          { path: file_url, name: 'Loan Estimation.pdf' }
+          # { io: file_io, name: options[:document].attachment.instance.attachment_file_name }
+        ],
+      )
+    end
+
+    def create_template_from_document(options = {})
+      # TODO: method create_template_from_document
+      # => generate draft template with name, default recipient
+      # => we have to modify tab later before using create_envelope_from_template method
+    end
+
+  end
+end
--- app/services/docusign/helper.rb
+++ app/services/docusign/helper.rb
@@ -18,13 +18,54 @@ module Docusign
       recipients = @client.get_envelope_recipients(envelope_id: envelope_id, include_tabs: true)
     end
 
-    # GET list of tabs from template name
-    def get_tabs_from_template_name(template_name)
-      template_id = find_template_id_from_name(template_name)
+    # GET list of tabs from template name to apply
+    # PARAMS: template_id / template_name
+    def get_tabs_from_template(options = {})
+      if options[:template_id].blank?
+        options[:template_id] = find_template_id_from_name(options[:template_name])
+      end
 
-      tabs = get_envelope_recipients_and_tabs(template_id)
+      # request to get the template recipient info
+      tabs = get_envelope_recipients_and_tabs(options[:template_id])
+      tabs = tabs["signers"][0]["tabs"]
 
-      ap tabs["signers"][0]["tabs"]
+      # convert javascript tabs to ruby tabs
+      ruby_tabs = {}
+      tabs.each do |key, array_of_hash|
+        ruby_array_of_hash = []
+        array_of_hash.each do |hash|
+          tab = {
+            name: hash["name"],
+            page_number: hash["pageNumber"],
+            x_position: hash["xPosition"],
+            y_position: hash["yPosition"]
+          }
+
+          tab[:optional]   = hash["optional"] if hash["optional"]
+          tab[:tabLabel]   = hash["label"] if hash["label"]
+          tab[:width]      = hash["width"] if hash["width"]
+          tab[:height]     = hash["height"] if hash["height"]
+          tab[:fontSize]   = hash["fontSize"] if hash["fontSize"]
+          tab[:fontColor]  = hash["fontColor"] if hash["fontColor"]
+          tab[:bold]       = hash["bold"] if hash["bold"]
+          tab[:italic]     = hash["italic"] if hash["italic"]
+          tab[:underline]  = hash["underline"] if hash["underline"]
+          tab[:locked]     = hash["locked"] if hash["locked"]
+          tab[:required]   = hash["required"] if hash["required"]
+
+          # map value to tab
+          if options[:values][tab[:name]]
+            tab[:value]    = options[:values][tab[:name]]
+          end
+
+          ruby_array_of_hash << tab
+        end
+
+        ruby_key = key.underscore.to_sym
+        ruby_tabs[ruby_key] = ruby_array_of_hash
+      end
+
+      ruby_tabs
     end
 
     # GET list of envelope objects
