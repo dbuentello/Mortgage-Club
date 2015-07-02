d03dd0cddd966fcae9fa4ae44436f9e140e93eb7
Hoang Le <lehoang1417@gmail.com>
2015-07-02 14:49:41 +0700

    apply front-end for iframe of embedded docusign

--- webpack/assets/javascripts/client/loans/edit/FormESigning.jsx
+++ webpack/assets/javascripts/client/loans/edit/FormESigning.jsx
@@ -1,26 +1,43 @@
 var _ = require('lodash');
 var React = require('react/addons');
-var AddressField = require('components/form/AddressField');
-var SelectField = require('components/form/SelectField');
-var TextField = require('components/form/TextField');
-
-var Router = require('react-router');
-var { Route, RouteHandler, Link } = Router;
 
 var FormESigning = React.createClass({
+  onClick: function(e) {
+    e.preventDefault();
+
+    $.ajax({
+      url: "/electronic_signature/demo",
+      method: 'POST',
+      data: {},
+      dataType: 'json',
+      success: function(response) {
+        console.log(response);
+
+        $(this.refs.iframe.getDOMNode()).attr("src", response.message.url);
+        $(this.refs.iframe.getDOMNode()).css("display", "block");
+      }.bind(this),
+      error: function(response, status, error) {
+        alert(error);
+      }
+    });
+  },
+
   render: function() {
     return (
       <div>
         <div className='formContent'>
           <div className='pal'>
-            <div className='box mtn'>
+            <div className='text-left'>
+              <a className='btn btnSml btnPrimary' onClick={this.onClick}>Sign</a>
+            </div>
 
+            <div className='mtl text-left'>
+              <iframe ref='iframe' height='600px' width='100%' style={{display: 'none'}}></iframe>
             </div>
-            <div className='box text-left'>
-              <a href="/electronic_signature/demo" className='btn btnSml btnPrimary' data-method='post'>Sign</a>
-            </div>
+
           </div>
         </div>
+
         <div className='helpSection sticky pull-right overlayRight overlayTop'>
         </div>
       </div>
