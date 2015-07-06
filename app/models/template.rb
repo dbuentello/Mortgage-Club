43f6ac87e8a5c25aa39a17350580f33df770ee55
Hoang Le <lehoang1417@gmail.com>
2015-07-06 11:36:49 +0700

    fix minor typo

--- app/models/template.rb
+++ app/models/template.rb
@@ -1,6 +1,6 @@
 class Template < ActiveRecord::Base
   belongs_to :creator, inverse_of: :templates, class_name: "User", foreign_key: 'creator_id'
 
-  has_namy :envelopes, inverse_of: :template
+  has_many :envelopes, inverse_of: :template
 
 end
