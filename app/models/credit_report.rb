172e69a104ed00ba365bac3c3fdccef6ee1dce53
Hoang Le <lehoang1417@gmail.com>
2015-07-06 13:44:01 +0700

    revert borrower_id to none-document models

--- app/models/credit_report.rb
+++ app/models/credit_report.rb
@@ -1,5 +1,5 @@
 class CreditReport < ActiveRecord::Base
-  belongs_to :borrower, inverse_of: :credit_report, foreign_key: 'owner_id'
+  belongs_to :borrower, inverse_of: :credit_report, foreign_key: 'borrower_id'
   has_many  :liabilities, inverse_of: :credit_report, dependent: :destroy
   accepts_nested_attributes_for :liabilities, allow_destroy: true
 
