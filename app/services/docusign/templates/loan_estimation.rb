f69130ef22fa443fce040e4e638c9cb048ae3b94
Hoang Le <lehoang1417@gmail.com>
2015-07-14 16:01:40 +0700

    add default values for testing layout and apply money format

--- app/services/docusign/templates/loan_estimation.rb
+++ app/services/docusign/templates/loan_estimation.rb
@@ -1,4 +1,6 @@
 require 'open-uri'
+require 'money'
+
 include NumbersHelper
 
 module Docusign
@@ -8,31 +10,35 @@ module Docusign
         property = loan.property
         borrower = user.borrower
 
-        estimated_escrow = number_with_precision(property.estimated_hazard_insurance.to_f + property.estimated_property_tax.to_f)
+        estimated_escrow = property.estimated_hazard_insurance.to_f + property.estimated_property_tax.to_f
+        estimated_total_monthly_payment_1 = loan.pmi.to_f + estimated_escrow
 
         values = {
-          "applicant_name" => "#{borrower.first_name} #{borrower.last_name}",
-          "sale_price" => property.purchase_price,
-          "purpose" => loan.purpose,
-          "product" => loan.amortization_type,
+          "applicant_name" => "#{borrower.first_name} #{borrower.last_name}".titleize,
+          "sale_price" => Money.new(property.purchase_price).format,
+          "purpose" => loan.purpose.titleize,
+          "product" => loan.amortization_type.titleize,
 
           # Loan Terms
-          "loan_amount" => loan.amount,
+          "loan_amount" => Money.new(loan.amount).format,
           "interest_rate" => loan.interest_rate,
-          "monthly_principal_interest" => loan.monthly_payment,
-          "prepayment_penalty" => loan.prepayment_penalty,
-          "prepayment_penalty_amount" => loan.prepayment_penalty_amount,
+          "monthly_principal_interest" => Money.new(loan.monthly_payment).format,
+          "prepayment_penalty" => Money.new(loan.prepayment_penalty).format,
+          "prepayment_penalty_amount" => Money.new(loan.prepayment_penalty_amount).format,
+          "prepayment_penalty_amount" => 'As high',
           "balloon_payment" => loan.balloon_payment,
 
           # Projected Payments
-          "projected_principal_interest_1" => loan.monthly_payment,
+          "projected_principal_interest_1" => Money.new(loan.monthly_payment).format,
           "projected_mortgage_insurance_1" => loan.pmi,
-          "estimated_total_monthly_payment_1" => number_with_precision(loan.pmi.to_f + property.estimated_hazard_insurance.to_f + property.estimated_property_tax.to_f),
           "estimated_escrow_1" => estimated_escrow,
-          "estimated_taxes_insurance_assessments" => estimated_escrow,
+          "estimated_total_monthly_payment_1" => Money.new(estimated_total_monthly_payment_1).format,
+
+          "estimated_taxes_insurance_assessments" => Money.new(estimated_escrow).format,
+          "estimated_taxes_insurance_assessments_tooltip" => 'a month',
 
           # Costs at Closing
-          "estimated_closing_costs" => loan.estimated_closing_costs
+          "estimated_closing_costs" => Money.new(loan.estimated_closing_costs).format
         }
 
         if borrower.borrower_addresses.first.present? && borrower.borrower_addresses.first.address.present?
@@ -85,6 +91,15 @@ module Docusign
             'rate_lock_yes' => 'x'
           })
         end
+
+        # default values for testing
+        values.merge! ({
+          'date_issued' => Date.today.to_s,
+          'include_property_taxes_yes_no' => 'x',
+          'include_homeowners_insurance_yes_no' => 'x',
+          'include_other_yes_no' => 'x',
+          'include_other_text' => 'hardcode test'
+        })
 
         values
       end
