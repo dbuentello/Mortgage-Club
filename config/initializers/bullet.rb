a73779e7da9fb4308816223908bc52c0f1ff16c6
Hoang Le <lehoang1417@gmail.com>
2015-07-15 11:20:29 +0700

    minor tweat

--- config/initializers/bullet.rb
+++ config/initializers/bullet.rb
@@ -2,7 +2,7 @@ if Rails.env == 'development'
   Bullet.enable = true
   # Bullet.alert = true
   # Bullet.bullet_logger = true
-  Bullet.console = true
+  # Bullet.console = true
   Bullet.rails_logger = true
   # Bullet.add_footer = true
 end
