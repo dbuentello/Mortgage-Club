2dd73ddfcec7059455ca297789b68b0c43cf1b72
Hoang Le <lehoang1417@gmail.com>
2015-07-16 12:09:54 +0700

    apply redis connection configuration for all environments

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,1 +1,7 @@
-@redis = Redis.new(:host => "127.0.0.1", :port => 6379)
+if Rails.env.development? || Rails.env.test?
+  $redis = Redis.new(:host => ENV['REDIS_HOST'], :port => ENV['REDIS_PORT'])
+elsif Rails.env.produciton?
+  uri = URI.parse(ENV['REDISTOGO_URL'])
+
+  $redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+end
