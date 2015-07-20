5bd907b0697481cc68d3aa5413db85e97e04a9e6
Hoang Le <lehoang1417@gmail.com>
2015-07-20 17:12:31 +0700

    add basic view for notiying email to secondary borrower

---
+++ app/mailers/secondary_borrower_mailer.rb
@@ -0,0 +1,15 @@
+class SecondaryBorrowerMailer < ActionMailer::Base
+  default :from => "MortgageClub #{ENV['EMAIL_SENDER']}"
+
+  def notify_being_added(loan_id)
+    @loan = Loan.find loan_id
+
+    @borrower = @loan.borrower
+    @secondary_borrower = @loan.secondary_borrower
+
+    mail(
+      to: @secondary_borrower.user.email,
+      subject: "You have been added as secondary borrower from #{@loan.user.to_s}"
+    )
+  end
+end
