8dc1629c250ca5e17caaecb399cab8bc7379f5e5
Hoang Le <lehoang1417@gmail.com>
2015-07-27 17:41:02 +0700

    set rspec config for pundit gem

--- spec/services/rate_spec.rb
+++ spec/services/rate_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-describe Rate, type: :model do
+describe Rate do
   context "convert_text_to_boolean" do
     it "should convert correctly" do
       expect(Rate.convert_text_to_boolean('False')).to eq(false)
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -18,6 +18,7 @@
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 require_relative 'support/controller_helpers'
 require 'devise'
+require 'pundit/rspec'
 require 'capybara/rspec'
 require 'database_cleaner'
 
