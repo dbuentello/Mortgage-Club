59789cb607c51a366be695dfe6bc4964da082aa6
Hoang Le <lehoang1417@gmail.com>
2015-07-27 15:58:04 +0700

    remove unnecessary bootstrap calls

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,14 +5,12 @@ class PagesController < ApplicationController
   before_filter :redirect_if_auth, only: [:index]
 
   def index
-    bootstrap
     respond_to do |format|
       format.html { render template: 'public_app' }
     end
   end
 
   def take_home_test
-    bootstrap
     respond_to do |format|
       format.html { render template: 'public_app' }
     end
