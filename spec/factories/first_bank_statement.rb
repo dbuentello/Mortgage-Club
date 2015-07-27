13383ea2f46b53c43da6c13a4f79f364b0ee8254
Hoang Le <lehoang1417@gmail.com>
2015-07-27 14:14:21 +0700

    fix wrong class name bug and increase rspec performance #19

--- spec/factories/first_bank_statement.rb
+++ spec/factories/first_bank_statement.rb
@@ -1,6 +1,6 @@
 FactoryGirl.define do
   factory :first_bank_statement, :class => Documents::FirstBankStatement do |f|
-    f.type { 'FirstBankStatement' }
+    f.type { 'Documents::FirstBankStatement' }
     f.attachment File.new(Rails.root.join 'spec', 'files', 'sample.png')
   end
 end
--- spec/factories/second_w2.rb
+++ spec/factories/second_w2.rb
@@ -1,6 +1,6 @@
 FactoryGirl.define do
   factory :second_w2, :class => Documents::SecondW2 do |f|
-    f.type { 'SecondW2' }
+    f.type { 'Documents::SecondW2' }
     f.attachment File.new(Rails.root.join 'spec', 'files', 'sample.png')
   end
 end
