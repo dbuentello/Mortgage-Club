848d2c8b7d655fcfe9652004c7f57e98c79c21da
Hoang Le <lehoang1417@gmail.com>
2015-07-28 17:25:08 +0700

    update minor specs

--- spec/models/role_spec.rb
+++ spec/models/role_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
-RSpec.describe Role, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Role, type: :model do
+  it 'has a valid factory' do
+    expect(FactoryGirl.create(:role)).to be_valid
+  end
 end
