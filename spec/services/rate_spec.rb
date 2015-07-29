4815cdc6d7da25d673c34d055ea90cd697dc0129
Hoang Le <lehoang1417@gmail.com>
2015-07-29 16:34:12 +0700

    add 'signed in user of loan' context

--- spec/services/rate_spec.rb
+++ spec/services/rate_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 describe Rate do
-  context "get_rates" do
+  context "get_rates method" do
     it "converts correctly" do
       rates = Rate.get_rates
       expect(rates).to be_a(Array)
@@ -9,13 +9,13 @@ describe Rate do
   end
 
   context "get_fee" do
-    it "returns an array of hash" do
+    it "returns an array of hash method" do
       fee = Rate.get_fee
       expect(fee).to be_a(Array)
     end
   end
 
-  context "convert_text_to_boolean" do
+  context "convert_text_to_boolean method" do
     it "converts correctly" do
       expect(Rate.convert_text_to_boolean('False')).to eq(false)
       expect(Rate.convert_text_to_boolean('True')).to eq(true)
--- spec/support/shared_contexts.rb
+++ spec/support/shared_contexts.rb
@@ -5,3 +5,8 @@ RSpec.shared_context 'signed in user' do
   let(:user) { FactoryGirl.create :user }
   before { login_with user }
 end
+
+RSpec.shared_context 'signed in user of loan' do
+  let(:loan) { FactoryGirl.create :loan }
+  before { login_with loan.user }
+end
