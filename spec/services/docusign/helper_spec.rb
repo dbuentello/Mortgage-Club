a8879a2a6f1ac45772c1e97cb216134c03708df1
Hoang Le <lehoang1417@gmail.com>
2015-07-29 14:46:46 +0700

    fix wrong param names

--- spec/services/docusign/helper_spec.rb
+++ spec/services/docusign/helper_spec.rb
@@ -4,17 +4,17 @@ describe Docusign::Helper do
 
   before :all do
     @base = Docusign::Base.new
-    @base.create_template_object_from_name('Loan Estimation')
+    @template = @base.create_template_object_from_name('Loan Estimation')
   end
 
-  let(:template) { Template.where(name: 'Loan Estimation').first }
+  context "when run make_sure_template_name_and_id_exist" do
+    it "should return right data" do
+      template_docusign_id = @base.helper.make_sure_template_name_and_id_exist(template_name: @template.name)[:template_id]
+      template_name = @base.helper.make_sure_template_name_and_id_exist(template_id: @template.docusign_id)[:template_name]
 
-  it "make_sure_template_name_and_id_exist should return right data" do
-    template_id = @base.helper.make_sure_template_name_and_id_exist(template_name: template.name)[:template_id]
-    template_name = @base.helper.make_sure_template_name_and_id_exist(docusign_id: template.docusign_id)[:template_name]
-
-    expect(template_id).to eq(template.docusign_id)
-    expect(template_name).to eq(template.name)
+      expect(template_docusign_id).to eq(@template.docusign_id)
+      expect(template_name).to eq(@template.name)
+    end
   end
 
   after :all do
