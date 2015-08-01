7ceb8d22c8ed229cf909a655df08f13d80570a74
Hoang Le <lehoang1417@gmail.com>
2015-08-01 10:40:51 +0700

    fix wrong number format and apply text template

--- app/services/nexmo_service.rb
+++ app/services/nexmo_service.rb
@@ -4,17 +4,21 @@ class NexmoService
   def self.send_sms
     @client = Nexmo::Client.new(key: ENV['NEXMO_API_KEY'] , secret: ENV['NEXMO_API_SECRET'])
 
-    from = '+19853020418'
+    from = '19853020418'
 
     # to = '+84906944722'
-    to = '+16507877799'
+    to = '16507877799'
 
-    puts "Send from #{from} to #{to}"
+    borrower = Borrower.first
+    loan = borrower.user.loans.first
+    text = "Dear #{borrower.user.to_s}, Your loan amount is #{loan.amount}. Interest rate is #{loan.interest_rate}."
+
+    puts "Send from #{from} to #{to} with text: #{text}"
 
     @client.send_message(
       from: from,
       to: to,
-      text: "Hey there! I'm Hoang from Nexmo."
+      text: text
     )
   end
 end
