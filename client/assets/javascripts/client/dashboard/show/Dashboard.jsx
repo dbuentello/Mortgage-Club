b43e095e7d98b7baea5b527531a710ced8e2c028
Hoang Le <lehoang1417@gmail.com>
2015-08-03 10:58:20 +0700

    fix undefined bug and update code style

--- client/assets/javascripts/client/dashboard/show/Dashboard.jsx
+++ client/assets/javascripts/client/dashboard/show/Dashboard.jsx
@@ -2,11 +2,13 @@ var _ = require('lodash');
 var React = require('react/addons');
 var BorrowerTab = require('./BorrowerTab');
 var UserInfo = require('./UserInfo');
+
 var Dashboard = React.createClass({
   render: function() {
-    var current_user = this.props.bootstrapData.currentUser
-    var docList = this.props.bootstrapData.doc_list
-    var address = this.props.bootstrapData.address
+    var current_user = this.props.bootstrapData.currentUser;
+    var docList = this.props.bootstrapData.doc_list;
+    var address = this.props.bootstrapData.address;
+
     return (
       <div className='dashboard'>
         <div className='dashboard-header'>
@@ -25,7 +27,9 @@ var Dashboard = React.createClass({
         </div>
         <div className='col-xs-12 plxl prxl backgroundLowlight'>
           <ul className="nav nav-tabs" >
-            <li role="presentation" className="active"><a href="#borrower" aria-controls="borrower" role="tab" data-toggle="tab">Borrower</a></li>
+            <li role="presentation" className="active">
+              <a href="#borrower" aria-controls="borrower" role="tab" data-toggle="tab">Borrower</a>
+            </li>
           </ul>
           <div className='col-xs-7'>
             <div className="tab-content">
