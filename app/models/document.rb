28fb9115e8ffb12e7654fccf4dbb7cf7f5b0184f
Hoang Le <lehoang1417@gmail.com>
2015-08-04 09:55:04 +0700

    move ALLOWED_MIME_TYPES to static_variables config

--- app/models/document.rb
+++ app/models/document.rb
@@ -14,44 +14,6 @@
 
 class Document < ActiveRecord::Base
 
-  # see this for reference: https://en.wikipedia.org/wiki/Internet_media_type#List_of_common_media_types
-  ALLOWED_MIME_TYPES = [
-    'image/jpg',
-    'image/jpeg',
-    'image/pjpeg',
-    'image/gif',
-    'image/png',
-    'image/x-png',
-    'image/tiff',
-    'image/x-tiff',
-    'image/vnd.adobe.photoshop',
-    'application/pdf',
-    'application/octet-stream',
-    'application/x-photoshop',
-    'application/msword',
-    'application/vnd.ms-office',
-    'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
-    'application/excel',
-    'application/vnd.ms-excel',
-    'application/x-excel',
-    'application/x-msexcel',
-    'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
-    'application/mspowerpoint',
-    'application/powerpoint',
-    'application/vnd.ms-powerpoint',
-    'application/x-mspowerpoint',
-    'application/vnd.openxmlformats-officedocument.presentationml.presentation',
-    'application/rtf',
-    'application/x-rtf',
-    'application/zip', # file --mime-type spec/files/sample.xlsx => spec/files/sample.xlsx: application/zip
-    'text/richtext',
-    'text/rtf',
-    'text/plain',
-    'inode/x-empty'
-  ]
-
-  belongs_to :borrower, foreign_key: 'owner_id'
-
   has_attached_file :attachment,
     s3_permissions: 'authenticated-read',
     path: ":class/:token/:filename"
@@ -61,7 +23,7 @@ class Document < ActiveRecord::Base
   validates_attachment :attachment,
     presence: true,
     content_type: {
-      content_type: Document::ALLOWED_MIME_TYPES,
+      content_type: ALLOWED_MIME_TYPES,
       message: ' allows MS Excel, MS Documents, MS Powerpoint, Rich Text, Text File and Images'
     },
     size: {
@@ -86,8 +48,8 @@ class Document < ActiveRecord::Base
 
   private
 
-    def set_private_token
-      self.token = Digest::MD5.hexdigest(Time.now.to_s)
-    end
+  def set_private_token
+    self.token = Digest::MD5.hexdigest(Time.now.to_s)
+  end
 
 end
