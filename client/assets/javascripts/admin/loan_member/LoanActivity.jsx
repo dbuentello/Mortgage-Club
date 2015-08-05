e1ad3d6b1f7268064948fe3cd1e75a5422c1bffb
Hoang Le <lehoang1417@gmail.com>
2015-08-05 11:36:37 +0700

    create sample LoanActivity React.js page and config Rails to include admin.js

---
+++ client/assets/javascripts/admin/loan_member/LoanActivity.jsx
@@ -0,0 +1,29 @@
+var _ = require('lodash');
+var React = require('react/addons');
+
+var LoanActivity = React.createClass({
+  mixins: [],
+
+  getInitialState: function() {
+    return {};
+  },
+
+  getDefaultProps: function() {
+    return {};
+  },
+
+  componentDidMount: function() {
+    console.dir(this.props.bootstrapData);
+  },
+
+  render: function() {
+    return (
+      <div className='content text-center'>
+        <h1>Hello guys</h1>
+      </div>
+    )
+  }
+
+});
+
+module.exports = LoanActivity;
