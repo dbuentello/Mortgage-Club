4d1d0967d79ee0f4ce1109147f4c1ba30518af30
Cuong Vu <cuongvu0103@gmail.com>
2015-08-05 15:58:21 +0700

    Remove param token in RegistrationController

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -50,7 +50,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # If you have extra params to permit, append them to the sanitizer.
   def configure_account_update_params
     devise_parameter_sanitizer.for(:account_update) { |u|
-      u.permit({ :borrower_attributes => [:first_name, :last_name]}, :email, :password, :password_confirmation, :avatar, :token)
+      u.permit({ :borrower_attributes => [:first_name, :last_name]}, :email, :password, :password_confirmation, :avatar)
     }
   end
 
