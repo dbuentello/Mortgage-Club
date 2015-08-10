04bfde73d6cc38efd6f8c9a5d7b36eda1d6962c5
Cuong Vu <cuongvu0103@gmail.com>
2015-08-10 13:56:58 +0700

    Configure Circle CI + Update specs

--- features/dashboard.feature
+++ features/dashboard.feature
@@ -9,11 +9,10 @@ Feature: Dashboard
   @javascript
   Scenario: display borrower's address and loan's title
     Given there is a user with the email "testing@man.net" and the password "secretpass" and the password confirmation "secretpass"
-      And there is a borrower with the first name "Cuong" and the last name "Vu" with the user above
-      And there is a loan with the amortization type "USDA" with the user above
-      And there is a borrower address with the is current "true" with the borrower above
-      And there is a address with the street address "81458 Borer Falls" and the street address2 "Apt. 305" and the city "West Emiltown" and the state "Virginia" and the zip "9999" with the borrower address above
+      And there is a property with the purchase price "1000000" and with the usage "0"
+      And there is a loan with the amount "500000" and with the num of months "24" and with the purpose "0" and with the user above and with the property above
+      And there is a address with the street address "81458 Borer Falls" and the street address2 "Apt. 305" and the city "West Emiltown" and the state "Virginia" and the zip "9999" with the property above
       And I login as "testing@man.net" with password "secretpass"
-      And show me the page
     Then I follow "Dashboard"
       And I should see content as "81458 Borer Falls, Apt. 305, West Emiltown, Virginia, 9999"
+      And I should see content as "$500,000k 2-year fixed 50% LTV Primary Residence Purchase Loan"
--- spec/models/loan_activity_spec.rb
+++ spec/models/loan_activity_spec.rb
@@ -1,7 +1,65 @@
-require 'rails_helper'
+require "rails_helper"
 
 describe LoanActivity do
-  it 'has a valid factory' do
+  let(:loan) { FactoryGirl.create(:loan) }
+  let(:loan_member) { FactoryGirl.create(:loan_member) }
+  let(:loan_activity) { FactoryGirl.create(:loan_activity) }
+
+  it "has a valid factory" do
     expect(FactoryGirl.build(:loan_activity)).to be_valid
+  end
+
+  context "invalid params" do
+    it "is invalid without a loan" do
+      loan_activity = LoanActivity.new(loan_member: loan_member)
+      loan_activity.valid?
+      expect(loan_activity.errors[:loan]).to include("can't be blank")
+    end
+
+    it "is invalid without a loan member" do
+      loan_activity = LoanActivity.new(loan: loan)
+      loan_activity.valid?
+      expect(loan_activity.errors[:loan_member]).to include("can't be blank")
+    end
+
+    it "is invalid with a duplicate name within a loan" do
+      duplicate_activity = LoanActivity.new(
+        loan: loan_activity.loan,
+        loan_member: loan_activity.loan_member,
+        user_visible: true,
+        name: loan_activity.name
+      )
+      duplicate_activity.valid?
+      expect(duplicate_activity.errors[:name]).to include("has already been taken")
+    end
+
+    it "raises an error with an invalid user_visible" do
+      loan_activity = LoanActivity.new(
+        loan: loan,
+        loan_member: loan_member,
+        user_visible: 'wrong-data'
+      )
+      expect { raise loan_activity.valid? }.to raise_error(TypeError)
+    end
+
+    it "raises an error with an invalid activity_type" do
+      expect {
+        raise LoanActivity.new(
+          loan: loan,
+          loan_member: loan_member,
+          activity_type: 10
+        ).valid?
+      }.to raise_error(ArgumentError)
+    end
+
+    it "raises an error with an invalid activity_status" do
+      expect {
+        raise LoanActivity.new(
+          loan: loan,
+          loan_member: loan_member,
+          activity_status: 10
+        ).valid?
+      }.to raise_error(ArgumentError)
+    end
   end
 end
