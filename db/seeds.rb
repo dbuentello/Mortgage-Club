4adb387865e9ea7fbf5b84fa10d212d2612dafbc
Hoang Le <lehoang1417@gmail.com>
2015-08-10 09:01:24 +0700

    add more loan and property documents into sample user

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,8 +15,18 @@ if User.where(email: 'lehoang1417@gmail.com').blank?
 
   # create loan base on user
   loan = FactoryGirl.create(:loan_with_property, user: user)
-  hud_estimate = loan.build_hud_estimate(attachment: File.new(Rails.root.join 'spec', 'files', 'sample.docx'))
-  hud_estimate.save
+
+  hud_estimate = loan.create_hud_estimate(attachment: File.new(Rails.root.join 'spec', 'files', 'sample.docx'))
+  hud_final = loan.create_hud_final(attachment: File.new(Rails.root.join 'spec', 'files', 'sample.docx'))
+  loan_estimate = loan.create_loan_estimate(attachment: File.new(Rails.root.join 'spec', 'files', 'sample.docx'))
+  loan_estimate = loan.create_uniform_residential_lending_application(attachment: File.new(Rails.root.join 'spec', 'files', 'sample.docx'))
+
+  property = loan.property
+  appraisal_report = property.create_appraisal_report(attachment: File.new(Rails.root.join 'spec', 'files', 'sample.docx'))
+  mortgage_statement = property.create_mortgage_statement(attachment: File.new(Rails.root.join 'spec', 'files', 'sample.docx'))
+  purchase_agreement = property.create_purchase_agreement(attachment: File.new(Rails.root.join 'spec', 'files', 'sample.docx'))
+  risk_report = property.create_risk_report(attachment: File.new(Rails.root.join 'spec', 'files', 'sample.docx'))
+
 end
 
 # create staff user
