786e2080b22aec11d2337ea35e031ea4047eb689
Hoang Le <lehoang1417@gmail.com>
2015-08-13 11:15:41 +0700

    add one missing uuid key

--- db/migrate/20150727110045_rolify_create_roles.rb
+++ db/migrate/20150727110045_rolify_create_roles.rb
@@ -2,7 +2,7 @@ class RolifyCreateRoles < ActiveRecord::Migration
   def change
     create_table :roles, id: :uuid do |t|
       t.string :name
-      t.references :resource, :polymorphic => true
+      t.references :resource, :polymorphic => true, type: :uuid
 
       t.timestamps null: false
     end
