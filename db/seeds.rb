b3670a28e14df92327b84112d6e8cdfc5e8fae89
Hoang Le <lehoang1417@gmail.com>
2015-08-13 09:02:15 +0700

    update seed data by using normal model classes, not FactoryGirl classes

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,19 +8,25 @@
 
 # create user with its borrower
 if User.where(email: 'borrower@gmail.com').blank?
-  user = FactoryGirl.build(:borrower_user, email: 'borrower@gmail.com', password: '12345678',
-    password_confirmation: '12345678')
+  user = User.new(email: 'borrower@gmail.com', password: '12345678', password_confirmation: '12345678')
   user.skip_confirmation!
   user.save
 
-  loan = FactoryGirl.create(:loan_with_property, user: user)
+  user.create_borrower
+  user.add_role :borrower
+
+  loan = user.build_loan
+  loan.save
 
   hud_estimate = loan.create_hud_estimate(attachment: File.new(Rails.root.join 'spec', 'files', 'sample.docx'))
   hud_final = loan.create_hud_final(attachment: File.new(Rails.root.join 'spec', 'files', 'sample.docx'))
   loan_estimate = loan.create_loan_estimate(attachment: File.new(Rails.root.join 'spec', 'files', 'sample.docx'))
   loan_estimate = loan.create_uniform_residential_lending_application(attachment: File.new(Rails.root.join 'spec', 'files', 'sample.docx'))
 
-  property = loan.property
+  property = loan.build_property
+  property.create_address
+  property.save
+
   appraisal_report = property.create_appraisal_report(attachment: File.new(Rails.root.join 'spec', 'files', 'sample.docx'))
   mortgage_statement = property.create_mortgage_statement(attachment: File.new(Rails.root.join 'spec', 'files', 'sample.docx'))
   purchase_agreement = property.create_purchase_agreement(attachment: File.new(Rails.root.join 'spec', 'files', 'sample.docx'))
@@ -30,10 +36,12 @@ end
 
 # create staff user
 if User.where(email: 'loan_member@gmail.com').blank?
-  user = FactoryGirl.build(:loan_member_user, email: 'loan_member@gmail.com', password: '12345678',
-    password_confirmation: '12345678')
+  user = User.new(email: 'loan_member@gmail.com', password: '12345678', password_confirmation: '12345678')
   user.skip_confirmation!
   user.save
+
+  user.create_loan_member
+  user.add_role :loan_member
 end
 
 # if Template.where(name: 'Loan Estimation').blank?
