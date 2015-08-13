f8c51b22fa03f6cbbe3faa959b424e11937a9ba5
Cuong Vu <cuongvu0103@gmail.com>
2015-08-13 11:04:16 +0700

    uncomment method take_home_test

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,9 +5,9 @@ class PagesController < ApplicationController
   def index
   end
 
-  # def take_home_test
-  #   respond_to do |format|
-  #     format.html { render template: 'public_app' }
-  #   end
-  # end
+  def take_home_test
+    respond_to do |format|
+      format.html { render template: 'public_app' }
+    end
+  end
 end
