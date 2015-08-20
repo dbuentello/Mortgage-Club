774a88d739d0476ec814bb037f1dba76e02afb30
Cuong Vu <cuongvu0103@gmail.com>
2015-08-20 15:09:17 +0700

    Implement tests for Zillow Service
    Why:
    
    * Tests for Zillow API: get property's info, get zipped
    *  A test for crawler getting property tax from Zillow
    
    This change addresses the need by:
    
    * Make sure Zillow API & crawler work well

---
+++ spec/services/zillow_service/get_zpid_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+describe ZillowService::GetZpid do
+
+  it "gets property's info from Zillow successfully" do
+    VCR.use_cassette("get property's info from Zillow") do
+      zpid = ZillowService::GetZpid.call('5045 Cedar Springs Rd', '75235')
+      expect(zpid).to eq('26697093')
+    end
+  end
+end
