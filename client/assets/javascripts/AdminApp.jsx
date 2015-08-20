a7355e67f870be2edf040e83163bb7e5798834eb
Hoang Le <lehoang1417@gmail.com>
2015-08-20 11:26:38 +0700

    change root path of Admin pages

--- client/assets/javascripts/AdminApp.jsx
+++ client/assets/javascripts/AdminApp.jsx
@@ -26,7 +26,7 @@ window.AdminApp = React.createClass({
           <div className='plm prl'>
             <div className='row'>
               <div className='col-xs-6 typeLowlight'>
-                <a className='mrl' href='/loan_activities'> MortgageClub </a>
+                <a className='mrl' href='/loan_assignments'> MortgageClub </a>
               </div>
               <div className='col-xs-6 text-right'>
                 {user
