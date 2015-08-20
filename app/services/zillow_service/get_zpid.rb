af582ddb861176eac6bfdff881dc1e428fe25adf
Cuong Vu <cuongvu0103@gmail.com>
2015-08-20 17:25:46 +0700

    Hotfix: if Zillow returns many results, we'll get the first one

--- app/services/zillow_service/get_zpid.rb
+++ app/services/zillow_service/get_zpid.rb
@@ -11,8 +11,10 @@ module ZillowService
       }
       response = get('http://www.zillow.com/webservice/GetDeepSearchResults.htm', query: params)
 
-      if (response['searchresults'] && response['searchresults']['response'])
-        return response['searchresults']['response']['results']['result'][0]['zpid']
+      if response['searchresults'] && response['searchresults']['response']
+        property_info = response['searchresults']['response']['results']['result']
+        return property_info[0]['zpid'] if property_info[0]
+        property_info['zpid']
       end
     end
   end
