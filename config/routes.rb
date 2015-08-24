2b58b6563e37501007d2a82909f1ef67a1cc9569
Cuong Vu <cuongvu0103@gmail.com>
2015-08-24 15:09:53 +0700

    Update Route
    Why:
    * Now at uploader controllers, Remove function is a member route
    * It's meaningful and easy to find & destroy document
    
    This change addresses the need by:
    * Uploader Controllers

--- config/routes.rb
+++ config/routes.rb
@@ -38,58 +38,44 @@ Rails.application.routes.draw do
   resources :borrower_document_uploader, only: [] do
     collection do
       post 'upload'
-      delete 'remove'
     end
 
     member do
       get 'download'
+      delete 'remove'
     end
-    # member do
-    #   post 'bank_statement'
-    #   post 'paystub'
-    #   post 'w2'
-    #   post 'other_borrower_report'
-    #   delete 'remove_bank_statement'
-    #   delete 'remove_paystub'
-    #   delete 'remove_w2'
-    #   delete 'remove_other_borrower_report'
-    #   get 'download_w2'
-    #   get 'download_paystub'
-    #   get 'download_bank_statement'
-    #   get 'download_other_borrower_report'
-    # end
   end
 
   resources :property_document_uploader do
     collection do
       post 'upload'
-      delete 'remove'
     end
 
     member do
       get 'download'
+      delete 'remove'
     end
   end
 
   resources :loan_document_uploader do
     collection do
       post 'upload'
-      delete 'remove'
     end
 
     member do
       get 'download'
+      delete 'remove'
     end
   end
 
   resources :closing_document_uploader do
     collection do
       post 'upload'
-      delete 'remove'
     end
 
     member do
       get 'download'
+      delete 'remove'
     end
   end
 
