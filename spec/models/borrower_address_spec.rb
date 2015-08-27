ec3e03266210a4cd7a2528350794b1aa53337379
Hoang Le <lehoang1417@gmail.com>
2015-08-27 16:28:31 +0700

    update rspec tests for borrower address and borrower models

--- spec/models/borrower_address_spec.rb
+++ spec/models/borrower_address_spec.rb
@@ -1,7 +1,13 @@
 require 'rails_helper'
 
 describe BorrowerAddress do
+  let(:borrower_address) { FactoryGirl.build(:borrower_address) }
+
   it 'has a valid factory' do
-    expect(FactoryGirl.build(:borrower_address)).to be_valid
+    expect(borrower_address).to be_valid
+  end
+
+  it 'has a valid factory with address object' do
+    expect(borrower_address.address).to be_truthy
   end
 end
--- spec/models/borrower_spec.rb
+++ spec/models/borrower_spec.rb
@@ -1,7 +1,32 @@
 require 'rails_helper'
 
 describe Borrower do
-  it 'has a valid factory' do
-    expect(FactoryGirl.build(:borrower)).to be_valid
+  context 'when created default' do
+    let(:borrower) { FactoryGirl.build(:borrower) }
+    subject { borrower }
+
+    it 'has a valid factory' do
+      expect(borrower).to be_valid
+    end
+
+    it 'has valid associations' do
+      expect(borrower.credit_report).to be_valid
+      expect(borrower.employments.count).to eq(1)
+      expect(borrower.borrower_addresses.count).to eq(1)
+    end
+  end
+
+  context 'when created with documents' do
+    let(:borrower) { FactoryGirl.build(:borrower_with_documents) }
+    subject { borrower }
+
+    it 'has documents' do
+      expect(borrower.first_w2).to be_valid
+      expect(borrower.second_w2).to be_valid
+      expect(borrower.first_paystub).to be_valid
+      expect(borrower.second_paystub).to be_valid
+      expect(borrower.first_bank_statement).to be_valid
+      expect(borrower.second_bank_statement).to be_valid
+    end
   end
 end
