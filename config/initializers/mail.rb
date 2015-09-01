3a902220132ad1139f7140209501c82c430dde42
Cuong Lam <thanhcuong1990@gmail.com>
2015-09-01 12:16:23 +0700

    Move mailjet config to .env

--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -3,8 +3,8 @@ if Rails.env.development? || Rails.env.production?
     address: 'in-v3.mailjet.com',
     port: 587,
     enable_starttls_auto: true,
-    user_name: 'e29db9857b8b24c7acff5235053d6c61',
-    password: '6bf5a4c3a72433b32221e37d9d409afc',
+    user_name: ENV["MAILJET_USERNAME"],
+    password: ENV["MAILJET_PASSWORD"],
     authentication: 'plain'
   }
 
