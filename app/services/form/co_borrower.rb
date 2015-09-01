298f99c4e95a426ecae7e588c374b5c88277f9b3
Cuong Vu <cuongvu0103@gmail.com>
2015-09-01 11:35:47 +0700

    Don't send a confirmation email to coborrower

--- app/services/form/co_borrower.rb
+++ app/services/form/co_borrower.rb
@@ -30,7 +30,8 @@ module Form
             password: default_password, password_confirmation: default_password,
             borrower_attributes: borrower_params
           )
-          user.skip_confirmation!
+          user.confirmed_at = Time.now
+          user.skip_confirmation_notification!
           user.save
           user.reload
         end
