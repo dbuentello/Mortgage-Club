6347c4e7695ff953ff31b2a4cc1fb2aa8186a302
Cuong Lam <thanhcuong1990@gmail.com>
2015-09-04 09:29:41 +0700

    Add dropzone to checklist upload.

--- client/assets/javascripts/components/ModalUpload.jsx
+++ client/assets/javascripts/components/ModalUpload.jsx
@@ -1,9 +1,12 @@
 var React = require('react/addons');
 
-/**
- * HelpTooltipView renders a pretty little circle with a question mark that
- * displays a helpful tooltip when hovered over.
- */
+var Dropzone = require('components/form/Dropzone');
+
+
+var fields = {
+  first_w2: {label: 'W2 - Most recent tax year', name: 'first_w2', placeholder: 'drap file here or browse', type: 'FirstW2'}
+};
+
 var ModalUpload = React.createClass({
   propTypes: {
     id: React.PropTypes.string.isRequired,
@@ -13,7 +16,27 @@ var ModalUpload = React.createClass({
     body: React.PropTypes.string.isRequired,
     yesCallback: React.PropTypes.func.isRequired
   },
-
+  getInitialState: function() {
+    return this.buildStateFromBorrower();
+  },
+  buildStateFromBorrower: function() {
+    var state = {};
+    _.map(Object.keys(fields), function(key) {
+      if (this.props.borrower[key]) { // has a document
+        state[fields[key].name] = this.props.borrower[key].attachment_file_name;
+        state[fields[key].id] = this.props.borrower[key].id;
+        state[fields[key].name + '_downloadUrl'] = '/document_uploaders/base_document/' + this.props.borrower[key].id +
+                                         '/download?type=' + fields[key].type;
+        state[fields[key].name + '_removedUrl'] = '/document_uploaders/base_document/' + this.props.borrower[key].id +
+                                         '/remove?type=' + fields[key].type;
+      } else {
+        state[fields[key].name] = fields[key].placeholder;
+        state[fields[key].name + '_downloadUrl'] = 'javascript:void(0)';
+        state[fields[key].name + '_removedUrl'] = 'javascript:void(0)';
+      }
+    }, this);
+    return state;
+  },
   getDefaultProps: function() {
     return {
       id: "modal-checklist",
@@ -27,6 +50,8 @@ var ModalUpload = React.createClass({
     var dataTarget = '#' + this.props.id;
     var labelId = this.props.id + 'Label';
 
+    var uploadUrl = '/document_uploaders/borrowers/upload';
+
     return (
       <span>
         {
@@ -37,21 +62,40 @@ var ModalUpload = React.createClass({
         }
 
         <div className="modal fade" id={this.props.id} tabIndex="-1" role="dialog" aria-labelledby={labelId}>
-          <div className="modal-dialog" role="document">
+          <div className="modal-dialog modal-upload" role="document">
             <div className="modal-content">
               <div className="modal-header">
                 <button type="button" className="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
                 <h4 className="modal-title" id={labelId}>{this.props.title}</h4>
               </div>
-              <div className="modal-body">
-                <div className="form-group">
-                  <label htmlFor="comment">{this.props.body}</label>
-                  <textarea id="answer" className="form-control typeWeightNormal input-lg" rows="4" cols="50" />
+              <div className="modal-body" >
+                <div className="upload-zone">
+                {
+                  _.map(Object.keys(fields), function(key) {
+                    var customParams = [
+                      {type: fields[key].type},
+                      {borrower_id: this.props.borrower.id}
+                    ];
+                    return(
+                      <div className="drop_zone" key={key}>
+                        <Dropzone field={fields[key]}
+                          uploadUrl={uploadUrl}
+                          downloadUrl={this.state[fields[key].name + '_downloadUrl']}
+                          removeUrl={this.state[fields[key].name + '_removedUrl']}
+                          tip={this.state[fields[key].name]}
+                          maxSize={10000000}
+                          customParams={customParams}
+                          supportOtherDescription={fields[key].customDescription}
+                        />
+                      </div>
+                    )
+                  }, this)
+                }
                 </div>
               </div>
               <div className="modal-footer">
-                <button type="button" className="btn btn-default" data-dismiss="modal">No</button>
-                <button type="button" className="btn btn-primary" onClick={this.props.yesCallback}>Yes</button>
+                <button type="button" className="btn btn-default" data-dismiss="modal">Cancel</button>
+                <button type="button" className="btn btn-primary" onClick={this.props.yesCallback}>OK</button>
               </div>
             </div>
           </div>
