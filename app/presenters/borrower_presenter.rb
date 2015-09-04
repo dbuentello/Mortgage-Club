a57e3ec0ea063fb6863180498b6902f0ccfdfedf
Cuong Vu <cuongvu0103@gmail.com>
2015-09-04 14:04:20 +0700

    Remove caching in BorrowerPresenter

--- app/presenters/borrower_presenter.rb
+++ app/presenters/borrower_presenter.rb
@@ -4,15 +4,11 @@ class BorrowerPresenter
   end
 
   def show
-    Rails.cache.fetch("borrower_presenter_show-#{@borrower.id}-#{@borrower.updated_at.to_i}", expires_in: 7.day) do
-      @borrower.as_json(show_borrower_json_options)
-    end
+    @borrower.as_json(show_borrower_json_options)
   end
 
   def show_documents
-    Rails.cache.fetch("borrower_presenter_show_documents-#{@borrower.id}-#{@borrower.updated_at.to_i}", expires_in: 7.day) do
-      @borrower.borrower_documents.includes(:owner).as_json(borrower_documents_json_options)
-    end
+    @borrower.borrower_documents.includes(:owner).as_json(borrower_documents_json_options)
   end
 
   private
