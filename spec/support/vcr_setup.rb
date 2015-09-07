f8aeeea9ae8767db606c4b017806b7ec1ed41b73
Cuong Vu <cuongvu0103@gmail.com>
2015-09-07 09:01:26 +0700

    Update tests + VCR

--- spec/support/vcr_setup.rb
+++ spec/support/vcr_setup.rb
@@ -3,4 +3,5 @@ VCR.configure do |c|
   c.hook_into :webmock # or :fakeweb
   c.ignore_localhost = true
   c.debug_logger = File.open('vcr.log', 'w')
+  c.allow_http_connections_when_no_cassette = true
 end
