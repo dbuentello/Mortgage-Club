c0ee01502256de091088a2cf09a7fe61630c1760
Cuong Lam <thanhcuong1990@gmail.com>
2015-09-10 12:23:14 +0700

    Add status to invite model.

--- app/services/invite_service.rb
+++ app/services/invite_service.rb
@@ -4,10 +4,11 @@ class InviteService
     if token.present?
       invite = Invite.find_by_token(token)
       invite.join_at = Time.zone.now
+      invite.status = 'done'
       invite.save
     else
       if invite_code.present?
-        invite = Invite.new(email: user.email, name: user.to_s)
+        invite = Invite.new(email: user.email, name: user.to_s, status: 'done')
         invite.sender_id = invite_code
         invite.join_at = Time.zone.now
         invite.save
