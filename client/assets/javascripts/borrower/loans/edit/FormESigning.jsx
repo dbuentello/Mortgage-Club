6cc0b96fc16a995e1e7ee3361c26f5a06cc79d50
Cuong Vu <cuongvu0103@gmail.com>
2015-09-14 11:08:49 +0700

    Update front end for Docusign Template
    Why:
    * To work with updated backend
    
    This change addresses the need by:
    * Composite template

--- client/assets/javascripts/borrower/loans/edit/FormESigning.jsx
+++ client/assets/javascripts/borrower/loans/edit/FormESigning.jsx
@@ -6,76 +6,50 @@ var SelectField = require('components/form/SelectField');
 var FormESigning = React.createClass({
   getInitialState: function() {
     return {
-      templateName: 'Loan Estimate'
+      loaded: false
     }
-  },
-
-  onClick: function(e) {
-    e.preventDefault();
-    $(this.refs.btnSign.getDOMNode()).button('loading')
-    $.ajax({
-      url: "/electronic_signature/template/",
-      method: 'POST',
-      data: {
-        template_name: this.state.templateName,
-        id: this.props.loan.id
-      },
-      dataType: 'json',
-      success: function(response) {
-        if (response.message == "don't render iframe") {
-          alert("Okay, done!");
-        } else if (response.message == "template does not exist yet") {
-          alert(response.details);
-        } else {
-          $(this.refs.iframe.getDOMNode()).attr("src", response.message.url);
-          $(this.refs.iframe.getDOMNode()).css("display", "block");
-        }
-        $(this.refs.btnSign.getDOMNode()).button('reset');
-      }.bind(this),
-      error: function(response, status, error) {
-        $(this.refs.btnSign.getDOMNode()).button('reset');
-        alert(error);
-      }
-    });
   },
 
   onChange: function(change) {
     this.setState(change);
   },
 
-  render: function() {
-    var templateOptions = [
-      {name: 'Loan Estimate', value: 'Loan Estimate'},
-      {name: 'Servicing Disclosure', value: 'Servicing Disclosure'}
-    ];
+  componentDidMount: function() {
+    $.ajax({
+      url: "/electronic_signature/template/",
+      method: 'POST',
+      data: {
+        id: this.props.loan.id
+      },
+      dataType: 'json',
+      success: function(response) {
+        $(this.refs.iframe.getDOMNode()).attr("src", response.message.url);
+        $(this.refs.iframe.getDOMNode()).css("display", "block");
+        this.setState({loaded: true});
+      }.bind(this),
+      error: function(response, status, error) {
+        var flash = { "alert-danger": response.responseJSON.message };
+        this.showFlashes(flash);
+        this.setState({loaded: true});
+      }
+    });
+  },
 
+  render: function() {
     return (
       <div>
         <div className='iframeContent'>
           <div className='pal'>
             <div className='row'>
               <div className='col-xs-3'>
-                <SelectField
-                  label='Choose an template to sign'
-                  keyName={'templateName'}
-                  value={this.state.templateName}
-                  options={templateOptions}
-                  editable={true}
-                  onChange={this.onChange}/>
+                <h4 style={{display: this.state.loaded ? 'none' : null}}>Loading...please wait</h4>
               </div>
             </div>
 
             <br/>
-            <div className='text-left'>
-              <a ref='btnSign' className='btn btnSml btnPrimary' onClick={this.onClick} data-loading-text="Loading...">
-                Sign
-              </a>
-            </div>
-
             <div className='mtl text-left'>
               <iframe ref='iframe' height='600px' width='100%' style={{display: 'none'}}></iframe>
             </div>
-
           </div>
         </div>
       </div>
