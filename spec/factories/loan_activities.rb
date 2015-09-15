e6dded4bdd7e73cf9efd4cf2edf3937b0f27391e
Cuong Vu <cuongvu0103@gmail.com>
2015-09-15 14:48:07 +0700

    Add new spec & cucumber for Checklist + Update specs for LoanActivity

--- spec/factories/loan_activities.rb
+++ spec/factories/loan_activities.rb
@@ -1,3 +1,21 @@
+# == Schema Information
+#
+# Table name: loan_activities
+#
+#  id              :uuid             not null, primary key
+#  name            :string
+#  activity_type   :integer          default(0), not null
+#  activity_status :integer          default(0), not null
+#  user_visible    :boolean          default(FALSE), not null
+#  loan_id         :uuid
+#  loan_member_id  :uuid
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#  duration        :integer          default(0)
+#  start_date      :datetime
+#  end_date        :datetime
+#
+
 FactoryGirl.define do
   factory :loan_activity do |f|
     loan
