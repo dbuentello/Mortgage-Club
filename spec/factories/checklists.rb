851f3eed317be1cdf1fa65c55e9332bb42e49061
Cuong Vu <cuongvu0103@gmail.com>
2015-09-15 14:06:24 +0700

    Update Checklist Model
    Why:
    * To make sense, changing name a colum from description to info
    * Add two columns: document, document_description
    * To make sense, changing name of method from document to document_info
    * Add some validations
    
    This change addresses the need by:
    * Checklist model

--- spec/factories/checklists.rb
+++ spec/factories/checklists.rb
@@ -2,13 +2,15 @@ FactoryGirl.define do
   factory :checklist do |f|
     loan
     template
-    user { create(:loan_member_user) }
+    user { create(:loan_member_user_with_loan_member) }
 
     f.name { Faker::Lorem.sentence }
-    f.description { Faker::Lorem.sentence }
+    f.document_description { Faker::Lorem.sentence }
+    f.info { Faker::Lorem.sentence }
     f.due_date { Faker::Date.forward(10) }
     f.status { 'pending' }
     f.document_type { 'FirstBankStatement' }
+    f.document { 'borrower' }
 
     factory :checklist_explain do
       checklist_type { 'explain' }
