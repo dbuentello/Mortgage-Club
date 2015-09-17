52006b9d67246dc6ea92a23f1ae33ae86b55b83e
Cuong Vu <cuongvu0103@gmail.com>
2015-09-17 14:57:42 +0700

    Extract logic into some methods

--- app/services/zillow_service/get_mortgage_rate.rb
+++ app/services/zillow_service/get_mortgage_rate.rb
@@ -8,7 +8,7 @@ module ZillowService
 
     def self.call(zipcode)
       return unless zipcode
-      Rails.cache.fetch("mortgage-rates-#{zipcode}-#{Time.zone.now.to_date.to_s}", expires_in: 1.day) do
+      Rails.cache.fetch("mortgage-rates-#{zipcode}-#{Time.zone.now.to_date.to_s}", expires_in: 12.hour) do
         zipcode = zipcode[0..4] if zipcode.length > 5
         set_up_crawler
         fill_in_form(zipcode)
@@ -66,69 +66,12 @@ module ZillowService
             data = Nokogiri::HTML.parse(@session.html)
             lender_name = data.css(".zmm-quote-details-content .zsg-h1").text
             nmls = data.css(".zmm-qdp-subtitle-list li")[0].text.gsub(/[^0-9\.]/,'')
-            #loan details
-            loan_details = {
-              "Interest rate" => 0,
-              "APR" => 0,
-              "Payment (principal & interest)" => 0,
-              "Loan amount" => 0,
-              "Down payment" => 0,
-              "Base loan amount" => 0,
-              "Total loan amount" => 0,
-              "FHA upfront MI premium" => 0,
-              "Loan product" => "",
-              "Quote ID" => "",
-              "Date submitted" => ""
-            }
-            data.css(".zmm-qdp-loan-details-section .zmm-table_tooltips tr").each do |tr|
-              info_name = tr.css("th").text
-              value = tr.css("td").text
-              loan_details[info_name]  = value
-
-              next if ["Loan product", "Quote ID", "Date submitted"].include? info_name
-
-              if value.include?("(")
-                loan_details[info_name] = ("-" << value.gsub(/[^0-9\.]/,'')).to_f
-              else
-                loan_details[info_name] = value.gsub(/[^0-9\.]/,'').to_f
-              end
-            end
-
-            duration = data.css(".zmm-qdp-loan-details-section .zmm-qdp-loan-product").text.gsub(/[^0-9\.]/,'').to_f
-            rate = data.css(".zmm-qdp-loan-details-section .zmm-table_tooltips")[0].css("tr")[1].css("td")[0].text.gsub(/[^0-9\.]/,'').to_f
-            apr = data.css(".zmm-qdp-loan-details-section .zmm-table_tooltips")[0].css("tr")[2].css("td")[0].text.gsub(/[^0-9\.]/,'').to_f
-            payment = data.css(".zmm-qdp-loan-details-section .zmm-table_tooltips")[0].css("tr")[3].css("td")[0].text.gsub(/[^0-9\.]/,'').to_f
-            loan_amount = data.css(".zmm-qdp-loan-details-section .zmm-table_tooltips")[0].css("tr")[4].css("td")[0].text.gsub(/[^0-9\.]/,'').to_f
-            down_payment = data.css(".zmm-qdp-loan-details-section .zmm-table_tooltips")[0].css("tr")[5].css("td")[0].text.gsub(/[^0-9\.]/,'').to_f
-            quote_id = data.css(".zmm-qdp-loan-details-section .zmm-table_tooltips")[0].css("tr")[6].css("td")[0].text
-            date_submitted = data.css(".zmm-qdp-loan-details-section .zmm-table_tooltips")[0].css("tr")[7].css("td")[0].text
-            rate_lock = data.css(".zmm-qdp-loan-details-section .zmm-table_tooltips")[1].css("tr")[0].css("td")[0].text
-            due_in = data.css(".zmm-qdp-loan-details-section .zmm-table_tooltips")[1].css("tr")[1].css("td")[0].text
-            prepayment_penalty = data.css(".zmm-qdp-loan-details-section .zmm-table_tooltips")[1].css("tr")[2].css("td")[0].text
-            fees = {
-              "Loan origination fee" => 0,
-              "Underwriting fee" => 0,
-              "Appraisal fee" => 0,
-              "Lender credit" => 0,
-              "Tax service fee" => 0,
-              "Credit report fee" => 0,
-              "Total Estimated Fees" => 0
-            }
-            #fees
-            data.css(".zmm-quote-details-fees .zmm-table_tooltips tr").each do |tr|
-              fee_name = tr.css("th").text
-              fee = tr.css("td").text
-              if fee.include?("(")
-                fees[fee_name] = ("-" << fee.gsub(/[^0-9\.]/,'')).to_f
-              else
-                fees[fee_name] = fee.gsub(/[^0-9\.]/,'').to_f
-              end
-            end
             @session.find(".zsg-icon-x-thin ").click
+
             {
               lender: {name: lender_name, nmls: nmls},
-              loan: loan_details,
-              fees: fees
+              loan: get_loan_details(data),
+              fees: get_lender_fees(data)
             }
           end
         end
@@ -136,6 +79,57 @@ module ZillowService
         Rails.logger.error error.message
       end
       lenders.flatten
+    end
+
+    def self.get_lender_fees(data)
+      fees = {
+        "Loan origination fee" => 0,
+        "Underwriting fee" => 0,
+        "Appraisal fee" => 0,
+        "Lender credit" => 0,
+        "Tax service fee" => 0,
+        "Credit report fee" => 0,
+        "Total Estimated Fees" => 0
+      }
+      data.css(".zmm-quote-details-fees .zmm-table_tooltips tr").each do |tr|
+        fee_name = tr.css("th").text
+        fee = tr.css("td").text
+        if fee.include?("(")
+          fees[fee_name] = ("-" << fee.gsub(/[^0-9\.]/,'')).to_f
+        else
+          fees[fee_name] = fee.gsub(/[^0-9\.]/,'').to_f
+        end
+      end
+    end
+
+    def self.get_loan_details(data)
+      loan_details = {
+        "Interest rate" => 0,
+        "APR" => 0,
+        "Payment (principal & interest)" => 0,
+        "Loan amount" => 0,
+        "Down payment" => 0,
+        "Base loan amount" => 0,
+        "Total loan amount" => 0,
+        "FHA upfront MI premium" => 0,
+        "Loan product" => "",
+        "Quote ID" => "",
+        "Date submitted" => ""
+      }
+      data.css(".zmm-qdp-loan-details-section .zmm-table_tooltips tr").each do |tr|
+        info_name = tr.css("th").text
+        value = tr.css("td").text
+        loan_details[info_name]  = value
+
+        next if ["Loan product", "Quote ID", "Date submitted"].include? info_name
+
+        if value.include?("(")
+          loan_details[info_name] = ("-" << value.gsub(/[^0-9\.]/,'')).to_f
+        else
+          loan_details[info_name] = value.gsub(/[^0-9\.]/,'').to_f
+        end
+      end
+      loan_details
     end
 
     def self.no_result?(data)
