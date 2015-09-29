8200928af3325943f6fb4f01288f67323b52410b
Cuong Vu <cuongvu0103@gmail.com>
2015-09-29 17:51:57 +0700

    Turn off mode:debug for log

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -43,14 +43,13 @@ Rails.application.configure do
   # config.force_ssl = true
 
   # Set to :info to decrease the log volume.
-  config.log_level = :debug
+  config.log_level = :info
 
   # Prepend all log lines with the following tags.
   # config.log_tags = [ :subdomain, :uuid ]
 
   # Use a different logger for distributed setups.
   # config.logger = ActiveSupport::TaggedLogging.new(SyslogLogger.new)
-    config.logger = Logger.new(STDOUT)
   # Use a different cache store in production.
   # config.cache_store = :mem_cache_store
 
