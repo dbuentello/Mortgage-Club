c2391d513eaaa117fecc4a0722fd3dbe04ed6a9d
Cuong Vu <cuongvu0103@gmail.com>
2015-10-19 16:10:05 +0700

    Auto sign-in for investor
    Why:
    * Investors can play around MortgageClub without sign-in
    
    This change addresses the need by:
    * Investor

--- app/controllers/users/base_controller.rb
+++ app/controllers/users/base_controller.rb
@@ -1,7 +1,20 @@
 class Users::BaseController < ApplicationController
-  before_action :authenticate_borrower!
+  if Rails.env.production?
+    skip_before_action :authenticate_user!
+    before_action :fake_authenticate!
+  else
+    before_action :authenticate_borrower!
+  end
 
   private
+
+  def fake_authenticate!
+    @current_user = User.where(email: 'borrower@gmail.com').last
+    sign_in(@current_user)
+    unless @current_user.borrower?
+      redirect_to borrower_root_url, alert: "The page does not exist or you don't have permmission to access!"
+    end
+  end
 
   def authenticate_borrower!
     unless current_user.borrower?
