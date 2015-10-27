d77b2d3c3ddf375c69af73c589e83e7c5eadd8df
Cuong Lam <thanhcuong1990@gmail.com>
2015-10-27 15:59:28 +0700

    create ocr model.

--- app/services/ocr_parse_service.rb
+++ app/services/ocr_parse_service.rb
@@ -2,26 +2,16 @@ require 'securerandom'
 
 class OcrParseService
   include HTTParty
-  PATH = "#{Rails.root}/documents/"
 
   def self.call(raw_post)
     message = JSON.parse(raw_post["Message"])
     record = message["Records"].first
     bucket_name = record["s3"]["bucket"]["name"]
     key = record["s3"]["object"]["key"]
-    region = record["awsRegion"]
 
     s3 = AWS::S3::Client.new
-    extension = File.extname(key)
-    file_name = PATH << random_name << extension
-
     response = s3.get_object({bucket_name: bucket_name, key: key})
     doc = Nokogiri::XML(response.data[:data])
     # doc.css('_EmployerName').first.content
   end
-
-  def self.random_name
-    SecureRandom.hex(8)
-  end
-
 end
