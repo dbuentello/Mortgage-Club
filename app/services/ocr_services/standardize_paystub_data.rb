33772b068b02bcbe06ca1024ffd5c90bf9eb71eb
Cuong Vu <cuongvu0103@gmail.com>
2015-10-28 18:32:31 +0700

    Add StandardizePaystubData service
    Why:
    * We should standardise data before save them into database
    
    This change addresses the need by:
    * Employment's data

---
+++ app/services/ocr_services/standardize_paystub_data.rb
@@ -0,0 +1,103 @@
+require 'fuzzystringmatch'
+
+module OcrServices
+  class StandardizePaystubData
+    attr_reader :ocr_data
+
+    def initialize(borrower_id)
+      @ocr_data = Ocr.where(borrower_id: borrower_id).last
+      @jarow = FuzzyStringMatch::JaroWinkler.create(:pure)
+    end
+
+    def call
+      return Rails.logger.error "Borrower #{borrower_id} does not have OCR data" if ocr_data.nil?
+
+      {
+        employer_name: standardized_employer_name,
+        employer_address: standardized_employer_address,
+        period: standardized_period,
+        current_salary: standardized_salary,
+        ytd_salary: standardized_ytd_salary
+      }
+    end
+
+    def standardized_employer_name
+      return if percentage_similarity(ocr_data.employer_name_1, ocr_data.employer_name_2) < 0.97
+
+      (ocr_data.employer_name_1.length > ocr_data.employer_name_2.length) ? ocr_data.employer_name_1 : ocr_data.employer_name_2
+    end
+
+    def standardized_employer_address
+      first_line = standardized_employer_address_line(ocr_data.address_first_line_1, ocr_data.address_first_line_2)
+      second_line = standardized_employer_address_line(ocr_data.address_second_line_1, ocr_data.address_second_line_2)
+
+      return first_line << " " << second_line if first_line.present? && second_line.present?
+      return first_line if first_line.present?
+      return second_line if second_line.present?
+    end
+
+    def standardized_employer_address_line(first_address, last_address)
+      return if percentage_similarity(first_address, last_address) < 0.97
+
+      (first_address.length > last_address.length) ? first_address : last_address
+    end
+
+    def standardized_period
+      return "semimonthly" if semimonthly_frequency?
+      return "biweekly" if biweekly_frequency?
+      return "weekly" if weekly_frequency?
+    end
+
+    def standardized_salary
+      if valid_salary?(ocr_data.current_salary_1, ocr_data.current_salary_2)
+        return (ocr_data.current_salary_1 > ocr_data.current_salary_2 ? ocr_data.current_salary_1 : ocr_data.current_salary_2).ceil
+      end
+
+      if valid_salary?(ocr_data.current_earnings_1, ocr_data.current_earnings_2)
+        return (ocr_data.current_earnings_1 > ocr_data.current_earnings_2 ? ocr_data.current_earnings_1 : ocr_data.current_earnings_2).ceil
+      end
+    end
+
+    def standardized_ytd_salary
+      return unless valid_salary?(ocr_data.ytd_salary_1, ocr_data.ytd_salary_2)
+
+      (ocr_data.ytd_salary_1 > ocr_data.ytd_salary_2 ? ocr_data.ytd_salary_1 : ocr_data.ytd_salary_2).ceil
+    end
+
+    private
+
+    def percentage_similarity(first_str, last_str)
+      return 0 if first_str.nil? || last_str.nil?
+
+      @jarow.getDistance(first_str, last_str)
+    end
+
+    def date_of_month(datetime)
+      datetime.strftime("%d").to_i
+    end
+
+    def date_is_end_of_month?(datetime)
+      datetime.to_i == datetime.end_of_month.to_i
+    end
+
+    def semimonthly_frequency?
+      (date_of_month(ocr_data.period_ending_1) == 15 || date_is_end_of_month?(ocr_data.period_ending_1)) &&
+      (date_of_month(ocr_data.period_ending_2) == 15 || date_is_end_of_month?(ocr_data.period_ending_2))
+    end
+
+    def biweekly_frequency?
+      date_of_month(ocr_data.period_ending_1) - date_of_month(ocr_data.period_beginning_1) == 14 &&
+      date_of_month(ocr_data.period_ending_2) - date_of_month(ocr_data.period_beginning_2) == 14
+    end
+
+    def weekly_frequency?
+      date_of_month(ocr_data.period_ending_1) - date_of_month(ocr_data.period_beginning_1) == 7 &&
+      date_of_month(ocr_data.period_ending_2) - date_of_month(ocr_data.period_beginning_2) == 7
+    end
+
+    def valid_salary?(first_salary, last_salary)
+      ratio = first_salary.to_f / last_salary.to_f
+      0.95 <= ratio && ratio <= 1.05
+    end
+  end
+end
