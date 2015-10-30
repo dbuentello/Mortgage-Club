537ffc9886f88db65c411e40873d86ad91e23bc1
Cuong Lam <thanhcuong1990@gmail.com>
2015-10-30 15:57:12 +0700

    Fix bug & update spec for ParseXmlService.

---
+++ spec/services/ocr_services/parse_xml_file_spec.rb
@@ -0,0 +1,59 @@
+require "rails_helper"
+
+describe OcrServices::ParseXmlFile do
+  before(:each) do
+    AWS.config(access_key_id: ENV['AWS_ACCESS_KEY_ID'], secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'], region: 'us-west-2')
+
+    @raw_post = {
+      "Message" => {
+        "Records" => [
+          {
+            "eventVersion" => "2.0",
+            "eventSource" => "aws:s3",
+            "awsRegion" => "us-west-2",
+            "eventTime" => "2015-10-29T03:34:26.531Z",
+            "eventName" => "ObjectCreated:Put",
+            "userIdentity" => {
+              "principalId" => "AKTQO7Y5H729B"
+            },
+            "requestParameters" => {
+              "sourceIPAddress" => "14.167.113.143"
+            },
+            "responseElements" => {
+              "x-amz-request-id" => "7C057E095999E4CF",
+              "x-amz-id-2" => "+ooiYcrpxjT8+IDDFDaEO6gxBn2i3rqeHY+E8/5tDUBdQfTSeZV3sualcpMX9Ypt"
+            },
+            "s3" => {
+              "s3SchemaVersion" => "1.0",
+              "configurationId" => "ocrNotifications-Documents",
+              "bucket" => {
+                "name" => "dev-homieo",
+                "ownerIdentity" => {
+                  "principalId" => "AKTQO7Y5H729B"
+                },
+                "arn" => "arn:aws:s3:::dev-homieo"
+              },
+              "object" => {
+                "key" => "ocr_results/SecondPaystub-2fde4a53-0f89-413b-9a02-dfdd9dec4da5.xml",
+                "size" => "831",
+                "eTag" => "9817a6d1fcb7d4a2feb3fea0907d66bd",
+                "sequencer" => "00563193C27ADAF7F0"
+              }
+            }
+          }
+        ]
+      }.to_json
+    }
+
+    allow_any_instance_of(AWS::S3::Client::V20060301).to receive(:get_object).and_return("")
+  end
+
+  it "parse a xml file" do
+    expect_any_instance_of(AWS::S3::Client::V20060301).to receive(:get_object)
+    OcrServices::ParseXmlFile.call(@raw_post)
+  end
+
+  it "call OcrParseService" do
+    expect(OcrServices::ParseXmlFile.call(@raw_post)).to eq({})
+  end
+end
