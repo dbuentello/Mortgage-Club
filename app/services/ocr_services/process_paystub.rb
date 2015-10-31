1bc688c3a321fd78c7f8eab17eb1258b81fc8a4e
Cuong Lam <thanhcuong1990@gmail.com>
2015-10-31 23:49:53 +0700

    Fix check hash is empty.

--- app/services/ocr_services/process_paystub.rb
+++ app/services/ocr_services/process_paystub.rb
@@ -2,7 +2,7 @@ module OcrServices
   class ProcessPaystub
     def self.call(raw_post)
       data = OcrServices::ParseXmlFile.call(raw_post)
-      return if data.blank?
+      return if data.empty?
 
       borrower_id = data[:borrower_id]
       borrower = Borrower.find_by_id(borrower_id)
