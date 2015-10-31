aba8c1b15c8e83b549d808accabd8fdf4aca1633
Cuong Vu <cuongvu0103@gmail.com>
2015-10-31 21:57:33 +0700

    Remove hard code data

--- app/controllers/employments_controller.rb
+++ app/controllers/employments_controller.rb
@@ -2,14 +2,6 @@ class EmploymentsController < ApplicationController
   def show
     employment = Employment.find(params[:id])
 
-    # demo, it will be removed when we get ABBYY's license
-    employment.update(
-      employer_name: 'Samsung',
-      pay_frequency: 'weekly',
-      current_salary: 2334
-    )
-    employment.address.update(full_text: '227 Nguyen Van Cu, Q5, TP.HCM')
-
     render json: {employment: EmploymentPresenter.new(employment).show}
   end
 end
