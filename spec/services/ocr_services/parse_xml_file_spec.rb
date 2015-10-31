4b7a6d65d85c6a88d1ebaf7257b5a69fa113dee3
Cuong Lam <thanhcuong1990@gmail.com>
2015-11-01 00:16:03 +0700

    fix ocr_result spec.

--- spec/services/ocr_services/parse_xml_file_spec.rb
+++ spec/services/ocr_services/parse_xml_file_spec.rb
@@ -16,13 +16,6 @@ describe OcrServices::ParseXmlFile do
             "userIdentity" => {
               "principalId" => "AKTQO7Y5H729B"
             },
-            "requestParameters" => {
-              "sourceIPAddress" => "14.167.113.143"
-            },
-            "responseElements" => {
-              "x-amz-request-id" => "7C057E095999E4CF",
-              "x-amz-id-2" => "+ooiYcrpxjT8+IDDFDaEO6gxBn2i3rqeHY+E8/5tDUBdQfTSeZV3sualcpMX9Ypt"
-            },
             "s3" => {
               "s3SchemaVersion" => "1.0",
               "configurationId" => "ocrNotifications-Documents",
@@ -34,10 +27,10 @@ describe OcrServices::ParseXmlFile do
                 "arn" => "arn:aws:s3:::dev-homieo"
               },
               "object" => {
-                "key" => "ocr_results/SecondPaystub-2fde4a53-0f89-413b-9a02-dfdd9dec4da5.xml",
+                "key" => "ocr_results/TestSecondPaystub-2fde4a53-0f89-413b-9a02-dfdd9dec4da5.xml",
                 "size" => "831",
                 "eTag" => "9817a6d1fcb7d4a2feb3fea0907d66bd",
-                "sequencer" => "00563193C27ADAF7F0"
+                "sequencer" => "005634F6811633C495"
               }
             }
           }
@@ -47,20 +40,20 @@ describe OcrServices::ParseXmlFile do
   end
 
   it "call OcrParseService" do
-    VCR.use_cassette("get ocr result xml file", :record => :new_episodes) do
+    VCR.use_cassette("get ocr result xml file") do
       response = OcrServices::ParseXmlFile.call(@raw_post)
       expect(response).to eq(
         {
-          :employer_name=>"BRIAN R FONG DDS INC",
-          :address_first_line=>"3426 MURDOCH DR",
-          :address_second_line=>"Palo Alto CA 94306",
-          :period_beginning=>"4/1/2015",
-          :period_ending=>"4/30/2015",
-          :current_salary=>"7916.67",
-          :ytd_salary=>"63333.36",
-          :current_earnings=>"",
-          :borrower_id=>"2fde4a53-0f89-413b-9a02-dfdd9dec4da5",
-          :order_of_paystub=>2,
+          :employer_name => "BRIAN R FONG DDS INC",
+          :address_first_line => "3426 MURDOCH DR",
+          :address_second_line => "Palo Alto CA 94306",
+          :period_beginning => "4/1/2015",
+          :period_ending => "4/30/2015",
+          :current_salary => "7916.67",
+          :ytd_salary => "63333.36",
+          :current_earnings => "",
+          :borrower_id => "2fde4a53-0f89-413b-9a02-dfdd9dec4da5",
+          :order_of_paystub => nil,
         }
       )
     end
