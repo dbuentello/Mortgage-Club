4325a8550ba78a3c1562d61f85a782ce78d1661c
Cuong Vu <cuongvu0103@gmail.com>
2015-11-04 22:21:34 +0700

    Add spec for UnderwriteLoan service

--- spec/services/underwriting_loan_services/calculate_debt_to_income_spec.rb
+++ spec/services/underwriting_loan_services/calculate_debt_to_income_spec.rb
@@ -5,8 +5,8 @@ describe UnderwritingLoanServices::CalculateDebtToIncome do
   let!(:primary_property) { FactoryGirl.create(:rental_property, is_primary: true, loan: loan) }
   let!(:first_property) { FactoryGirl.create(:rental_property, is_primary: false, loan: loan) }
   let!(:second_property) { FactoryGirl.create(:rental_property, is_primary: false, loan: loan) }
-  let!(:mortgage_liability) { FactoryGirl.create(:liability, liability_type: "Mortgage", property: first_property) }
-  let!(:other_liability) { FactoryGirl.create(:liability, liability_type: "OtherFinancing", property: second_property) }
+  let!(:mortgage_liability) { FactoryGirl.create(:liability, account_type: "Mortgage", property: first_property) }
+  let!(:other_liability) { FactoryGirl.create(:liability, account_type: "OtherFinancing", property: second_property) }
 
   describe ".sum_liability_payment" do
     it "returns sum of liability's payment" do
