dad1d940729aeea55abe125c6c6be6b9f901828a
Cuong Lam <thanhcuong1990@gmail.com>
2015-11-05 14:48:04 +0700

    Update status follow timer.

--- client/assets/javascripts/borrower/loans/underwriting/Underwriting.jsx
+++ client/assets/javascripts/borrower/loans/underwriting/Underwriting.jsx
@@ -1,42 +1,70 @@
 var _ = require('lodash');
 var React = require('react/addons');
 
+var timer;
+
 var Underwriting = React.createClass({
+  getInitialState: function() {
+    return {
+      percentCounter: 0
+    }
+  },
+
+  componentDidMount: function() {
+    timer = window.setInterval(this.incrementPercent, 200);
+  },
+
+  incrementPercent: function() {
+    var counter = this.state.percentCounter + 1
+    this.setState({
+      percentCounter: counter
+    });
+    document.getElementById("percent").innerHTML = counter + "%";
+
+    switch(counter) {
+      case 10:
+        document.getElementById("status").innerHTML = "Verifying borrower's credit score.";
+        break;
+      case 20:
+        document.getElementById("status").innerHTML = "Verifying borrower's income.";
+        break;
+      case 30:
+        document.getElementById("status").innerHTML = "Calculating debt-to-income ratio.";
+        break;
+      case 40:
+        document.getElementById("status").innerHTML = "Verifying down payment and cash reserves";
+        break;
+      case 50:
+        document.getElementById("status").innerHTML = "Calculating loan-to-value ratio";
+        break;
+      case 60:
+        document.getElementById("status").innerHTML = "Verifying borrower's experience";
+        break;
+      case 70:
+        document.getElementById("status").innerHTML = "Searching for eligible loan programs";
+        break;
+      case 85:
+        document.getElementById("status").innerHTML = "Preparing loan programs to display";
+        break;
+      case 100:
+        window.clearInterval(timer);
+        break;
+      default:
+        break;
+    }
+  },
 
   render: function() {
     return (
       <div className='content container underwriting-text '>
         <div className='row mtl'>
           <div className='col-sm-5'>
-            <div className="percent outer">86%</div>
+            <div id="percent" className="percent outer">0%</div>
           </div>
+
           <div className='col-sm-6'>
-            <div className="design-row row1">
-              <div className="text columns small-9">Checking property eligibility<span className="dots">.</span></div>
-            </div>
-            <div className="design-row row2">
-              <div className="text columns small-9">Verifying borrower's credit score<span className="dots">.</span></div>
-            </div>
-            <div className="design-row row3">
-              <div className="text columns small-9">Verifying borrower's income<span className="dots">.</span></div>
-            </div>
-            <div className="design-row row4">
-              <div className="text columns small-9">Calculating debt-to-income ratio<span className="dots">.</span></div>
-            </div>
-            <div className="design-row row5">
-              <div className="text columns small-9">Verifying down payment and cash reserves<span className="dots">.</span></div>
-            </div>
-            <div className="design-row row6">
-              <div className="text columns small-9">Calculating loan-to-value ratio<span className="dots">.</span></div>
-            </div>
-            <div className="design-row row7">
-              <div className="text columns small-9">Verifying borrower's experience<span className="dots">.</span></div>
-            </div>
-            <div className="design-row row8">
-              <div className="text columns small-9">Searching for eligible loan programs<span className="dots">.</span></div>
-            </div>
-            <div className="design-row row9">
-              <div className="text columns small-9">Preparing loan programs to display<span className="dots">.</span></div>
+            <div className="row1">
+              <div id="status">Checking property eligibility</div>
             </div>
           </div>
         </div>
