c5ad1b20a3cba701fabc7ee24a7685240effdb7f
Cuong Lam <thanhcuong1990@gmail.com>
2015-11-06 13:44:43 +0700

    Crawl mortgage rates from Wells Fargo & Quicken Loans

---
+++ app/services/mortgage_rate_services/quickenloans.rb
@@ -0,0 +1,37 @@
+module MortgageRateServices
+  class Quickenloans
+    include HTTParty
+
+    def self.call
+      html = get("http://www.quickenloans.com/mortgage-rates")
+      doc = Nokogiri::HTML(html)
+
+      apr_5_libor = 0
+      apr_15_year = 0
+      apr_30_year = 0
+
+      doc.css('.rateTable__row').each do |rate|
+        product_link = rate.css('.rateTable__product__link')
+        if product_link.present?
+          product_link_text = product_link.first.text
+          apr = rate.css('.rateTable__product__apr').text.split('%').first.gsub('(', '')
+          if product_link_text == 'VA 5/1 ARM (1/1/5)'
+            apr_5_libor = apr
+          elsif product_link_text == '15-Year Fixed'
+            apr_15_year = apr
+          elsif product_link_text == '30-Year Fixed'
+            apr_30_year = apr
+          else
+            next
+          end
+        end
+      end
+
+      {
+        apr_30_year: apr_30_year,
+        apr_15_year: apr_15_year,
+        apr_5_libor: apr_5_libor
+      }
+    end
+  end
+end
---
+++ app/services/mortgage_rate_services/wellsfargo.rb
@@ -0,0 +1,32 @@
+module MortgageRateServices
+  class Wellsfargo
+    include HTTParty
+
+    def self.call
+      html = get("https://www.wellsfargo.com/mortgage/rates")
+      doc = Nokogiri::HTML(html)
+
+      apr_5_libor = 0
+      apr_15_year = 0
+      apr_30_year = 0
+
+      doc.css('#productName').each do |rate|
+        if rate.text == '5/1 ARM FHA' && rate.at_css('a').attr('href') == '/mortgage/rates/purchase-assumptions?prod=5'
+          apr_5_libor = rate.parent.css('td').last.text.gsub('%', '')
+        elsif rate.text == '15-Year Fixed Rate' && rate.at_css('a').attr('href') == '/mortgage/rates/purchase-assumptions?prod=3'
+          apr_15_year = rate.parent.css('td').last.text.gsub('%', '')
+        elsif rate.text == '30-Year Fixed Rate' && rate.at_css('a').attr('href') == '/mortgage/rates/purchase-assumptions?prod=1'
+          apr_30_year = rate.parent.css('td').last.text.gsub('%', '')
+        else
+          next
+        end
+      end
+
+      {
+        apr_30_year: apr_30_year,
+        apr_15_year: apr_15_year,
+        apr_5_libor: apr_5_libor
+      }
+    end
+  end
+end
