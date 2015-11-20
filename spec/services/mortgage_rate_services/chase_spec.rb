b3797aee9d4ab76e58a2a559ba66c73d6fc3cd37
Cuong Lam <thanhcuong1990@gmail.com>
2015-11-20 19:39:04 +0700

    Update Chase spec.

--- spec/services/mortgage_rate_services/chase_spec.rb
+++ spec/services/mortgage_rate_services/chase_spec.rb
@@ -5,7 +5,7 @@ describe MortgageRateServices::Chase do
   it 'gets aprs from Chase successfully' do
     VCR.use_cassette("get aprs from Chase") do
       response = MortgageRateServices::Chase.call
-      expect(response).to eq({"apr_30_year"=>0, "apr_15_year"=>0, "apr_5_libor"=>0})
+      expect(response).to eq({"apr_30_year" => 3.937, "apr_15_year" => 3.305, "apr_5_libor" => 3.214})
     end
   end
 end
