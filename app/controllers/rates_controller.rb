b2080342f58459b97edd555fc892f6d268e33fd3
Cuong Vu <cuongvu0103@gmail.com>
2015-11-24 15:36:29 +0700

    #RatesController Switch from primary property to subject property

--- app/controllers/rates_controller.rb
+++ app/controllers/rates_controller.rb
@@ -3,7 +3,7 @@ class RatesController < ApplicationController
     @loan = Loan.find(params[:loan_id])
     rates = []
 
-    if @loan.primary_property.address && zipcode = @loan.primary_property.address.zip
+    if @loan.subject_property.address && zipcode = @loan.subject_property.address.zip
       rates = ZillowService::GetMortgageRates.new(@loan.id, zipcode).call
     end
 
@@ -21,10 +21,10 @@ class RatesController < ApplicationController
     return {} unless @loan.borrower.credit_report
 
     borrower = @loan.borrower
-    primary_property = @loan.primary_property
+    subject_property = @loan.subject_property
     properties = @loan.properties.map do |property|
       {
-        is_primary: primary_property.id == property.id ? 1 : 0,
+        is_primary: subject_property.id == property.id ? 1 : 0,
         liability_payments: property.liability_payments,
         mortgage_payment: property.mortgage_payment,
         other_financing: property.other_financing,
@@ -42,11 +42,11 @@ class RatesController < ApplicationController
       housing_expense: UnderwritingLoanServices::CalculateHousingExpenseRatio.call(@loan),
       sum_liability_payment: borrower.credit_report.sum_liability_payment,
       sum_investment: UnderwritingLoanServices::CalculateDebtToIncome.sum_investment(@loan.rental_properties),
-      primary_liability_payments: primary_property.liability_payments,
-      primary_estimated_mortgage_insurance: primary_property.estimated_mortgage_insurance.to_f,
-      primary_estimated_hazard_insurance: primary_property.estimated_hazard_insurance.to_f,
-      primary_estimated_property_tax: primary_property.estimated_property_tax.to_f,
-      primary_hoa_due: primary_property.hoa_due.to_f,
+      primary_liability_payments: subject_property.liability_payments,
+      primary_estimated_mortgage_insurance: subject_property.estimated_mortgage_insurance.to_f,
+      primary_estimated_hazard_insurance: subject_property.estimated_hazard_insurance.to_f,
+      primary_estimated_property_tax: subject_property.estimated_property_tax.to_f,
+      primary_hoa_due: subject_property.hoa_due.to_f,
       total_income: UnderwritingLoanServices::CalculateTotalIncome.call(@loan),
       rental_income: UnderwritingLoanServices::CalculateRentalIncome.call(@loan),
       current_salary: borrower.current_salary,
