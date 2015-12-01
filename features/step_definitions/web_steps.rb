e39373a3a46c92220c1f7301a21fa4312ed70e36
Cuong Vu <cuongvu0103@gmail.com>
2015-12-01 17:18:02 +0700

    Update cucumber tests for new designed Document.

--- features/step_definitions/web_steps.rb
+++ features/step_definitions/web_steps.rb
@@ -77,10 +77,7 @@ When /^I am at dashboard page$/ do
       And there is a loan_member_user_with_loan_member with the first name "Michael" and the last name "Gifford"
       And there is a property with the id "be69aefe-3946-4b85-9c54-58dd51306b1c" and with the purchase price "1000000" and with the usage "0" and with the is primary "1" and with the is subject "1" and with the loan above
       And there is a address with the street address "81458 Borer Falls" and the street address2 "Apt. 305" and the city "West Emiltown" and the state "Virginia" and the zip "9999" and the property id "be69aefe-3946-4b85-9c54-58dd51306b1c"
-      And there is a lease agreement with the attachment file name "property-document-name" and the property id "be69aefe-3946-4b85-9c54-58dd51306b1c"
-      And there is a first w2 with the borrower above and with the attachment file name "borrower-document-name"
-      And there is a hud estimate with the loan above and with the attachment file name "loan-document-name"
-      And there is a closing disclosure with the closing above and with the attachment file name "closing-document-name"
+      And there is a document with the user above and with the document type "appraisal_report" and with the attachment file name "property-document-name" and with the subjectable type "Property" and the subjectable id "be69aefe-3946-4b85-9c54-58dd51306b1c"
       And there is a loans members association with the loan above and with the loan member above and with the title "sale"
       And there is a loans members association with the title "manager" and with the loan above
       And I login as "testing@man.net" with password "secretpass"
@@ -96,10 +93,7 @@ When /^I am at my loans page$/ do
       And there is a property with the purchase price "1000000" and with the usage "0" and with the is primary "1" and with the is subject "1" and with loan above
       And there is a loan_member_user_with_loan_member with the first name "Michael" and the last name "Gifford"
       And there is a address with the street address "81458 Borer Falls" and the street address2 "Apt. 305" and the city "West Emiltown" and the state "Virginia" and the zip "9999" with the property above
-      And there is a lease agreement with the property above and with the attachment file name "property-document-name"
-      And there is a first w2 with the borrower above and with the attachment file name "borrower-document-name"
-      And there is a hud estimate with the loan above and with the attachment file name "loan-document-name"
-      And there is a closing disclosure with the closing above and with the attachment file name "closing-document-name"
+      And there is a document with the user above and with the document type "appraisal_report" and with the attachment file name "property-document-name" and with the subjectable type "Property" and the subjectable id "be69aefe-3946-4b85-9c54-58dd51306b1c"
       And there is a loans members association with the loan above and with the loan member above and with the title "sale"
       And there is a loans members association with the title "manager" and with the loan above
       And I login as "testing@man.net" with password "secretpass"
