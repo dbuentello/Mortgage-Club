96dc7ac8bfdd53320735d7c341a7f447a8813dc6
Cuong Vu <cuongvu0103@gmail.com>
2015-12-04 17:34:42 +0700

    Update test for SubmitApplicationToLender Service

--- spec/services/submit_application_to_lender_service_spec.rb
+++ spec/services/submit_application_to_lender_service_spec.rb
@@ -6,13 +6,15 @@ describe SubmitApplicationToLenderService do
   let!(:lender_document) { FactoryGirl.create(:lender_document, loan: loan) }
 
   context "valid" do
+    before(:each) { allow(Amazon::GetUrlService).to receive(:call).and_return("http://example.com") }
+
     it "returns true" do
       expect(described_class.new(loan, staff).call).to be_truthy
     end
 
     it "calls LoanMemberMailer with proper params" do
       message_delivery = instance_double(ActionMailer::MessageDelivery)
-      allow(Amazon::GetUrlService).to receive(:call).and_return("http://example.com")
+
       expect(LoanMemberMailer).to receive(:submit_application).with({
         documents_info: [{url: "http://example.com", file_name: lender_document.attachment_file_name}],
         templates_name: [loan.lender.lender_templates.where(is_other: false).last.name],
@@ -20,7 +22,8 @@ describe SubmitApplicationToLenderService do
         lender_email: loan.lender.lock_rate_email,
         loan_member_name: staff.to_s,
         loan_member_email: "#{staff.to_s} <#{staff.email}>",
-        client_name: loan.borrower.user.to_s
+        client_name: loan.borrower.user.to_s,
+        loan_id: loan.id
       }).and_return(message_delivery)
       expect(message_delivery).to receive(:deliver_later)
 
