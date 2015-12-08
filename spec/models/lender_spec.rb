590eaec8ed44844a3e9e9a4398852f9d6fb03979
Linh Nguyen <linhvn09@gmail.com>
2015-12-08 15:53:53 +0700

    validate lender to make sure all fields required

--- spec/models/lender_spec.rb
+++ spec/models/lender_spec.rb
@@ -1,6 +1,6 @@
 require "rails_helper"
 
-describe Lender do
+describe Lender, type: :model do
   it { should have_many(:lender_template_requirements) }
   it { should have_many(:lender_templates) }
   it { should have_many(:loans) }
@@ -8,6 +8,9 @@ describe Lender do
   it { should validate_uniqueness_of(:lock_rate_email) }
   it { should validate_uniqueness_of(:docs_email) }
   it { should validate_uniqueness_of(:contact_email) }
+  it { should validate_presence_of(:contact_name) }
+  it { should validate_presence_of(:contact_phone) }
+  it { should validate_presence_of(:website) }
 
   it "has a valid factory" do
     expect(FactoryGirl.build(:lender)).to be_valid
