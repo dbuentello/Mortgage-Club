9c81a85934d2a51c1630b29b4e09dc8fb16ef145
Cuong Vu <cuongvu0103@gmail.com>
2015-12-09 09:24:24 +0700

    Remove autologin in production

--- app/controllers/users/base_controller.rb
+++ app/controllers/users/base_controller.rb
@@ -1,20 +1,7 @@
 class Users::BaseController < ApplicationController
-  if Rails.env.production?
-    skip_before_action :authenticate_user!
-    before_action :fake_authenticate!
-  else
-    before_action :authenticate_borrower!
-  end
+  before_action :authenticate_borrower!
 
   private
-
-  def fake_authenticate!
-    @current_user = User.where(email: 'borrower@gmail.com').last
-    sign_in(@current_user)
-    unless @current_user.borrower?
-      redirect_to borrower_root_url, alert: "The page does not exist or you don't have permmission to access!"
-    end
-  end
 
   def authenticate_borrower!
     unless current_user.borrower?
