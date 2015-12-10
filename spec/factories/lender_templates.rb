33f95ca90e12562d81cda736843d976269f3b50c
Cuong Vu <cuongvu0103@gmail.com>
2015-12-10 13:13:37 +0700

    Update cucumber test

--- spec/factories/lender_templates.rb
+++ spec/factories/lender_templates.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
   factory :lender_template do |f|
     f.name { "Wholesale Submission Form" } # hard code to test at Cucumber
-    f.description { Faker::Lorem.sentence }
+    f.description { "This is a Wholesale Submission Form" }
     f.is_other {false}
 
     factory :lender_template_with_docusign, parent: :lender_template do |f|
