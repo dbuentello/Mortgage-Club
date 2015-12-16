fbcbd6c44fa49d2398a5f7f5930d03b0012693f8
Cuong Vu <cuongvu0103@gmail.com>
2015-12-16 09:39:36 +0700

    Update test for SubmitApplicationToLenderService

--- spec/services/submit_application_to_lender_service_spec.rb
+++ spec/services/submit_application_to_lender_service_spec.rb
@@ -5,7 +5,7 @@ describe SubmitApplicationToLenderService do
   let(:staff) { FactoryGirl.create(:user) }
   let!(:lender_document) { FactoryGirl.create(:lender_document, loan: loan) }
 
-  context "valid" do
+  context "when params are valid" do
     before(:each) { allow(Amazon::GetUrlService).to receive(:call).and_return("http://example.com") }
 
     it "returns true" do
@@ -29,21 +29,48 @@ describe SubmitApplicationToLenderService do
 
       described_class.new(loan, staff).call
     end
+
+    context "with other template" do
+      let(:second_lender_document) { FactoryGirl.create(:lender_document, loan: loan) }
+      let!(:requirement) { FactoryGirl.create(:lender_template_requirement, lender:  loan.lender, lender_template: second_lender_document.lender_template) }
+
+      it "returns proper templates name" do
+        second_lender_document.lender_template.update(is_other: true)
+        message_delivery = instance_double(ActionMailer::MessageDelivery)
+
+        expect(LoanMemberMailer).to receive(:submit_application).with({
+          documents_info: [
+            {url: "http://example.com", file_name: lender_document.lender_template.name + File.extname(lender_document.attachment_file_name)},
+            {url: "http://example.com", file_name: second_lender_document.description + File.extname(second_lender_document.attachment_file_name)}
+          ],
+          templates_name: loan.lender.lender_templates.where(is_other: false).pluck(:description) << second_lender_document.description,
+          lender_name: loan.lender.name,
+          lender_email: loan.lender.lock_rate_email,
+          loan_member_name: staff.to_s,
+          loan_member_email: "#{staff.to_s} <#{staff.email}>",
+          client_name: loan.borrower.user.to_s,
+          loan_id: loan.id
+        }).and_return(message_delivery)
+        expect(message_delivery).to receive(:deliver_later)
+
+        described_class.new(loan, staff).call
+      end
+    end
   end
 
-  context "missing loan" do
+  context "when loan is missing" do
     it "returns false" do
       expect(described_class.new(nil, staff).call).to be_falsey
     end
   end
 
-  context "missing staff" do
+  context "when staff is missing" do
     it "returns false" do
       expect(described_class.new(loan, nil).call).to be_falsey
     end
   end
 
-  context "missing lender" do
+  context "when lender is missing" do
     it "returns false" do
       loan.lender = nil
       expect(described_class.new(loan, staff).call).to be_falsey
