be1ed65c50e310b6fd25222176518cfd41de824e
Cuong Vu <cuongvu0103@gmail.com>
2015-12-17 18:11:08 +0700

    Authenticate loan in LoanMember dashboard

--- app/controllers/loan_members/base_controller.rb
+++ app/controllers/loan_members/base_controller.rb
@@ -2,6 +2,12 @@ class LoanMembers::BaseController < ApplicationController
   layout 'loan_member'
   before_action :authenticate_loan_member!
 
+  def authenticate_loan!
+    if @loan && !current_user.loan_member.handle_this_loan?(@loan)
+      redirect_to unauthenticated_root_path, alert: "The page does not exist or you don't have permmission to access!"
+    end
+  end
+
   private
 
   def authenticate_loan_member!
