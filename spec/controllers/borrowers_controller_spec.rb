b82243468a21b8547fd3d8ac1cfb7e53f09604b3
Linh Nguyen <linhvn09@gmail.com>
2015-12-21 14:40:35 +0700

    update test case for borrowers controller

--- spec/controllers/borrowers_controller_spec.rb
+++ spec/controllers/borrowers_controller_spec.rb
@@ -20,6 +20,7 @@ RSpec.describe Users::BorrowersController do
             dob: "1969-12-31T17:00:00.000Z", ssn: "233-43-4444", phone: "(437) 678-4933",
             years_in_school: "12", marital_status: "", dependent_count: "0", self_employed: "false"}
       }
+
       @secondary_borrower ={current_address: {street_address: "23 Park Avenue", street_address2: "",
         city: "New York", state: "NY", zip: "10016",
         full_text: "23 Park Avenue, New York, NY, United States"},
@@ -30,15 +31,65 @@ RSpec.describe Users::BorrowersController do
         phone: "(342) 289-9555", years_in_school: "12", marital_status: "", dependent_count: "0", self_employed: "false"}}
     end
 
-    context "with valid data" do
-      it "calls BorrowerServices::RemoveSecondaryBorrower when there is no secondary borrower" do
-        expect(BorrowerServices::RemoveSecondaryBorrower).to receive(:call)
-        put :update, id: borrower.id, borrower: @borrower_params, loan_id: loan.id
+    context "when applying as an individual" do
+      context "with valid params" do
+        it "calls BorrowerForm and returns true" do
+          expect_any_instance_of(BorrowerForm).to receive(:save).and_return(true)
+          put :update, id: borrower.id, borrower: @borrower_params, loan_id: loan.id
+        end
+
+        it "returns status 200" do
+          put :update, id: borrower.id, borrower: @borrower_params, loan_id: loan.id
+          expect(response.status).to eq 200
+        end
+
+        it "calls RemoveSecondaryBorrower service" do
+          expect(BorrowerServices::RemoveSecondaryBorrower).to receive(:call)
+          put :update, id: borrower.id, borrower: @borrower_params, loan_id: loan.id
+        end
       end
 
-      it "calls BorrowerServices::AssignSecondaryBorrowerToLoan there is secondary borrower" do
-        expect_any_instance_of(BorrowerServices::AssignSecondaryBorrowerToLoan).to receive(:call)
-        put :update, id: borrower.id, borrower: @borrower_params, has_secondary_borrower: "true", loan_id: loan.id, secondary_borrower: @secondary_borrower
+      context "with invalid params" do
+        before(:each) do
+          allow_any_instance_of(BorrowerForm).to receive(:save).and_return(false)
+          put :update, id: borrower.id, borrower: @borrower_params, loan_id: loan.id
+        end
+
+        it "calls BorrowerForm.save and returns false" do
+          expect_any_instance_of(BorrowerForm).to receive(:save).and_return(false)
+          put :update, id: borrower.id, borrower: @borrower_params, loan_id: loan.id
+        end
+
+        it "retuns status 500" do
+          expect(response.status).to eq(500)
+        end
+      end
+    end
+
+    context "when applying as secondary borrower" do
+      context "with valid params both borrower" do
+
+        it "calls AssignSecondaryBorrowerToLoan service" do
+          expect_any_instance_of(BorrowerServices::AssignSecondaryBorrowerToLoan).to receive(:call)
+          put :update, id: borrower.id, borrower: @borrower_params, has_secondary_borrower: "true", loan_id: loan.id, secondary_borrower: @secondary_borrower
+        end
+
+        it "calls BorrowerForm save" do
+          expect_any_instance_of(BorrowerForm).to receive(:save)
+          put :update, id: borrower.id, borrower: @borrower_params, has_secondary_borrower: "true", loan_id: loan.id, secondary_borrower: @secondary_borrower
+        end
+      end
+
+      context "with invalid params" do
+        before(:each) do
+          allow_any_instance_of(BorrowerForm).to receive(:save).and_return(false)
+          put :update, id: borrower.id, borrower: @borrower_params, has_secondary_borrower: "true", loan_id: loan.id, secondary_borrower: @secondary_borrower
+        end
+
+        it "calls BorrowerForm.save and returns false" do
+          expect_any_instance_of(BorrowerForm).to receive(:save).and_return(false)
+          put :update, id: borrower.id, borrower: @borrower_params, has_secondary_borrower: "true", loan_id: loan.id, secondary_borrower: @secondary_borrower
+        end
       end
     end
   end
