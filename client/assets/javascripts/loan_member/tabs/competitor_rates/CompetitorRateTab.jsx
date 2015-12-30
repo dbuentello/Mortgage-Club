387c677adeb26d149c23f754dad488eceed6f919
Linh Nguyen <linhvn09@gmail.com>
2015-12-30 17:51:09 +0700

    [WIP]-display loan information with formatted total fee

--- client/assets/javascripts/loan_member/tabs/competitor_rates/CompetitorRateTab.jsx
+++ client/assets/javascripts/loan_member/tabs/competitor_rates/CompetitorRateTab.jsx
@@ -7,12 +7,12 @@ module.exports = React.createClass({
   mixins: [TextFormatMixin],
 
   renderTable: function(){
-    var this_obj = this;
+    var thisObject = this;
     return _.map(this.props.competitorRates, function(competitor_rate){
       return (
         <div className="competitor-name">
           <h2>
-            { competitor_rate.down_rate_value*100 }%
+            Down payment { competitor_rate.down_payment_percentage*100 }%
           </h2>
           <div className="row">
             <div className="col-md-2">
@@ -24,16 +24,16 @@ module.exports = React.createClass({
                   <div className="col-md-1">
                     <div className="row">
                       <div className="col-md-12">{rate.apr}</div>
-                      <div className="col-md-12">{this_obj.formatCurrency(rate.total_fee, "$")}</div>
+                      <div className="col-md-12">{this.formatCurrency(rate.total_fee, "$")}</div>
                     </div>
                   </div>
                   )
-              })
+              }.bind(this))
             }
           </div>
         </div>
         );
-    });
+    }.bind(this));
   },
   render: function(){
     console.log(this.props.competitor_rates);
