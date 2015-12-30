5527237ee0cac1e82874d13d3cd327c3b1f21249
Linh Nguyen <linhvn09@gmail.com>
2015-12-30 15:43:38 +0700

    change the structure of competitor rates object

--- app/controllers/loan_members/dashboard_controller.rb
+++ app/controllers/loan_members/dashboard_controller.rb
@@ -34,13 +34,13 @@ class LoanMembers::DashboardController < LoanMembers::BaseController
         down_rate_value: 0.2,
         lender_name: "google",
         rates: [
-            {"30_year_fixed": Random.rand(40)*0.01},
-            {"20_year_fixed": Random.rand(40)*0.01},
-            {"15_year_fixed": Random.rand(40)*0.01},
-            {"10_year_fixed": Random.rand(40)*0.01},
-            {"7_1_arm": Random.rand(40)*0.01},
-            {"5_1_arm": Random.rand(40)*0.01},
-            {"3_1_arm": Random.rand(40)*0.01}
+            {name: "30_year_fixed", rate: Random.rand(40)*0.01},
+            {name: "20_year_fixed", rate: Random.rand(40)*0.01},
+            {name: "15_year_fixed", rate: Random.rand(40)*0.01},
+            {name: "10_year_fixed", rate: Random.rand(40)*0.01},
+            {name: "7_1_arm", rate: Random.rand(40)*0.01},
+            {name: "5_1_arm", rate: Random.rand(40)*0.01},
+            {name: "3_1_arm", rate: Random.rand(40)*0.01}
           ]
         }
     ]
