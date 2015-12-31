726bcc15a9b9cc38e0e3f5d547c4ec9d347cf764
Linh Nguyen <linhvn09@gmail.com>
2015-12-31 14:21:15 +0700

    update rate comparison test file

--- spec/models/rate_comparison_spec.rb
+++ spec/models/rate_comparison_spec.rb
@@ -2,7 +2,7 @@ require "rails_helper"
 
 describe RateComparison do
   it { should validate_presence_of(:loan_id) }
-  it { should validate_presence_of(:lender_name) }
+  it { should validate_presence_of(:competitor_name) }
 
   it "has a valid factory" do
     expect(FactoryGirl.build(:rate_comparison)).to be_valid
