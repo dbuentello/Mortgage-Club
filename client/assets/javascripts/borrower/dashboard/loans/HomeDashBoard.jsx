8f2ae7ebc79458a1c1e92e12ac23a8110365dd2e
Linh Nguyen <linhvn09@gmail.com>
2016-01-04 15:21:15 +0700

    restyle borrower dashboard

--- client/assets/javascripts/borrower/dashboard/loans/HomeDashBoard.jsx
+++ client/assets/javascripts/borrower/dashboard/loans/HomeDashBoard.jsx
@@ -35,11 +35,10 @@ var HomeDashBoard = React.createClass({
     }
 
     return (
-      <div class="loanPart signupPart content">
-        <div class="container">
-          <ul class="nav nav-tabs mortgageTabs" role="tablist">
-            <li role="presentation" class="active"><a href="#loans" aria-controls="loans" role="tab" data-toggle="tab" class="text-capitalize">loan</a></li>
-            <li role="presentation"><a href="#referrals" aria-controls="referrals" role="tab" data-toggle="tab" class="text-capitalize">referrals</a></li>
+     <div className="container borrower-dashboard">
+          <ul className="nav nav-tabs mortgageTabs" role="tablist">
+            <li role="presentation" className="active"><a href="#loans" aria-controls="loans" role="tab" data-toggle="tab" className="text-capitalize">loan</a></li>
+            <li role="presentation"><a href="#referrals" aria-controls="referrals" role="tab" data-toggle="tab" className="text-capitalize">referrals</a></li>
             <li role="presentation"><a href="#settings" aria-controls="settings" role="tab" data-toggle="tab" class="text-capitalize">settings</a></li>
           </ul>
           <div className='tabs'>
@@ -57,7 +56,6 @@ var HomeDashBoard = React.createClass({
           </div>
 
         </div>
-      </div>
     )
   }
 });
