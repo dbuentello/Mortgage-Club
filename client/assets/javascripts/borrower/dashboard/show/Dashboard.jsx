a1cf4c17afb635e6441a8e44c917ef3b6f78ae77
TangNV <nvtang.khtn@gmail.com>
2016-01-05 16:55:19 +0700

    apply UI for header - dashboard

--- client/assets/javascripts/borrower/dashboard/show/Dashboard.jsx
+++ client/assets/javascripts/borrower/dashboard/show/Dashboard.jsx
@@ -13,7 +13,6 @@ var PropertyTab = require('./tabs/PropertyTab');
 var LoanTab = require('./tabs/LoanTab');
 var ClosingTab = require('./tabs/ClosingTab');
 var RelationshipManager = require('./RelationshipManager');
-
 var Dashboard = React.createClass({
   mixins: [ObjectHelperMixin, TextFormatMixin, FlashHandler],
 
@@ -61,80 +60,82 @@ var Dashboard = React.createClass({
     var checklists = this.props.bootstrapData.checklists;
 
     return (
-      <div className='dashboard content'>
-        <div className='dashboard-header row mbl'>
-          <div className='col-xs-offset-2 col-xs-6 ptl'>
-            <h3 className='typeBold'>{address}</h3>
-            {
-              loan.amount
-              ?
-                <h4>{this.formatCurrency(loan.amount, '$')} {loan.amortization_type} {property.usage_name} {loan.purpose_titleize} Loan</h4>
-              :
-                null
-            }
+      <div className="content dashboard">
+        <div className="container">
+          <div className='row dashboard-top'>
+            <div className='col-md-9'>
+              <h3 className='typeBold'>{address}</h3>
+              {
+                loan.amount
+                ?
+                  <p>{this.formatCurrency(loan.amount, '$')} {loan.amortization_type} {property.usage_name} {loan.purpose_titleize} Loan</p>
+                :
+                  null
+              }
+            </div>
+            <div className='col-md-3'>
+              <a className='btn edit-btn' href={'/loans/' + loan.id + '/edit'}><i className="iconPencil mrs"/>Edit</a>
+              <ModalLink
+                id="deleteLoan"
+                icon="iconTrash mrs"
+                name="Delete"
+                class="btn delete-btn"
+                title="Confirmation"
+                body="Are you sure to destroy this loan?"
+                yesCallback={this.destroyLoan}
+              />
+            </div>
           </div>
-          <div className='col-xs-4 ptl'>
-            <a className='btn edit-btn' href={'/loans/' + loan.id + '/edit'}><i className="iconPencil mrs"/>Edit</a>
-            <ModalLink
-              id="deleteLoan"
-              icon="iconTrash mrs"
-              name="Delete"
-              class="btn delete-btn"
-              title="Confirmation"
-              body="Are you sure to destroy this loan?"
-              yesCallback={this.destroyLoan}
-            />
-          </div>
-        </div>
 
-        <div className='dashboard-tabs phxl backgroundLowlight'>
-          <ul className="nav nav-tabs" role="tablist">
-            <li role="presentation" className="active">
-              <a href="#overview" aria-controls="overview" role="tab" data-toggle="tab">Overview</a>
-            </li>
-            <li role="presentation">
-              <a href="#property" aria-controls="property" role="tab" data-toggle="tab">Property</a>
-            </li>
-            <li role="presentation">
-              <a href="#borrower" aria-controls="borrower" role="tab" data-toggle="tab">Borrower</a>
-            </li>
-            <li role="presentation">
-              <a href="#loan" aria-controls="loan" role="tab" data-toggle="tab">Loan</a>
-            </li>
-            <li role="presentation">
-              <a href="#closing" aria-controls="closing" role="tab" data-toggle="tab">Closing</a>
-            </li>
-            <li role="presentation">
-              <a href="#contacts" aria-controls="contacts" role="tab" data-toggle="tab">Contacts</a>
-            </li>
-          </ul>
+          <div className='dashboard-tabs phxl backgroundLowlight'>
+            <ul className="nav nav-tabs" role="tablist">
+              <li role="presentation" className="active">
+                <a href="#overview" aria-controls="overview" role="tab" data-toggle="tab">Overview</a>
+              </li>
+              <li role="presentation">
+                <a href="#property" aria-controls="property" role="tab" data-toggle="tab">Property</a>
+              </li>
+              <li role="presentation">
+                <a href="#borrower" aria-controls="borrower" role="tab" data-toggle="tab">Borrower</a>
+              </li>
+              <li role="presentation">
+                <a href="#loan" aria-controls="loan" role="tab" data-toggle="tab">Loan</a>
+              </li>
+              <li role="presentation">
+                <a href="#closing" aria-controls="closing" role="tab" data-toggle="tab">Closing</a>
+              </li>
+              <li role="presentation">
+                <a href="#contacts" aria-controls="contacts" role="tab" data-toggle="tab">Contacts</a>
+              </li>
+            </ul>
 
-          <div className='tabs row'>
-            <div className='left-side col-xs-8'>
-              <div className="tab-content">
-                <div role="tabpanel" className="tab-pane fade in active" id="overview">
-                  <OverviewTab loan={loan} borrower={loan.borrower} checklists={checklists} />
-                </div>
-                <div role="tabpanel" className="tab-pane fade" id="property">
-                  <PropertyTab propertyDocuments={propertyDocuments}></PropertyTab>
-                </div>
-                <div role="tabpanel" className="tab-pane fade" id="borrower">
-                  <BorrowerTab borrowerDocuments={borrowerDocuments}></BorrowerTab>
-                </div>
-                <div role="tabpanel" className="tab-pane fade" id="loan">
-                  <LoanTab loanDocuments={loanDocuments}></LoanTab>
-                </div>
-                <div role="tabpanel" className="tab-pane fade" id="closing">
-                  <ClosingTab closingDocuments={closingDocuments}></ClosingTab>
-                </div>
-                <div role="tabpanel" className="tab-pane fade" id="contacts">
-                  <ContactTab contactList={contactList}></ContactTab>
+            <div className='tabs row'>
+              <div className='left-side col-xs-8'>
+                <div className="tab-content">
+                  <div role="tabpanel" className="tab-pane fade in active" id="overview">
+                    <OverviewTab loan={loan} borrower={loan.borrower} checklists={checklists} />
+                  </div>
+                  <div role="tabpanel" className="tab-pane fade" id="property">
+                    <PropertyTab propertyDocuments={propertyDocuments}></PropertyTab>
+                  </div>
+                  <div role="tabpanel" className="tab-pane fade" id="borrower">
+                    <BorrowerTab borrowerDocuments={borrowerDocuments}></BorrowerTab>
+                  </div>
+                  <div role="tabpanel" className="tab-pane fade" id="loan">
+                    <LoanTab loanDocuments={loanDocuments}></LoanTab>
+                  </div>
+                  <div role="tabpanel" className="tab-pane fade" id="closing">
+                    <ClosingTab closingDocuments={closingDocuments}></ClosingTab>
+                  </div>
+                  <div role="tabpanel" className="tab-pane fade" id="contacts">
+                    <ContactTab contactList={contactList}></ContactTab>
+                  </div>
                 </div>
               </div>
-            </div>
 
-            <div className='right-side col-xs-4'>
-              <RelationshipManager Manager={manager} LoanActivities={this.props.bootstrapData.loan_activities} ActiveTab={this.state.activeTab}></RelationshipManager>
+              <div className='right-side col-xs-4'>
+                <RelationshipManager Manager={manager} LoanActivities={this.props.bootstrapData.loan_activities} ActiveTab={this.state.activeTab}></RelationshipManager>
+              </div>
             </div>
           </div>
         </div>
