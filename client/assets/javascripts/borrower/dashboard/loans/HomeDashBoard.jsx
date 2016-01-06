60a522e89e02650fa131dc2c5f6577c35188ad28
Cuong Vu <cuongvu0103@gmail.com>
2016-01-06 18:03:01 +0700

    Update html + css code for LoanListPage

--- client/assets/javascripts/borrower/dashboard/loans/HomeDashBoard.jsx
+++ client/assets/javascripts/borrower/dashboard/loans/HomeDashBoard.jsx
@@ -36,7 +36,7 @@ var HomeDashBoard = React.createClass({
     }
 
     return (
-      <div className="content">
+      <div className="content loans-part">
         <div className="container borrower-dashboard">
           <ul className="nav nav-tabs mortgageTabs" role="tablist">
             <li role="presentation" className="active"><a href="#loans" aria-controls="loans" role="tab" data-toggle="tab" className="text-capitalize">Loans</a></li>
