9df545df0b441049c0847ee71d82983f7a2d8f62
Linh Nguyen <linhvn09@gmail.com>
2016-01-07 10:13:49 +0700

    [WIP] -restyle the List result item

--- client/assets/javascripts/borrower/loans/rates/List.jsx
+++ client/assets/javascripts/borrower/loans/rates/List.jsx
@@ -8,6 +8,10 @@ var List = React.createClass({
   propTypes: {
     rates: React.PropTypes.array
   },
+  toggleHandler: function(event){
+    $(event.target).prev().slideToggle(500);
+    $(event.target).find('span').toggleClass('up-state');
+  },
 
   render: function() {
     return (
@@ -40,6 +44,68 @@ var List = React.createClass({
                     </div>
                   </div>
                 </div>
+                <div className="row">
+                <div className="col-md-6">
+                  <h4>Product details</h4>
+                  <div className="row">
+                    <div className="col-xs-6">
+                      <p>Product type</p>
+                      <p>Rate</p>
+                      <p>APR</p>
+                      <p>Loan amount</p>
+                      <p>Down payment</p>
+                    </div>
+                    <div className="col-xs-6">
+                      <p>30-year fixed</p>
+                      <p>3.875%</p>
+                      <p>3.879%</p>
+                      <p>$350,000</p>
+                      <p>$90,000 (20%)</p>
+                    </div>
+                  </div>
+
+                    <h4>Lender fees</h4>
+                    <div className="row">
+                      <div className="col-xs-6">
+                        <p>Loan original fee</p>
+                        <p>Lender credit</p>
+                        <p>Appraisal fee</p>
+                        <p>Total estimated lender fees</p>
+                      </div>
+                      <div className="col-xs-6">
+                        <p>$1,900</p>
+                        <p>($2,191)</p>
+                        <p>$450</p>
+                        <p>$159</p>
+                      </div>
+                    </div>
+                  </div>
+
+                <div className="col-md-6">
+                  <h4>Monthly payment details</h4>
+                  <div className="row">
+                    <div className="col-xs-9">
+                      <p>Principle and interest</p>
+                      <p>Estimated mortgage insurance</p>
+                      <p>Estimated property tax</p>
+                      <p>Estimated homeowners payment</p>
+                      <p>Total estimated monthly payment</p>
+                    </div>
+                    <div className="col-xs-3">
+                      <p>$1,646</p>
+                      <p>0</p>
+                      <p>333</p>
+                      <p>$80</p>
+                      <p>$2,059</p>
+                    </div>
+                  </div>
+                  <p className="note">Of all 30-year fixed mortgages on Mortgage Club that you’ve qualified for, this one has the lowest rate and APR.</p>
+                </div>
+
+              </div>
+                <div className="board-content-toggle" onClick={this.toggleHandler}>
+                  <span className="glyphicon glyphicon-menu-down"></span>
+                </div>
                 <div className='col-sm-3'>
                   <div className='typeBold'>{rate.lender_name}</div>
                   Logo
@@ -48,14 +114,6 @@ var List = React.createClass({
                   </div>
                 </div>
                 <div className='col-sm-6'>
-                  {rate.product}<br/>
-                  {this.commafy(rate.apr, 3)}% APR
-                  <span className='typeLowlight mlm'>Monthly Payment: </span>
-                  {this.formatCurrency(rate.monthly_payment, '$')}<br/>
-                  <span className='typeLowlight'>Rate: </span>{this.commafy(rate.interest_rate * 100, 3)}%
-                  <span className='typeLowlight mlm'>Total Closing Cost: </span>
-                  {this.formatCurrency(rate.total_closing_cost, '$')}
-                  <br/>
                   <span className='typeLowlight mlm'>Lender credit: </span>
                   {rate.lender_credit ? this.formatCurrency(rate.lender_credit, '$') : "$0"}
                   <br/>
