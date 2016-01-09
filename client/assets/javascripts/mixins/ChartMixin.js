841b96cc63807a2b24bddbda30c4f7d564355e40
TangNV <nvtang.khtn@gmail.com>
2016-01-09 13:47:35 +0700

    update chartmixin part 2

--- client/assets/javascripts/mixins/ChartMixin.js
+++ client/assets/javascripts/mixins/ChartMixin.js
@@ -134,14 +134,14 @@ var ChartMixin = {
 
     var chartWidth = chartContainer.width();
     var chartHeight = 320;
-    var marginLeft = 2;
     var marginRight = 45;
+    var marginBottom = 30;
 
     var xScale = d3.scale.linear()
-      .range([marginLeft, chartWidth - marginRight]).domain([0, numOfMonths]);
+      .range([0, chartWidth - marginRight]).domain([0, numOfMonths]);
 
     var yScale = d3.scale.linear()
-      .range([0, chartHeight - 30]).domain([loanAmount, 0]);
+      .range([0, chartHeight - marginBottom]).domain([loanAmount, 0]);
 
     var xAxis = d3.svg.axis().scale(xScale)
       .tickValues([0, numOfMonths / 3, numOfMonths / 3 * 2, numOfMonths])
@@ -184,12 +184,12 @@ var ChartMixin = {
       .attr('height', chartHeight);
 
     svg.append('g')
-      .attr('transform', 'translate(' + (marginLeft - 2) + ',' + (chartHeight - 30) + ')')
+      .attr('transform', 'translate(0,' + (chartHeight - marginBottom) + ')')
       .attr('class', 'axis x-axis')
       .call(xAxis);
 
     svg.append('g')
-      .attr('transform', 'translate(' + (marginLeft) + ',0)')
+      .attr('transform', 'translate(2, 0)')
       .attr('class', 'axis y-axis')
       .call(yAxis);
 
@@ -208,17 +208,18 @@ var ChartMixin = {
         .attr('d', line(allData[0][i]));
     }
 
-    // svg.append("path") // this is the black vertical line to follow mouse
-    //   .attr("class","mouseLine")
-    //   .style("stroke","black")
-    //   .style("stroke-width", "1px")
-    //   .style("opacity", "1");
+    svg.append("path") // this is the black vertical line to follow mouse
+      .attr("class","mouseLine")
+      .style("stroke","black")
+      .style("stroke-width", "1px")
+      .style("opacity", "1");
 
-    // var svgRect = svg.append('svg:rect') // append a rect to catch mouse movements on canvas
-    //   .attr('width', chartWidth) // can't catch mouse events on a g element
-    //   .attr('height', chartHeight)
-    //   .attr('fill', 'none')
-    //   .attr('pointer-events', 'all');
+    var svgRect = svg.append('svg:rect') // append a rect to catch mouse movements on canvas
+      .attr('width', chartWidth - marginRight) // can't catch mouse events on a g element
+      .attr('height', chartHeight - marginBottom)
+      .attr('fill', 'none')
+      .attr('pointer-events', 'all');
+
 
     // svg.selectAll(".line-graph")
     //   .append("circle") // add a circle to follow along path
@@ -230,49 +231,55 @@ var ChartMixin = {
 
     // var bisect = d3.bisector(function(d) { return d.month; }).right;
 
-    // svgRect.on('mouseout', function(){
-    //   d3.select(".mouse-circle")
-    //     .style("stroke-width", "0px");
-    // });
+    svgRect.on('mouseout', function(){
+      d3.select(".mouse-circle")
+        .style("stroke-width", "0px");
 
-    // svgRect.on('mouseover', function(){
-    //   d3.select(".mouse-circle")
-    //     .style("stroke-width", "1px");
-    // });
+      svg.select(".mouseLine")
+        .style("stroke-width", "0px");
+    });
+
+    svgRect.on('mouseover', function(){
+      d3.select(".mouse-circle")
+        .style("stroke-width", "1px");
+
+      svg.select(".mouseLine")
+        .style("stroke-width", "1px");
+    });
 
     // var x = d3.time.scale().range([0, chartWidth]);
 
-    // var y = d3.scale.linear().range([chartHeight, 0]);
+    var y = d3.scale.linear().range([chartHeight, 0]);
 
-    // svgRect.on('mousemove', function() { // mouse moving over canvas
-    //   d3.select(".mouseLine")
-    //   .attr("d", function(){
-    //     var yRange = y.range(); // range of y axis
-    //     var xCoor = d3.mouse(this)[0]; // mouse position in x
-    //     var xDate = x.invert(xCoor); // date corresponding to mouse x
-    //     d3.selectAll('.mouse-circle') // for each circle group
-    //       .each(function(d, i){
+    svgRect.on('mousemove', function() { // mouse moving over canvas
+      svg.select(".mouseLine")
+      .attr("d", function(){
+        var yRange = y.range(); // range of y axis
+        var xCoor = d3.mouse(this)[0]; // mouse position in x
+        // var xDate = x.invert(xCoor); // date corresponding to mouse x
+        // d3.selectAll('.mouse-circle') // for each circle group
+        //   .each(function(d, i){
 
-    //         // var rightIdx = bisect(data[1].values, xDate);
-    //         // console.log(rightIdx); // find date in data that right off mouse
-    //         // var interSect = this.getLineIntersection(xCoor,  // get the intersection of our vertical line and the data line
-    //         //   yRange[0],
-    //         //   xCoor,
-    //         //   yRange[1],
-    //         //   x(data[i].values[rightIdx-1].month),
-    //         //   y(data[i].values[rightIdx-1].amount),
-    //         //   x(data[i].values[rightIdx].month),
-    //         //   y(data[i].values[rightIdx].amount));
+        //     // var rightIdx = bisect(data[1].values, xDate);
+        //     // console.log(rightIdx); // find date in data that right off mouse
+        //     // var interSect = this.getLineIntersection(xCoor,  // get the intersection of our vertical line and the data line
+        //     //   yRange[0],
+        //     //   xCoor,
+        //     //   yRange[1],
+        //     //   x(data[i].values[rightIdx-1].month),
+        //     //   y(data[i].values[rightIdx-1].amount),
+        //     //   x(data[i].values[rightIdx].month),
+        //     //   y(data[i].values[rightIdx].amount));
 
-    //         // d3.select(this)
-    //         //   .attr('transform', 'translate(' + xCoor + ',' + yRange[1] + i * 100 + ')');
+        //     // d3.select(this)
+        //     //   .attr('transform', 'translate(' + xCoor + ',' + yRange[1] + i * 100 + ')');
 
-    //         // d3.select(this.children[1]) // write coordinates out
-    //         //   .text(xDate.toLocaleDateString() + "," + y.invert(interSect.y).toFixed(0));
-    //       });
-    //     return "M"+ xCoor +"," + yRange[0] + "L" + xCoor + "," + yRange[1]; // position vertical line
-    //   });
-    // });
+        //     // d3.select(this.children[1]) // write coordinates out
+        //     //   .text(xDate.toLocaleDateString() + "," + y.invert(interSect.y).toFixed(0));
+        //   });
+        return "M"+ xCoor +"," + yRange[0] + "L" + xCoor + "," + yRange[1]; // position vertical line
+      });
+    });
   }
 }
 module.exports = ChartMixin;
