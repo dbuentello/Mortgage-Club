dfffdc79ce82044825d1f46689dc9e09d71628b2
TangNV <nvtang.khtn@gmail.com>
2016-01-11 10:21:29 +0700

    init Q&A managements controller

---
+++ app/controllers/admins/loan_qa_managements_controller.rb
@@ -0,0 +1,22 @@
+class Admins::LoanQaManagementsController < Admins::BaseController
+
+  def index
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def create
+
+  end
+
+  def destroy
+
+  end
+end
