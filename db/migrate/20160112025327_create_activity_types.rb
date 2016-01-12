4a041a434045bd553ed00d00573c6767dba9eed5
TangNV <nvtang.khtn@gmail.com>
2016-01-12 15:47:48 +0700

    convert id int to id uuid

--- db/migrate/20160112025327_create_activity_types.rb
+++ db/migrate/20160112025327_create_activity_types.rb
@@ -1,6 +1,6 @@
 class CreateActivityTypes < ActiveRecord::Migration
   def change
-    create_table :activity_types do |t|
+    create_table :activity_types, id: :uuid do |t|
       t.text :label
       t.text :type_name_mapping, array: true, default: []
 
