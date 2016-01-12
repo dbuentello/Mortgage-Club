287386c935eb03a2ddce4a84464859c801e52631
TangNV <nvtang.khtn@gmail.com>
2016-01-12 10:14:35 +0700

    init activity type presenter

---
+++ app/presenters/admins/loan_activity_types_presenter.rb
@@ -0,0 +1,17 @@
+class Admins::LoanActivityTypesPresenter
+  def initialize(activity_types)
+    @activity_types = activity_types
+  end
+
+  def show
+    @activity_types.as_json(json_options)
+  end
+
+  private
+
+  def json_options
+    {
+      only: [ :id, :type, :type_name_mapping ]
+    }
+  end
+end
