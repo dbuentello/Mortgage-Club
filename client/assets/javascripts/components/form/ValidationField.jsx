3dbcfa851154152aa26a34f0cd9f752b097933d0
Tang Nguyen <nvtang.khtn@gmail.com>
2016-01-14 14:10:37 +0700

    update validation field

--- client/assets/javascripts/components/form/ValidationField.jsx
+++ client/assets/javascripts/components/form/ValidationField.jsx
@@ -7,16 +7,20 @@ var ValidationField = React.createClass({
       if(this.props.activateRequiredField === true)
       {
         if(this.props.value === null || this.props.value === "" || this.props.value === undefined){
-          $("#" + this.props.id).tooltip({
-            title: this.props.title,
-            placement: "bottom"
-          }).tooltip('show');
+          if($("#" + this.props.id).attr("aria-describedby") === undefined){
+            $("#" + this.props.id).tooltip({
+              title: this.props.title,
+              placement: "bottom",
+              trigger: "manual"
+            }).tooltip('show');
+          }
         }
         else{
           $("#" + this.props.id).tooltip('destroy');
         }
       }
       else{
+        console.dir('destroy')
         $("#" + this.props.id).tooltip('destroy');
       }
       return (
