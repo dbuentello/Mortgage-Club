de03e5efc7800e7c8f3ac50153ffa49ff1a0f928
Tang Nguyen <nvtang.khtn@gmail.com>
2016-01-18 10:31:03 +0700

    update property tab service

--- app/services/completed_loan_services/tab_property.rb
+++ app/services/completed_loan_services/tab_property.rb
@@ -1,12 +1,52 @@
 module CompletedLoanServices
-  class TabProperty
+  class TabProperty < Base
     def self.call(loan)
-      loan.properties.size > 0 && loan.subject_property && loan.subject_property.completed? && purpose_completed?(loan)
+      @loan = loan
+      return false unless property_completed?
+      return false unless purpose_completed?
+
+      true
     end
 
-    def self.purpose_completed?(loan)
-      loan.purpose.present? && loan.subject_property && (loan.purchase? && loan.subject_property.purchase_price.present? ||
-        loan.refinance? && loan.subject_property.refinance_completed?)
+    def self.property_completed?
+      return false unless @loan.properties.size > 0
+      return false unless @loan.subject_property
+      return false unless subject_property_completed?
+
+      true
+    end
+
+    def self.subject_property_completed?
+      property = @loan.subject_property
+
+      return false unless property.address.present?
+      return false unless address_completed?(property.address)
+      return false unless property.usage.present?
+
+      true
+    end
+
+    def self.purpose_completed?
+      return false unless @loan.purpose.present?
+
+      if @loan.purchase?
+        return false unless @loan.subject_property.purchase_price.present?
+      end
+
+      if @loan.refinance?
+        return false unless refinance_completed?
+      end
+
+      true
+    end
+
+    def refinance_completed?
+      property = @loan.subject_property
+
+      return false unless property.original_purchase_price.present?
+      return false unless property.original_purchase_year.present?
+
+      true
     end
   end
 end
