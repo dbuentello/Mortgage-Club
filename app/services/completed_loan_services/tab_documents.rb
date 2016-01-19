fb7de32c36e92f2d52b7c99638e27ecaedb88b37
Tang Nguyen <nvtang.khtn@gmail.com>
2016-01-19 11:39:54 +0700

    refactor tab document services

--- app/services/completed_loan_services/tab_documents.rb
+++ app/services/completed_loan_services/tab_documents.rb
@@ -2,20 +2,6 @@ module CompletedLoanServices
   class TabDocuments
     attr_accessor :borrower, :secondary_borrower
 
-    DOCUMENTS_BORROWER_SELF_EMPLOYED = %w(first_personal_tax_return second_personal_tax_return
-                                        first_business_tax_return second_business_tax_return
-                                        first_bank_statement second_bank_statement)
-
-    DOCUMENTS_BORROWER_SELF_EMPLOYED_TAXES_JOINLY = %w(first_business_tax_return second_business_tax_return
-                                                          first_bank_statement second_bank_statement)
-
-    DOCUMENTS_BORROWER_NOT_SELF_EMPLOYED = %w(first_w2 second_w2 first_paystub second_paystub
-                                            first_federal_tax_return second_federal_tax_return
-                                            first_bank_statement second_bank_statement)
-
-    DOCUMENTS_BORROWER_NOT_SELF_EMPLOYED_TAXES_JOINLY = %w(first_w2 second_w2 first_paystub second_paystub
-                                                          first_bank_statement second_bank_statement)
-
     def initialize(args)
       @borrower = args[:borrower]
       @secondary_borrower = args[:secondary_borrower]
@@ -27,7 +13,7 @@ module CompletedLoanServices
     end
 
     def borrower_documents_completed?
-      required_documents = borrower.self_employed ? DOCUMENTS_BORROWER_SELF_EMPLOYED : DOCUMENTS_BORROWER_NOT_SELF_EMPLOYED
+      required_documents = borrower.self_employed ? Document::BORROWER_SELF_EMPLOYED : Document::BORROWER_NOT_SELF_EMPLOYED
 
       (required_documents - borrower.documents.pluck(:document_type)).empty?
     end
@@ -37,13 +23,13 @@ module CompletedLoanServices
     end
 
     def docoments_self_employed_completed?
-      required_documents = borrower.is_file_taxes_jointly ? DOCUMENTS_BORROWER_SELF_EMPLOYED_TAXES_JOINLY : DOCUMENTS_BORROWER_SELF_EMPLOYED
+      required_documents = borrower.is_file_taxes_jointly ? Document::BORROWER_SELF_EMPLOYED_TAXES_JOINLY : Document::BORROWER_SELF_EMPLOYED
 
       (required_documents - borrower.documents.pluck(:document_type)).empty?
     end
 
     def docoments_not_self_employed_completed?
-      required_documents = borrower.is_file_taxes_jointly ? DOCUMENTS_BORROWER_NOT_SELF_EMPLOYED_TAXES_JOINLY : DOCUMENTS_BORROWER_NOT_SELF_EMPLOYED
+      required_documents = borrower.is_file_taxes_jointly ? Document::BORROWER_NOT_SELF_EMPLOYED_TAXES_JOINLY : Document::BORROWER_NOT_SELF_EMPLOYED
 
       (required_documents - borrower.documents.pluck(:document_type)).empty?
     end
