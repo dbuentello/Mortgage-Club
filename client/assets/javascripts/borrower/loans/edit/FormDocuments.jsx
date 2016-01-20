3a7e932364893baccd5dec05bc218c20e8603611
Tang Nguyen <nvtang.khtn@gmail.com>
2016-01-20 11:57:44 +0700

    validate documents tab

--- client/assets/javascripts/borrower/loans/edit/FormDocuments.jsx
+++ client/assets/javascripts/borrower/loans/edit/FormDocuments.jsx
@@ -36,6 +36,10 @@ var co_borrower_upload_fields = {
   second_bank_statement: {label: 'Bank statement - Previous month', name: 'co_second_bank_statement'}
 };
 
+var uploaded_files = [];
+var upload_fields = [];
+var co_upload_fields = [];
+
 var FormDocuments = React.createClass({
   mixins: [TextFormatMixin],
 
@@ -62,6 +66,12 @@ var FormDocuments = React.createClass({
     this.props.saveLoan(this.buildLoanFromState(), 2, true);
   },
 
+  componentDidUpdate: function(){
+    if(!this.valid()){
+      this.scrollTopError();
+    }
+  },
+
   render: function() {
     var uploadUrl = '/document_uploaders/base_document/upload';
     var borrower = this.props.loan.borrower;
@@ -77,9 +87,6 @@ var FormDocuments = React.createClass({
     // coborrower - self employed
     var co_no_file_taxes_jointly_fields = ['first_personal_tax_return', 'second_personal_tax_return', 'first_business_tax_return', 'second_business_tax_return', 'first_bank_statement', 'second_bank_statement'];
     var co_file_taxes_jointly_fields = ['first_business_tax_return', 'second_business_tax_return', 'first_bank_statement', 'second_bank_statement'];
-
-    var upload_fields = [];
-    var co_upload_fields = [];
 
     if (borrower.self_employed == true) {
       upload_fields = self_employed_fields;
@@ -115,13 +122,15 @@ var FormDocuments = React.createClass({
                   <div className="drop_zone" key={key}>
                     <Dropzone field={owner_upload_fields[key]}
                       uploadUrl={uploadUrl}
+                      activateRequiredField={this.state.activateRequiredField}
                       downloadUrl={this.state[owner_upload_fields[key].name + '_downloadUrl']}
                       removeUrl={this.state[owner_upload_fields[key].name + '_removedUrl']}
                       tip={this.state[owner_upload_fields[key].name]}
                       maxSize={10000000}
                       customParams={customParams}
                       supportOtherDescription={owner_upload_fields[key].customDescription}
-                      uploadSuccessCallback={this.afterUploadingDocument}/>
+                      uploadSuccessCallback={this.afterUploadingDocument}
+                      removeSuccessCallback={this.afterRemovingDocument}/>
                   </div>
                 )
               }
@@ -166,13 +175,15 @@ var FormDocuments = React.createClass({
                         <div className="drop_zone" key={key}>
                           <Dropzone field={co_borrower_upload_fields[key]}
                             uploadUrl={uploadUrl}
+                            activateRequiredField={this.state.activateRequiredField}
                             downloadUrl={this.state[co_borrower_upload_fields[key].name + '_downloadUrl']}
                             removeUrl={this.state[co_borrower_upload_fields[key].name + '_removedUrl']}
                             tip={this.state[co_borrower_upload_fields[key].name]}
                             maxSize={10000000}
                             customParams={customParams}
                             supportOtherDescription={co_borrower_upload_fields[key].customDescription}
-                            uploadSuccessCallback={this.afterUploadingDocument}/>
+                            uploadSuccessCallback={this.afterUploadingDocument}
+                            removeSuccessCallback={this.afterRemovingDocument}/>
                         </div>
                       )
                     }
@@ -190,7 +201,15 @@ var FormDocuments = React.createClass({
     );
   },
 
-  afterUploadingDocument: function() {
+  afterUploadingDocument: function(name) {
+    uploaded_files.push(name);
+  },
+
+  afterRemovingDocument: function(name) {
+    var index = uploaded_files.indexOf(name);
+    if (index > -1) {
+      uploaded_files.splice(index, 1);
+    }
   },
 
   componentWillReceiveProps: function(nextProps) {
@@ -204,7 +223,7 @@ var FormDocuments = React.createClass({
     var secondary_borrower = loan.secondary_borrower;
     var state = {};
     state['is_file_taxes_jointly'] = loan.borrower.is_file_taxes_jointly;
-
+    state.activateRequiredField = false;
     this.setStateForUploadFields(borrower, state, owner_upload_fields);
     if (secondary_borrower) {
       this.setStateForUploadFields(secondary_borrower, state, co_borrower_upload_fields);
@@ -216,6 +235,8 @@ var FormDocuments = React.createClass({
     _.map(Object.keys(upload_fields), function(key) {
       var borrower_document = _.find(borrower.documents, { 'document_type': key });
       if (borrower_document){
+        uploaded_files.push(upload_fields[key].name);
+
         state[upload_fields[key].name] = borrower_document.original_filename;
         state[upload_fields[key].id] = borrower_document.id;
         state[upload_fields[key].name + '_downloadUrl'] = '/document_uploaders/base_document/' + borrower_document.id + '/download';
@@ -232,9 +253,42 @@ var FormDocuments = React.createClass({
     return loan;
   },
 
+  scrollTopError: function(){
+    var offset = $(".tooltip").first().parents(".form-group").offset();
+    var top = offset === undefined ? 0 : offset.top;
+    $('html, body').animate({scrollTop: top}, 1000);
+  },
+
+  valid: function(){
+    var borrower_uploaded_files = [];
+    var co_borrower_uploaded_files = [];
+
+    _.each(upload_fields, function(upload_field) {
+      borrower_uploaded_files.push(owner_upload_fields[upload_field].name);
+    });
+
+    _.each(co_upload_fields, function(upload_field) {
+      co_borrower_uploaded_files.push(co_borrower_upload_fields[upload_field].name);
+    });
+
+    var result = borrower_uploaded_files.concat(co_borrower_uploaded_files).filter(function(i) {
+      return uploaded_files.indexOf(i) < 0;
+    });
+
+    if(result.length == 0)
+      return true;
+    return false;
+  },
+
   save: function(event) {
-    this.setState({saving: true});
-    this.props.saveLoan(this.buildLoanFromState(), 2);
+    this.setState({saving: true, activateRequiredField: true});
+
+    if(this.valid()){
+      this.props.saveLoan(this.buildLoanFromState(), 2);
+    }
+    else{
+      this.setState({saving: false});
+    }
     event.preventDefault();
   }
 });
