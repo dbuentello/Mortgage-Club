d12b0ffdfef7d60964c94941ce107cb185680096
Linh Nguyen <linhvn09@gmail.com>
2016-01-22 11:10:57 +0700

    add more test cases for employment model and liability model

--- spec/models/employment_spec.rb
+++ spec/models/employment_spec.rb
@@ -12,9 +12,13 @@ describe Employment do
   end
 
   describe 'raises error when current_salary exceeds maximum allowed value' do
-    let(:employment) { FactoryGirl.build(:employment, current_salary: 123123123123) }
+    let(:employment1) { FactoryGirl.build(:employment, current_salary: 123123123123) }
+    let(:employment2) { FactoryGirl.build(:employment, ytd_salary: 999123123123) }
+    let(:employment3) { FactoryGirl.build(:employment, monthly_income: 123123123123) }
     it "raises StatementInvalid Error" do
-      expect { employment.save }.to raise_error(ActiveRecord::StatementInvalid)
+      expect { employment1.save }.to raise_error(ActiveRecord::StatementInvalid)
+      expect { employment2.save }.to raise_error(ActiveRecord::StatementInvalid)
+      expect { employment3.save }.to raise_error(ActiveRecord::StatementInvalid)
     end
   end
 end
--- spec/models/liability_spec.rb
+++ spec/models/liability_spec.rb
@@ -3,10 +3,12 @@ require 'rails_helper'
 describe Liability, type: :model do
   context 'with invalid asset params' do
     describe 'Money amount fields exceed maximum allowed value will raise error' do
-      let(:liability) { FactoryGirl.build(:liability, balance: 321456789123)}
+      let(:liability1) { FactoryGirl.build(:liability, balance: 321456789123)}
+      let(:liability2) { FactoryGirl.build(:liability, payment: 123456789123)}
 
       it 'raise StatementInvalid Error' do
-        expect { raise liability.save }.to raise_error(ActiveRecord::StatementInvalid)
+        expect { raise liability1.save }.to raise_error(ActiveRecord::StatementInvalid)
+        expect { raise liability2.save }.to raise_error(ActiveRecord::StatementInvalid)
       end
     end
   end
