f5197cf78732579e1b190e75a31b989396512c57
Cuong Vu <cuongvu0103@gmail.com>
2016-01-22 18:14:33 +0700

    Fix bug: set min max of date picker

--- client/assets/javascripts/components/form/NewDateField.jsx
+++ client/assets/javascripts/components/form/NewDateField.jsx
@@ -56,7 +56,7 @@ var DateFieldView = React.createClass({
     return (
       <div>
         <h6>{this.props.label}</h6>
-        <input className={"form-control " + this.props.customClass} defaultValue={dateVal} type="text" placeholder={this.props.placeholder}
+        <input readOnly={"readonly"} className={"form-control " + this.props.customClass} defaultValue={dateVal} type="text" placeholder={this.props.placeholder} data-date-end-date="0d"
           onBlur={this.onBlur} onFocus={this.handleFocus} id={this.props.keyName} name={this.props.label}/>
         <img src="/icons/date.png" alt="title" onClick={this.showDatePicker}/>
         <ValidationField id={this.props.keyName} activateRequiredField={this.props.activateRequiredField} value={dateVal} title={requiredMessage} validationTypes={this.props.validationTypes}/>
@@ -70,7 +70,8 @@ var DateFieldView = React.createClass({
       format: "mm/dd/yyyy",
       forceParse: true,
       autoclose: true,
-      todayHighlight: true
+      todayHighlight: true,
+      enableOnReadonly: false
     });
     this.$input.datepicker().on("changeDate", this.handleChange);
   },
