c13bb5232b369b81849df7687a68863f6385173f
Linh Nguyen <linhvn09@gmail.com>
2016-01-22 14:14:46 +0700

    rewrite format PhoneNumber Mixin

--- client/assets/javascripts/mixins/TextFormatMixin.js
+++ client/assets/javascripts/mixins/TextFormatMixin.js
@@ -114,12 +114,6 @@ var TextFormatMixin = {
     return splice(splice(val, 3, '-'), 6, '-').slice(0, 11);
   },
 
-  formatPhoneNumber: function(s) {
-    var s2 = ('' + s).replace(/\D/g, '');
-    var m = s2.match(/^(\d{3})(\d{3})(\d{4})$/);
-    return (!m) ? s : '(' + m[1] + ') ' + m[2] + '-' + m[3];
-  },
-
   formatYear: function(s) {
     if(!s) { return; }
     var currentYear = (new Date(Date.now()).getFullYear())*1;
@@ -133,6 +127,12 @@ var TextFormatMixin = {
       return currentYear.toString();
     }
     return s;
+  },
+
+  formatPhoneNumber: function(val) {
+    if (!val) { return; }
+    val = val.replace(/\D/g, '');
+    return ('(' + splice(splice(val, 3, ') '), 8, '-').slice(0, 13));
   }
 };
 
