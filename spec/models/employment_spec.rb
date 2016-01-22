436aaf0d0dad1613637ca5600f7a80ff124e1136
Linh Nguyen <linhvn09@gmail.com>
2016-01-22 10:41:09 +0700

    update test case for employment model with exceeds money amount value

--- spec/models/employment_spec.rb
+++ spec/models/employment_spec.rb
@@ -11,4 +11,10 @@ describe Employment do
     expect(employment.address).to be_truthy
   end
 
+  describe 'raises error when current_salary exceeds maximum allowed value' do
+    let(:employment) { FactoryGirl.build(:employment, current_salary: 123123123123) }
+    it "raises StatementInvalid Error" do
+      expect { employment.save }.to raise_error(ActiveRecord::StatementInvalid)
+    end
+  end
 end
