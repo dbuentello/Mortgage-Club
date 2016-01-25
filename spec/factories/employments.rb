b0bd56f10b88ffc0e4719e02a9c653abea8d55ca
Tang Nguyen <nvtang.khtn@gmail.com>
2016-01-25 15:50:17 +0700

    update cucumber

--- spec/factories/employments.rb
+++ spec/factories/employments.rb
@@ -7,7 +7,7 @@ FactoryGirl.define do
     employer_contact_name { Faker::Name.name }
     employer_contact_number { '(' + Faker::Number.number(3) + ') ' + Faker::Number.number(3) + '-' + Faker::Number.number(4) }
     job_title { Faker::Lorem.word }
-    duration { Random.rand(1..100) }
+    duration { Random.rand(2..100) }
     pay_frequency { Random.rand(1..100) }
     current_salary { Random.rand(1..100) }
     is_current { false }
