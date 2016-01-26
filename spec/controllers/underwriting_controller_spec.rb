2de5fa2a917dc7f9fbf2fb8a9aa08d6871988db2
Linh Nguyen <linhvn09@gmail.com>
2016-01-26 09:52:01 +0700

    add test case for check_loan method

--- spec/controllers/underwriting_controller_spec.rb
+++ spec/controllers/underwriting_controller_spec.rb
@@ -8,16 +8,34 @@ RSpec.describe UnderwritingController do
     address = FactoryGirl.build(:address, street_address: "208 Silver Eagle Road", city: "Sacramento", zip: 95838, property_id: loan.subject_property.id)
     address.save
   }
-  it "responds with code 200 when loan is completed" do
+  describe "#index" do
+    it "responds with code 200 when loan is completed" do
       allow_any_instance_of(Loan).to receive("completed?").and_return(true)
-      get :index, :loan_id => loan.id
+      get :index, loan_id: loan.id
       expect(response.status).to eq(200)
     end
 
     it "redirects to the loan edit page when the loan uncompleted" do
       allow_any_instance_of(Loan).to receive("completed?").and_return(false)
 
-      get :index, :loan_id => loan.id
+      get :index, loan_id: loan.id
       expect(response.status).to eq(302)
     end
+  end
+
+  describe "#check_loan" do
+    it "renders message ok when loan is valid" do
+      allow_any_instance_of(UnderwritingLoanServices::UnderwriteLoan).to receive("valid_loan?").and_return(true)
+
+      get :check_loan, loan_id: loan.id
+      expect(JSON.parse(response.body)["message"]).to eq "ok"
+    end
+
+    it "renders message error when loan is invalid" do
+      allow_any_instance_of(UnderwritingLoanServices::UnderwriteLoan).to receive("valid_loan?").and_return(false)
+
+      get :check_loan, loan_id: loan.id
+      expect(JSON.parse(response.body)["message"]).to eq "error"
+    end
+  end
 end
