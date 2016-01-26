aac7eb8619b568f79945990c3db5176c4e37e850
Cuong Vu <cuongvu0103@gmail.com>
2016-01-26 10:26:27 +0700

    Edit filename for Cucumber's screenshot

--- features/support/env.rb
+++ features/support/env.rb
@@ -72,8 +72,7 @@ end
 
 After do |scenario|
   if scenario.failed?
-    time_now = Time.now
-    timestamp = "#{time_now.strftime('%Y-%m-%d-%H-%M-%S.')}#{'%03d' % (time_now.usec/1000).to_i}"
+    timestamp = "#{Time.now.strftime('%Y-%m-%d-%H:%M:%S')}"
 
     screenshot_name = "screenshot-failed-cucumber-#{timestamp}.png"
     screenshot_path = "#{ENV.fetch('CIRCLE_ARTIFACTS', Rails.root.join('tmp/capybara'))}/#{screenshot_name}"
