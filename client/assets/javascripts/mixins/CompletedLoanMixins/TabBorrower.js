598d7ab844b7ef368ddd9177aac443ed8fa0a43a
Tang Nguyen <nvtang.khtn@gmail.com>
2016-01-27 16:15:04 +0700

    add tab borrower mixing

---
+++ client/assets/javascripts/mixins/CompletedLoanMixins/TabBorrower.js
@@ -0,0 +1,97 @@
+var TabBorrower = {
+  completed: function(loan){
+    var borrower = loan.borrower;
+    var secondaryBorrower = loan.secondary_borrower;
+
+    if(this.objectFieldNotValid(secondaryBorrower))
+      return this.borrowerCompleted(borrower);
+    return (this.borrowerCompleted(borrower) && this.borrowerCompleted(secondaryBorrower));
+  },
+
+  borrowerCompleted: function(borrower){
+    if (borrower === undefined)
+      return false;
+
+    if (this.objectFieldNotValid(borrower.self_employed))
+      return false;
+
+    if (this.valueFieldNotValid(borrower.first_name))
+      return false;
+
+    if (this.valueFieldNotValid(borrower.last_name))
+      return false;
+
+    if (this.valueFieldNotValid(borrower.ssn))
+      return false;
+
+    if (this.valueFieldNotValid(borrower.dob))
+      return false;
+
+    if (this.valueFieldNotValid(borrower.years_in_school))
+      return false;
+
+    if (this.valueFieldNotValid(borrower.marital_status))
+      return false;
+
+    if (this.valueFieldNotValid(borrower.dependent_count))
+      return false;
+
+    if (borrower.dependent_ages.length == 0 && borrower.dependent_count >= 1)
+      return false;
+
+    if (this.objectFieldNotValid(borrower.current_address))
+      return false;
+
+    if (this.valueFieldNotValid(borrower.current_address.is_rental))
+      return false;
+
+    if (this.valueFieldNotValid(borrower.current_address.years_at_address))
+      return false;
+
+    if (borrower.current_address.years_at_address < 0)
+      return false;
+
+    if (this.rentHouseAndMonthlyRentNotValid(borrower.current_address))
+      return false;
+
+    if (!this.previousAddressCompleted(borrower))
+      return false;
+    return true;
+  },
+
+  valueFieldNotValid: function(field){
+    if (field === undefined || field === null || field === "")
+      return true;
+    return false;
+  },
+
+  objectFieldNotValid: function(obj){
+    if(obj === undefined || obj === null)
+      return true;
+    return false;
+  },
+
+  previousAddressCompleted: function(borrower){
+    if (this.objectFieldNotValid(borrower))
+      return false;
+
+    if (borrower.current_address.years_at_address > 1)
+      return true;
+
+    if (this.valueFieldNotValid(borrower.previous_address))
+      return false;
+
+    if (this.valueFieldNotValid(borrower.previous_address.is_rental))
+      return false;
+
+    if (this.rentHouseAndMonthlyRentNotValid(borrower.previous_address))
+      return false;
+    return true;
+  },
+
+  rentHouseAndMonthlyRentNotValid: function(address){
+    return ((address.is_rental === true) && (!this.valueFieldNotValid(address.monthly_rent)))
+  }
+}
+
+module.exports = TabBorrower;
