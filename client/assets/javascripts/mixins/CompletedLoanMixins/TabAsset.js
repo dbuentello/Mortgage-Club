df760082f2e1301ee6703dd278179b4536ece5b4
Cuong Vu <cuongvu0103@gmail.com>
2016-01-27 17:22:40 +0700

    Check whether Tabs Property, Asset and Declaration is completed or not

---
+++ client/assets/javascripts/mixins/CompletedLoanMixins/TabAsset.js
@@ -0,0 +1,84 @@
+var _ = require('lodash');
+
+var TabAsset = {
+  assetCompleted: function(loan) {
+    if(!this.propertyCompleted(loan.subject_property)) { return false; }
+
+    if(this.requiredPrimaryProperty(loan) && !this.propertyCompleted(loan.primary_property)) {
+      return false;
+    }
+
+    if(loan.own_investment_property && !this.rentalPropertiesCompleted(loan)) {
+      return false;
+    }
+
+    if(!this.allAssetsAreCompleted(loan)) {
+      return false;
+    }
+
+    return true;
+  },
+
+  allAssetsAreCompleted: function(loan) {
+    var assets = loan.borrower.assets;
+    if(assets.length < 1) { return false; }
+    var completed = true;
+
+    assets.forEach(function (asset) {
+      if(asset.institution_name == null || asset.asset_type == null || asset.current_balance == null) {
+        completed = false;
+        return false;
+      }
+    });
+
+    return completed;
+  },
+
+  rentalPropertiesCompleted: function(loan) {
+    var rental_properties = loan.rental_properties;
+    var completed = true;
+    _.each(rental_properties, function(property) {
+      if(!this.propertyCompleted(property)) {
+        completed = false;
+        return false;
+      }
+    });
+
+    return completed;
+  },
+
+  propertyCompleted: function(property) {
+    if(property == null || property == undefined) { return false; }
+    if(property.property_type == null || property.usage == null ||
+      property.market_price == null ||
+      property.mortgage_includes_escrows == null ||
+      property.estimated_property_tax == null ||
+      property.estimated_hazard_insurance == null
+    ){
+      return false;
+    }
+
+    if(!this.addressCompleted(property.address)) {
+      return false;
+    }
+
+    return true;
+  },
+
+  addressCompleted: function(address) {
+    if(address == null || address == undefined) { return false; }
+    if(address.street_address == null && address.city == null && address.state == null && address.street_address2 == null)
+      return false;
+
+    return true;
+  },
+
+  requiredPrimaryProperty: function(loan) {
+    if(loan.primary_property !== undefined && loan.primary_property != loan.subject_property) {
+      return true;
+    }
+
+    return false;
+  }
+}
+module.exports = TabAsset;
