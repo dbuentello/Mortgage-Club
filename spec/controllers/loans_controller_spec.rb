af402dfb7c7245596e21ad93fbd63f76321ab511
Linh Nguyen <linhvn09@gmail.com>
2016-01-28 11:27:07 +0700

    update loans controller to check all valid right to update a loan

--- spec/controllers/loans_controller_spec.rb
+++ spec/controllers/loans_controller_spec.rb
@@ -2,8 +2,13 @@ require 'rails_helper'
 
 describe Users::LoansController do
   include_context 'signed in as borrower user of loan'
+  let!(:not_permission_loan) { FactoryGirl.create(:loan) }
   let(:other_user) { FactoryGirl.create(:borrower_user_with_borrower) }
   let!(:credit_report) { FactoryGirl.create(:credit_report, borrower: loan.borrower)}
+
+  before do
+    not_permission_loan.user = other_user
+  end
 
   describe 'POST #create' do
     it 'assigns the requested loan to @loan' do
@@ -22,12 +27,6 @@ describe Users::LoansController do
     end
 
     context 'when user does not have permission to edit loan' do
-      let!(:not_permission_loan) { FactoryGirl.create(:loan) }
-
-      before do
-        not_permission_loan.user = other_user
-      end
-
       it 'responses 403' do
         get :edit, id: not_permission_loan.id
         expect(response.status).to eq 403
@@ -36,15 +35,37 @@ describe Users::LoansController do
   end
 
   describe 'PUT #update' do
-    it 'updates a loan and assigns loan to @loan' do
-      put :update, id: loan.id, loan: FactoryGirl.attributes_for(:loan), format: :json
-      expect(assigns(:loan)).to eq(loan)
+    context 'when user can update his loan' do
+      it 'updates a loan and assigns loan to @loan' do
+        put :update, id: loan.id, loan: FactoryGirl.attributes_for(:loan), format: :json
+        expect(assigns(:loan)).to eq(loan)
+      end
+    end
+
+    context 'when user cannot update the other user loan' do
+      it 'responds with status 403' do
+        put :update, id: not_permission_loan.id, loan: FactoryGirl.attributes_for(:loan), format: :json
+        expect(response.status).to eq 403
+      end
     end
   end
 
   describe 'DELETE #destroy' do
-    it 'destroys passed id loan' do
-      expect { delete :destroy, id: loan.id, format: :json }.to change{Loan.count}.by(-1)
+    context 'when user has permission to delete a loan' do
+      it 'destroys passed id loan' do
+        expect { delete :destroy, id: loan.id, format: :json }.to change{Loan.count}.by(-1)
+      end
+    end
+
+    context 'when user access other user loan' do
+      it 'does not allow a user delete the loan' do
+        expect { delete :destroy, id: not_permission_loan.id, format: :json }.to change{Loan.count}.by(0)
+      end
+
+      it 'returns status 403' do
+        delete :destroy, id: not_permission_loan.id, format: :json
+        expect(response.status).to eq 403
+      end
     end
   end
 
