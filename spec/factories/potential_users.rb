0fb2f9792678c0b48878d155bc340982d928f09d
Tang Nguyen <nvtang.khtn@gmail.com>
2016-01-29 17:26:34 +0700

    [WIP] add test for potential user managements

--- spec/factories/potential_users.rb
+++ spec/factories/potential_users.rb
@@ -1,8 +1,7 @@
 FactoryGirl.define do
   factory :potential_user do |f|
-    f.name { Faker::Name.first_name }
     f.email { Faker::Internet.email }
-    f.phone { '(' + Faker::Number.number(3) + ') ' + Faker::Number.number(3) + '-' + Faker::Number.number(4) }
+    f.phone_number { '(' + Faker::Number.number(3) + ') ' + Faker::Number.number(3) + '-' + Faker::Number.number(4) }
     f.mortgage_statement { File.new(Rails.root.join 'spec', 'files', 'sample.png') }
   end
 end
