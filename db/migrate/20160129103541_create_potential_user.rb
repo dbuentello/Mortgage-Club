2a6adc2ab81f84b497784fb3952744c3f7cff5e2
Linh Nguyen <linhvn09@gmail.com>
2016-01-29 16:51:20 +0700

    change migration using uuid

--- db/migrate/20160129103541_create_potential_user.rb
+++ db/migrate/20160129103541_create_potential_user.rb
@@ -1,6 +1,6 @@
 class CreatePotentialUser < ActiveRecord::Migration
   def change
-    create_table :potential_users do |t|
+    create_table :potential_users, id: :uuid do |t|
       t.string :email
       t.string :name
       t.string :phone_number
