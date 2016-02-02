bdeca8c66d95f1f70b2b5c277b5f3357fa79bdb0
Cuong Vu <cuongvu0103@gmail.com>
2016-02-02 15:53:01 +0700

    Update cucumber + spec + factories girl

--- spec/factories/potential_users.rb
+++ spec/factories/potential_users.rb
@@ -3,5 +3,7 @@ FactoryGirl.define do
     f.email { Faker::Internet.email }
     f.phone_number { '(' + Faker::Number.number(3) + ') ' + Faker::Number.number(3) + '-' + Faker::Number.number(4) }
     f.mortgage_statement { File.new(Rails.root.join 'spec', 'files', 'sample.png') }
+    f.send_as_email { [true, false].sample }
+    f.send_as_text_message { [true, false].sample }
   end
 end
