9c1c527c43d7415745ae6ec72235e06b773bdd5f
Tang Nguyen <nvtang.khtn@gmail.com>
2016-02-02 17:57:52 +0700

    write service get loan tek rates

--- app/services/homepage_rate_services/get_mortgage_aprs.rb
+++ app/services/homepage_rate_services/get_mortgage_aprs.rb
@@ -2,20 +2,20 @@ module HomepageRateServices
   class GetMortgageAprs
     def self.call(refresh_cache = false)
       # return default_aprs unless Rails.env.production?
-
+      refresh_cache = true
       cache_key = "mortgage-apr"
 
       if !refresh_cache && mortgage_aprs = REDIS.get(cache_key)
         mortgage_aprs = JSON.parse(mortgage_aprs)
       else
-        zillow = HomepageRateServices::LoanTek.call
+        loan_tek = HomepageRateServices::LoanTek.call
         quicken_loans = HomepageRateServices::Quickenloans.call
         wellsfargo = HomepageRateServices::Wellsfargo.call
 
-        edit_rates(zillow, quicken_loans, wellsfargo)
+        edit_rates(loan_tek, quicken_loans, wellsfargo)
 
         mortgage_aprs = {
-          "zillow" => zillow,
+          "loan_tek" => loan_tek,
           "quicken_loans" => quicken_loans,
           "wellsfargo" => wellsfargo,
           "updated_at" => Time.zone.now
@@ -28,7 +28,7 @@ module HomepageRateServices
 
     def self.default_aprs
       {
-        "zillow" => {
+        "loan_tek" => {
           "apr_30_year" => 0,
           "apr_15_year" => 0,
           "apr_5_libor" => 0
@@ -47,23 +47,23 @@ module HomepageRateServices
       }
     end
 
-    def self.edit_rates(zillow, quicken_loans, wellsfargo)
-      zillow.each do |type, rate|
+    def self.edit_rates(loan_tek, quicken_loans, wellsfargo)
+      loan_tek.each do |type, rate|
         quicken_loans_rate = quicken_loans[type]
         wellsfargo_rate = wellsfargo[type]
         offset = (type == "apr_30_year".freeze) ? 0.538 : 0.125
 
         if should_edit_rate?(rate, quicken_loans_rate, wellsfargo_rate, type)
-          zillow[type] = [quicken_loans_rate, wellsfargo_rate].min - offset
+          loan_tek[type] = [quicken_loans_rate, wellsfargo_rate].min - offset
         end
       end
     end
 
-    def self.should_edit_rate?(zillow_rate, quicken_loans_rate, wellsfargo_rate, type)
+    def self.should_edit_rate?(loan_tek_rate, quicken_loans_rate, wellsfargo_rate, type)
       if type == "apr_30_year".freeze
-        return (zillow_rate == 0 || zillow_rate > ([quicken_loans_rate, wellsfargo_rate].min - 0.375))
+        return (loan_tek_rate == 0 || loan_tek_rate > ([quicken_loans_rate, wellsfargo_rate].min - 0.375))
       else
-        return (zillow_rate == 0 || zillow_rate > [quicken_loans_rate, wellsfargo_rate].min)
+        return (loan_tek_rate == 0 || loan_tek_rate > [quicken_loans_rate, wellsfargo_rate].min)
       end
     end
   end
--- app/services/homepage_rate_services/loan_tek.rb
+++ app/services/homepage_rate_services/loan_tek.rb
@@ -1,18 +1,65 @@
 module HomepageRateServices
   class LoanTek
     def self.call
-      # @rates = ZillowService::CrawlZillowRates.new({
-      #   zipcode: 94103,
-      #   purchase_price: 500000,
-      #   down_payment: 100000,
-      #   annual_income: 200000
-      # }).call
+      get_quotes
+    end
+
+    def self.get_quotes
+      url ="https://api.loantek.com/Clients/WebServices/Client/#{ENV["LOANTEK_CLIENT_ID"]}/Pricing/V2/Quotes/LoanPricer/#{ENV["LOANTEK_USER_ID"]}"
+      connection = Faraday.new(url: url)
+      @response = connection.post do |conn|
+        conn.headers["Content-Type"] = "application/json"
+        conn.body = {
+          BestExecutionMethodType: 1,
+          QuotingChannel: 0,
+          ClientDefinedIdentifier: ENV["LOANTEK_IDENTIFIER"],
+          ZipCode: 94103,
+          CreditScore: 'R_760_',
+          LoanPurpose: 1,
+          LoanAmount: 400000,
+          LoanToValue: 80,
+          PropertyUsage: 1,
+          PropertyType: 1,
+          QuoteTypesToReturn: [-1, 0, 1, 2, 3, 4]
+        }.to_json
+      end
+
+      @response.status == 200 ? get_rates(JSON.parse(@response.body)["Quotes"]) : []
+    end
+
+    def self.get_rates(quotes)
+      rates = quotes.map! do |quote|
+        if product_name_valid?(quote)
+          {
+            product: quote["ProductName"],
+            apr: quote["APR"] / 100
+          }
+        end
+      end
+
+      rates.compact
+
+      apr_30_year = 1
+      apr_15_year = 1
+      apr_5_libor = 1
+
+      rates.each do |rate|
+        apr_30_year = rate[:apr] if rate[:product] == "30yearFixed" && rate[:apr] < apr_30_year
+        apr_15_year = rate[:apr] if rate[:product] == "15yearFixed" && rate[:apr] < apr_15_year
+        apr_5_libor = rate[:apr] if rate[:product] == "5yearARM" && rate[:apr] < apr_5_libor
+      end
 
       {
-        "apr_30_year" => 0,
-        "apr_15_year" => 0,
-        "apr_5_libor" => 0
+        "apr_30_year" => apr_30_year * 100,
+        "apr_15_year" => apr_15_year * 100,
+        "apr_5_libor" => apr_5_libor * 100
       }
+    end
+
+    def self.product_name_valid?(quote)
+      name = quote["ProductName"]
+
+      name == "30yearFixed" || name == "15yearFixed" || name == "5yearARM"
     end
   end
 end
