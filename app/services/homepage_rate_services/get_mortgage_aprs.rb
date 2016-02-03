d4c77cc63de21e21228fe297e1a1e486bc7d90d8
Tang Nguyen <nvtang.khtn@gmail.com>
2016-02-03 09:24:05 +0700

    Remove the cheating formula: MC rate = lowest rate - 0.5xx%

--- app/services/homepage_rate_services/get_mortgage_aprs.rb
+++ app/services/homepage_rate_services/get_mortgage_aprs.rb
@@ -12,8 +12,6 @@ module HomepageRateServices
         quicken_loans = HomepageRateServices::Quickenloans.call
         wellsfargo = HomepageRateServices::Wellsfargo.call
 
-        edit_rates(loan_tek, quicken_loans, wellsfargo)
-
         mortgage_aprs = {
           "loan_tek" => loan_tek,
           "quicken_loans" => quicken_loans,
@@ -45,26 +43,6 @@ module HomepageRateServices
         },
         "updated_at" => Time.zone.now
       }
-    end
-
-    def self.edit_rates(loan_tek, quicken_loans, wellsfargo)
-      loan_tek.each do |type, rate|
-        quicken_loans_rate = quicken_loans[type]
-        wellsfargo_rate = wellsfargo[type]
-        offset = (type == "apr_30_year".freeze) ? 0.538 : 0.125
-
-        if should_edit_rate?(rate, quicken_loans_rate, wellsfargo_rate, type)
-          loan_tek[type] = [quicken_loans_rate, wellsfargo_rate].min - offset
-        end
-      end
-    end
-
-    def self.should_edit_rate?(loan_tek_rate, quicken_loans_rate, wellsfargo_rate, type)
-      if type == "apr_30_year".freeze
-        return (loan_tek_rate == 0 || loan_tek_rate > ([quicken_loans_rate, wellsfargo_rate].min - 0.375))
-      else
-        return (loan_tek_rate == 0 || loan_tek_rate > [quicken_loans_rate, wellsfargo_rate].min)
-      end
     end
   end
 end
