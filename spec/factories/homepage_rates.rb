3e3c022064fc241e01ea081f92e3d5f803fc17bd
Tang Nguyen <nvtang.khtn@gmail.com>
2016-02-05 12:36:52 +0700

    add test for get mortgage apr

--- spec/factories/homepage_rates.rb
+++ spec/factories/homepage_rates.rb
@@ -4,4 +4,16 @@ FactoryGirl.define do
     program { ["15 Year Fixed", "5/1 Libor ARM", "30 Year Fixed"].sample }
     rate_value { Random.rand(1..50)/1000.0 }
   end
+
+  factory :loan_tek_rate, parent: :homepage_rate do |f|
+    f.lender_name { "Mortgage Club" }
+  end
+
+  factory :wellsfargo_rate, parent: :homepage_rate do |f|
+    f.lender_name { "Wells Fargo" }
+  end
+
+  factory :quicken_loans_rate, parent: :homepage_rate do |f|
+    f.lender_name { "Quicken Loans" }
+  end
 end
