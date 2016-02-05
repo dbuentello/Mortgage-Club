d7f16134b0ba36d66ebc7505dff6b6473cdecaf8
Linh Nguyen <linhvn09@gmail.com>
2016-02-05 17:20:47 +0700

    update lender model test cases

--- spec/models/lender_spec.rb
+++ spec/models/lender_spec.rb
@@ -5,9 +5,6 @@ describe Lender do
   it { should have_many(:lender_templates) }
   it { should have_many(:loans) }
   it { should validate_presence_of(:name) }
-  it { should validate_uniqueness_of(:lock_rate_email) }
-  it { should validate_uniqueness_of(:docs_email) }
-  it { should validate_uniqueness_of(:contact_email) }
   it { should validate_presence_of(:contact_name) }
   it { should validate_presence_of(:contact_phone) }
   it { should validate_presence_of(:website) }
