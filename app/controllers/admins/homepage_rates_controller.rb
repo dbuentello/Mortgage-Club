c2d8fb5c41ff172bbbe4cb1db91c33dbbf54d9df
Tang Nguyen <nvtang.khtn@gmail.com>
2016-02-05 17:57:05 +0700

    remove bye bug

--- app/controllers/admins/homepage_rates_controller.rb
+++ app/controllers/admins/homepage_rates_controller.rb
@@ -12,7 +12,6 @@ class Admins::HomepageRatesController < Admins::BaseController
   end
 
   def edit
-    byebug
     Time.zone = "Pacific Time (US & Canada)"
     display_time = Time.zone.parse(@homepage_rate.display_time.to_s).strftime('%m/%d/%Y %H:%M')
 
