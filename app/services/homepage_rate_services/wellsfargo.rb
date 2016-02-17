d1774e803dff33965ddcecabb97b1761f3c98600
Tang Nguyen <nvtang.khtn@gmail.com>
2016-02-17 10:48:03 +0700

    add wellsfargo

--- app/services/homepage_rate_services/wellsfargo.rb
+++ app/services/homepage_rate_services/wellsfargo.rb
@@ -3,27 +3,34 @@ module HomepageRateServices
     include HTTParty
 
     def self.call
-      apr_5_libor = 0
+      apr_5_libor = WellsfargoServices::CrawlWellsfargoArmRate.new({
+        loan_purpose: "Purchase",
+        home_value: 400000,
+        down_payment: 80000,
+        property_state: "CA",
+        property_county: "San Francisco"
+      }).call
+
       apr_30_year = 0
       apr_15_year = 0
 
       html = get("https://www.wellsfargo.com/mortgage/rates", verify: false)
       doc = Nokogiri::HTML(html)
 
-      doc.css("#productName").each do |rate|
-        if rate.text == "5/1 ARM FHA".freeze && rate.at_css("a".freeze).attr("href".freeze) == "/mortgage/rates/purchase-assumptions?prod=5".freeze
-          apr_5_libor = rate.parent.css('td'.freeze).last.text.delete('%'.freeze).to_f
-        elsif rate.text == "15-Year Fixed Rate".freeze && rate.at_css("a").attr("href") == "/mortgage/rates/purchase-assumptions?prod=3".freeze
-          apr_15_year = rate.parent.css("td").last.text.delete("%").to_f
-        elsif rate.text == "30-Year Fixed Rate".freeze && rate.at_css("a").attr("href") == "/mortgage/rates/purchase-assumptions?prod=1".freeze
-          apr_30_year = rate.parent.css("td").last.text.delete("%").to_f
+      doc.css('#productName').each do |rate|
+        if rate.text == '15-Year Fixed Rate'.freeze && rate.at_css('a').attr('href') == '/mortgage/rates/purchase-assumptions?prod=3'.freeze
+          apr_15_year = rate.parent.css('td').last.text.delete('%').to_f
+        elsif rate.text == '30-Year Fixed Rate'.freeze && rate.at_css('a').attr('href') == '/mortgage/rates/purchase-assumptions?prod=1'.freeze
+          apr_30_year = rate.parent.css('td').last.text.delete('%').to_f
+        else
+          next
         end
       end
 
       {
-        "apr_30_year" => apr_30_year == 0 ? nil : apr_30_year,
-        "apr_15_year" => apr_15_year == 0 ? nil : apr_15_year,
-        "apr_5_libor" => nil
+        "apr_30_year" => apr_30_year,
+        "apr_15_year" => apr_15_year,
+        "apr_5_libor" => apr_5_libor
       }
     end
   end
