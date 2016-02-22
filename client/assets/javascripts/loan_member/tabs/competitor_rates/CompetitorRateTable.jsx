18e39a3067ce35888f3c8828fffbec715c372b84
Tang Nguyen <nvtang.khtn@gmail.com>
2016-02-22 13:30:48 +0700

    update panel

--- client/assets/javascripts/loan_member/tabs/competitor_rates/CompetitorRateTable.jsx
+++ client/assets/javascripts/loan_member/tabs/competitor_rates/CompetitorRateTable.jsx
@@ -7,66 +7,68 @@ module.exports = React.createClass({
     {
       return (
         <div className="list-rates">
-          <div className="panel-heading">
-            <h4 className="panel-title">{this.props.title}</h4>
-          </div>
-          <div className="table-responsive competitor-rates">
-            <table className="table table-hover competitor-rates" style={{width: "100%"}}>
-              <thead>
-                <th width="9%"></th>
-                <th width="13%">30 Year Fixed</th>
-                <th width="13%">20 Year Fixed</th>
-                <th width="13%">15 Year Fixed</th>
-                <th width="13%">10 Year Fixed</th>
-                <th width="13%">7/1 ARM</th>
-                <th width="13%">5/1 ARM</th>
-                <th width="13%">3/1 ARM</th>
-              </thead>
-              <tbody>
-              {
-                _.map(this.props.competitorRates, function(competitor_rate){
-                  return (
-                    <tr>
-                      <td>
-                        {competitor_rate.competitor_name}
-                      </td>
-                      {
-                        _.map(competitor_rate.rates, function(rate){
-                          return (
-                              <div>
-                                <td data-toggle="tooltip"
-                                  title={rate.lender_name + "\n" + this.formatCurrency(rate.total_fee, "$")}
-                                  data-trigger="focus"
-                                  data-container="body"
-                                  >
-                                  <div className="panel panel-info">
-                                    {
-                                      rate.apr != 0
-                                      ?
-                                        <div className="panel-body text-center">
-                                          <p className="lead">
-                                              <strong>{this.commafy(rate.apr, 3)+"% APR"}</strong>
-                                          </p>
-                                          <ul className="list-group list-group-flush text-center">
-                                            <li className="list-group-item">{rate.lender_name}</li>
-                                            <li className="list-group-item">{this.formatCurrency(rate.total_fee, "$")} Fees</li>
-                                          </ul>
-                                        </div>
-                                      :
-                                        <div className="panel-body text-center"></div>
-                                    }
-                                  </div>
-                                </td>
-                              </div>
-                            );
-                        }.bind(this))
-                      }
-                    </tr>
-                    );
-                }.bind(this))
-              }
-              </tbody>
-            </table>
+          <div className="panel panel-flat">
+            <div className="panel-heading">
+              <h4 className="panel-title">{this.props.title}</h4>
+            </div>
+            <div className="table-responsive competitor-rates">
+              <table className="table table-hover competitor-rates" style={{width: "100%"}}>
+                <thead>
+                  <th width="9%"></th>
+                  <th width="13%">30 Year Fixed</th>
+                  <th width="13%">20 Year Fixed</th>
+                  <th width="13%">15 Year Fixed</th>
+                  <th width="13%">10 Year Fixed</th>
+                  <th width="13%">7/1 ARM</th>
+                  <th width="13%">5/1 ARM</th>
+                  <th width="13%">3/1 ARM</th>
+                </thead>
+                <tbody>
+                {
+                  _.map(this.props.competitorRates, function(competitor_rate){
+                    return (
+                      <tr>
+                        <td>
+                          {competitor_rate.competitor_name}
+                        </td>
+                        {
+                          _.map(competitor_rate.rates, function(rate){
+                            return (
+                                <div>
+                                  <td data-toggle="tooltip"
+                                    title={rate.lender_name + "\n" + this.formatCurrency(rate.total_fee, "$")}
+                                    data-trigger="focus"
+                                    data-container="body"
+                                    >
+                                    <div className="panel panel-info">
+                                      {
+                                        rate.apr != 0
+                                        ?
+                                          <div className="panel-body text-center">
+                                            <p className="lead">
+                                                <strong>{this.commafy(rate.apr, 3)+"% APR"}</strong>
+                                            </p>
+                                            <ul className="list-group list-group-flush text-center">
+                                              <li className="list-group-item">{rate.lender_name}</li>
+                                              <li className="list-group-item">{this.formatCurrency(rate.total_fee, "$")} Fees</li>
+                                            </ul>
+                                          </div>
+                                        :
+                                          <div className="panel-body text-center"></div>
+                                      }
+                                    </div>
+                                  </td>
+                                </div>
+                              );
+                          }.bind(this))
+                        }
+                      </tr>
+                      );
+                  }.bind(this))
+                }
+                </tbody>
+              </table>
+            </div>
           </div>
         </div>
       );
