f6ac278e78ba8613cc6568f2646d42d895e9cb6d
Tang Nguyen <nvtang.khtn@gmail.com>
2016-02-23 12:11:10 +0700

    remove quote '

--- client/assets/javascripts/borrower/dashboard/loans/tabs/LoansTab.jsx
+++ client/assets/javascripts/borrower/dashboard/loans/tabs/LoansTab.jsx
@@ -1,6 +1,6 @@
-var moment = require('moment');
-var React = require('react/addons');
-var TextFormatMixin = require('mixins/TextFormatMixin');
+var moment = require("moment");
+var React = require("react/addons");
+var TextFormatMixin = require("mixins/TextFormatMixin");
 
 var LoansTab = React.createClass({
   mixins: [TextFormatMixin],
@@ -8,11 +8,11 @@ var LoansTab = React.createClass({
     return (
       <div className="col-md-4 loan-item" key={loan.id} index={i}>
         <div className="loan-item-holder">
-          <a href={'/my/dashboard/' + loan.id}>
+          <a href={"/my/dashboard/" + loan.id}>
             <img className="img-responsive fixed-height-246" src={loan.subject_property.zillow_image_url ? loan.subject_property.zillow_image_url : "/default.jpg"}/>
           </a>
           <div className="caption">
-            <a href={'/my/dashboard/' + loan.id}>
+            <a href={"/my/dashboard/" + loan.id}>
               {
                 this.props.addresses[loan.subject_property.id] ?
                   <h6><strong>{this.props.addresses[loan.subject_property.id]}</strong></h6>
@@ -21,21 +21,21 @@ var LoansTab = React.createClass({
               }
             </a>
             <p><strong>Status:</strong> {loan.pretty_status}</p>
-            <p><strong>Create at:</strong> {moment(loan.created_at).format('MMM DD, YYYY')}</p>
+            <p><strong>Create at:</strong> {moment(loan.created_at).format("MMM DD, YYYY")}</p>
             <p><strong>Loan amount:</strong> {this.formatCurrency(loan.amount, "$")}</p>
             <p><strong>Rate:</strong> {this.commafy(loan.interest_rate*100, 3)}%</p>
             {
               loan.pretty_status == "New"
               ?
                 <p>
-                  <a href={'/loans/' + loan.id + "/edit"} className="btn edit-app-btn" role="button">
+                  <a href={"/loans/" + loan.id + "/edit"} className="btn edit-app-btn" role="button">
                     <i className="iconPencil mrs"></i>
                     <span>Edit Application</span>
                   </a>
                 </p>
               :
                 <p>
-                  <a href={'/my/dashboard/' + loan.id} className="btn dashboard-btn" role="button">
+                  <a href={"/my/dashboard/" + loan.id} className="btn dashboard-btn" role="button">
                     <img className="gear-icon" src="/icons/gear.png"/>
                     <span>Dashboard</span>
                   </a>
