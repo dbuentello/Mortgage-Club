1e165522c78cd1b82b956d4ebb0c856b71776133
Linh Nguyen <linhvn09@gmail.com>
2016-02-23 23:39:21 +0700

    update test case

--- spec/factories/loans_members_associations.rb
+++ spec/factories/loans_members_associations.rb
@@ -2,5 +2,6 @@ FactoryGirl.define do
   factory :loans_members_association do |f|
     loan
     loan_member { build(:loan_member) }
+    loan_members_title { build(:loan_members_title) }
   end
 end
