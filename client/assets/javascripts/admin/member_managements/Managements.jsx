693f4b4ca76cd61d8f18f295f803c54a3f6f78ab
jamesnguyenvnn <james.nguyen.vnn@gmail.com>
2016-02-23 14:25:52 +0700

    team member table

--- client/assets/javascripts/admin/member_managements/Managements.jsx
+++ client/assets/javascripts/admin/member_managements/Managements.jsx
@@ -23,51 +23,88 @@ var Managements = React.createClass({
     var url = '/loan_member_managements/';
 
     return (
-      <div className='content container'>
-        <div className='pal'>
-          <div className='row'>
-            <h2 className='mbl'>Team Members</h2>
-            <table className="table table-striped">
-              <thead>
-                <tr>
-                  <th>Avatar</th>
-                  <th>Name</th>
-                  <th>Email</th>
-                  <th>Phone number</th>
-                  <th>Skype</th>
-                  <th></th>
-                </tr>
-              </thead>
-              <tbody>
-                {
-                  _.map(this.state.members, function(member) {
-                    return (
-                      <tr key={member.id}>
-                        <td>
-                          <img src={member.user.avatar_url} width="40px" height="30px"/>
-                        </td>
-                        <td>{member.user.to_s}</td>
-                        <td>{member.user.email}</td>
-                        <td>{member.phone_number}</td>
-                        <td>{member.skype_handle}</td>
-                        <td>
-                          <span>
-                            <a className='linkTypeReversed btn btn-primary' href={'loan_member_managements/' + member.id + '/edit'} data-method='get'>Edit</a>
-                          </span>
-                        </td>
-                      </tr>
-                    )
-                  }, this)
-                }
-              </tbody>
-            </table>
-          </div>
-          <div className='row'>
-            <h2 className='mbl'>Add new member</h2>
-            <Form Url={url} Method='POST' onReloadTable={this.onReloadTable}></Form>
+      <div>
+          {/* Page header */ }
+      <div className="page-header">
+        <div className="page-header-content">
+          <div className="page-title">
+            <h4><i className="icon-arrow-left52 position-left"></i> <span className="text-semibold">Team member</span> - Management</h4>
           </div>
         </div>
       </div>
+      {/* /page header */ }
+
+      {/* Page container */ }
+      <div className="page-container">
+
+        {/* Page content */ }
+        <div className="page-content">
+
+          {/* Main content */ }
+          <div className="content-wrapper">
+
+            {/* Table */ }
+            <div className="panel panel-flat">
+              <div className="panel-heading">
+                <h5 className="panel-title">Team Members</h5>
+                <div className="heading-elements">
+                  <ul className="icons-list">
+                      <li><a data-action="collapse"></a></li>
+                      <li><a data-action="close"></a></li>
+                  </ul>
+                </div>
+              </div>
+              <div className="panel-body">
+
+              </div>
+              <div className="table-responsive">
+                <table className="table table-striped table-hover">
+                  <thead>
+                    <tr>
+                      <th>Avatar</th>
+                      <th>Name</th>
+                      <th>Email</th>
+                      <th>Phone number</th>
+                      <th>Skype</th>
+                      <th></th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {
+                      _.map(this.state.members, function(member) {
+                        return (
+                          <tr key={member.id}>
+                            <td>
+                              <img src={member.user.avatar_url} width="40px" height="30px"/>
+                            </td>
+                            <td>{member.user.to_s}</td>
+                            <td>{member.user.email}</td>
+                            <td>{member.phone_number}</td>
+                            <td>{member.skype_handle}</td>
+                            <td>
+                              <span>
+                                <a className='linkTypeReversed btn btn-primary' href={'loan_member_managements/' + member.id + '/edit'} data-method='get'>Edit</a>
+                              </span>
+                            </td>
+                          </tr>
+                        )
+                      }, this)
+                    }
+                  </tbody>
+                </table>
+              </div>
+            </div>
+            {/* /table */ }
+
+          </div>
+          {/* /main content */ }
+
+        </div>
+        {/* /page content */ }
+
+      </div>
+      {/* /page container */ }
+    </div>
     )
   }
 
