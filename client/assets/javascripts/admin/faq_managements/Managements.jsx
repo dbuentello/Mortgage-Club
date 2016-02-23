2f02a0633d2b55c985b47b93ab2ec9e6fd581483
jamesnguyenvnn <james.nguyen.vnn@gmail.com>
2016-02-23 14:55:12 +0700

    faq management ui

--- client/assets/javascripts/admin/faq_managements/Managements.jsx
+++ client/assets/javascripts/admin/faq_managements/Managements.jsx
@@ -21,41 +21,102 @@ var Managements = React.createClass({
     var url = '/loan_faq_managements/';
 
     return (
-      <div className='content container'>
-        <div className='pal'>
-          <div className='row'>
-            <h2 className='mbl'>FAQ Managements</h2>
-            <table className="table table-striped">
-              <thead>
-                <tr>
-                  <th>Question</th>
-                  <th></th>
-                </tr>
-              </thead>
-              <tbody>
-                {
-                  _.map(this.state.faqs, function(faq) {
-                    return (
-                      <tr key={faq.id}>
-                        <td>{faq.question}</td>
-                        <td>
-                          <span>
-                            <a className='linkTypeReversed btn btn-primary' href={'loan_faq_managements/' + faq.id + '/edit'} data-method='get'>Edit</a>
-                          </span>
-                        </td>
-                      </tr>
-                    )
-                  }, this)
-                }
-              </tbody>
-            </table>
-          </div>
-          <div className='row'>
-            <h2 className='mbl'>Add new FAQ</h2>
-            <Form Url={url} Method='POST' onReloadTable={this.onReloadTable}></Form>
+      <div>
+          {/* Page header */ }
+      <div className="page-header">
+        <div className="page-header-content">
+          <div className="page-title">
+            <h4><i className="icon-arrow-left52 position-left"></i> <span className="text-semibold">FAQ</span> - Management</h4>
           </div>
         </div>
       </div>
+      {/* /page header */ }
+
+      {/* Page container */ }
+      <div className="page-container">
+
+        {/* Page content */ }
+        <div className="page-content">
+
+          {/* Main content */ }
+          <div className="content-wrapper">
+
+            {/* Table */ }
+            <div className="panel panel-flat">
+              <div className="panel-heading">
+                <h5 className="panel-title">FAQ</h5>
+                <div className="heading-elements">
+                  <ul className="icons-list">
+                      <li><a data-action="collapse"></a></li>
+                      <li><a data-action="close"></a></li>
+                  </ul>
+                </div>
+              </div>
+              <div className="panel-body">
+
+              </div>
+              <div className="table-responsive">
+                <table className="table table-striped table-hover">
+                  <thead>
+                    <tr>
+                      <th>Question</th>
+                      <th>Actions</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {
+                      _.map(this.state.faqs, function(faq) {
+                        return (
+                          <tr key={faq.id}>
+                            <td>{faq.question}</td>
+                            <td>
+                              <span>
+                                <a className='linkTypeReversed btn btn-primary' href={'loan_faq_managements/' + faq.id + '/edit'} data-method='get'>Edit</a>
+                              </span>
+                            </td>
+                          </tr>
+                        )
+                      }, this)
+                    }
+                  </tbody>
+                </table>
+              </div>
+            </div>
+            {/* /table */ }
+
+            {/* Grid */ }
+				    <div className="row">
+					    <div className="col-md-12">
+
+    						{/* Horizontal form */ }
+    						<div className="panel panel-flat">
+    							<div className="panel-heading">
+    								<h5 className="panel-title">Add new Question</h5>
+    								<div className="heading-elements">
+    									<ul className="icons-list">
+		                		<li><a data-action="collapse"></a></li>
+		                		<li><a data-action="close"></a></li>
+		                	</ul>
+	                	</div>
+                	</div>
+
+    							<div className="panel-body">
+                      <Form Url={url} Method='POST' onReloadTable={this.onReloadTable}></Form>
+                  </div>
+    						</div>
+    						{/* /horizotal form */ }
+					    </div>
+				    </div>
+				    {/* /grid */ }
+          </div>
+          {/* /main content */ }
+
+        </div>
+        {/* /page content */ }
+
+      </div>
+      {/* /page container */ }
+    </div>
     )
   }
 });
