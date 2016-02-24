76df2301b3d5dc27904e0910d4cb88da7403b3f2
Linh Nguyen <linhvn09@gmail.com>
2016-02-24 14:46:46 +0700

    fix cucumber test

--- features/step_definitions/web_steps.rb
+++ features/step_definitions/web_steps.rb
@@ -78,8 +78,8 @@ When /^I am at dashboard page$/ do
       And there is a property with the id "be69aefe-3946-4b85-9c54-58dd51306b1c" and with the purchase price "1000000" and with the usage "0" and with the is primary "1" and with the is subject "1" and with the loan above
       And there is a address with the street address "81458 Borer Falls" and the street address2 "Apt. 305" and the city "West Emiltown" and the state "Virginia" and the zip "9999" and the property id "be69aefe-3946-4b85-9c54-58dd51306b1c"
       And there is a document with the user above and with the document type "appraisal_report" and with the attachment file name "property-document-name" and with the subjectable type "Property" and the subjectable id "be69aefe-3946-4b85-9c54-58dd51306b1c"
-      And there is a loans members association with the loan above and with the loan member above and with the title "sale"
-      And there is a loans members association with the title "manager" and with the loan above
+      And there is a loans members association with the loan above and with the loan member above
+      And there is a loans members association with the loan above
       And I login as "testing@man.net" with password "secretpass"
     Then I follow "Dashboard"
   }
@@ -94,8 +94,9 @@ When /^I am at my loans page$/ do
       And there is a loan_member_user_with_loan_member with the first name "Michael" and the last name "Gifford"
       And there is a address with the street address "81458 Borer Falls" and the street address2 "Apt. 305" and the city "West Emiltown" and the state "Virginia" and the zip "9999" with the property above
       And there is a document with the user above and with the document type "appraisal_report" and with the attachment file name "property-document-name" and with the subjectable type "Property" and the subjectable id "be69aefe-3946-4b85-9c54-58dd51306b1c"
-      And there is a loans members association with the loan above and with the loan member above and with the title "sale"
-      And there is a loans members association with the title "manager" and with the loan above
+      And there is a loan members title with the title "manager"
+      And there is a loans members association with the loan above and with the loan member above
+      And there is a loans members association with the loan above
       And I login as "testing@man.net" with password "secretpass"
   }
 end
@@ -122,7 +123,8 @@ When /^I am at loan member dashboard$/ do
       And there is a loan with the amount "500000" and with the num of months "24" and with the purpose "0" and with the user above and with the closing above and with the user above and with the lender above
       And there is a property with the purchase price "1000000" and with the usage "0" and with the is primary "1" and with the is subject "1" and with loan above
       And there is a loan_member_user_with_loan_member with the email "loan_member@gmail.com" and the password "secretpass" and the password confirmation "secretpass" and the first name "Mark" and the last name "Lim"
-      And there is a loans members association with the loan above and with the loan member above and with the title "sale"
+      And there is a loan members title with the title "sale"
+      And there is a loans members association with the loan above and with the loan member above
       And there is a checklist_upload with the loan above
       And I login as "loan_member@gmail.com" with password "secretpass"
     Then I click link with div ".linkTypeReversed"
