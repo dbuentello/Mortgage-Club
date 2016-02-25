a1d8e4f8423218e6b07ae67a6cd4e7874bc9b3b9
Linh Nguyen <linhvn09@gmail.com>
2016-02-25 18:11:45 +0700

    update test case for loan

--- spec/models/loan_spec.rb
+++ spec/models/loan_spec.rb
@@ -109,6 +109,14 @@ describe Loan do
       end
     end
 
+    context "non existing loan_members_title in loans_members_associations" do
+      it "returns nil" do
+        loan_with_loan_member.loans_members_associations.last
+        loan_with_loan_member.loans_members_associations.last.update(loan_members_title: nil)
+        expect(loan_with_loan_member.relationship_manager).to be_nil
+      end
+    end
+
     context "loans_members_associations are valid" do
       let!(:manager) { FactoryGirl.create(:loan_members_title, title: "manager")}
       it "returns a loan member" do
