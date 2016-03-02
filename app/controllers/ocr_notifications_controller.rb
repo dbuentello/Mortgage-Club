9c6cf5c752b0a37273f83b4dedf421b912827d94
jamesnguyenvnn <james.nguyen.vnn@gmail.com>
2016-03-02 15:35:02 +0700

    improve OCR performance

--- app/controllers/ocr_notifications_controller.rb
+++ app/controllers/ocr_notifications_controller.rb
@@ -2,19 +2,10 @@ class OcrNotificationsController < ApplicationController
   skip_before_action :verify_authenticity_token, :only => [:receive]
   skip_before_action :authenticate_user!, :only => [:receive]
 
-  include HTTParty
-
   def receive
     @setting = Setting.all.first
     if @setting.present? && @setting.ocr
-        if request.raw_post.present?
-          raw_post = JSON.parse(request.raw_post)
-          if raw_post["SubscribeURL"].present?
-            ConfirmSubscriptionService.call(raw_post)
-          else
-            OcrServices::ProcessPaystub.call(raw_post)
-          end
-        end
+      OcrServices::ProcessPaystub.call(params)
     end
     render nothing: true, status: 200, content_type: 'text/html'
   end
