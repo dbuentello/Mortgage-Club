7dd62a3c8e04c31a4de590e14b0fc20c8e1b8abe
jamesnguyenvnn <james.nguyen.vnn@gmail.com>
2016-03-03 22:14:50 +0700

    homepage react

--- client/assets/javascripts/public/BankPart.jsx
+++ client/assets/javascripts/public/BankPart.jsx
@@ -4,42 +4,42 @@ var BankPart = React.createClass({
   render: function() {
 
     return (
-      <div class="home-footer content">
+      <div className="home-footer content">
         <hr/>
-        <div class="container">
-            <div class="row header-logos">
-              <div class="col-md-2  col-sm-6 col-sm-offset-4 col-md-offset-0 header-logo asseen">
-                <div class="text-uppercase"><p>Our wholesale</p><p> investors</p></div>
+        <div className="container">
+            <div className="row header-logos">
+              <div className="col-md-2  col-sm-6 col-sm-offset-4 col-md-offset-0 header-logo asseen">
+                <div className="text-uppercase"><p>Our wholesale</p><p> investors</p></div>
               </div>
-              <div class="col-md-8">
-                <div class="row">
-                <div class="col-md-3 col-sm-6  partner-logo">
+              <div className="col-md-8">
+                <div className="row">
+                <div className="col-md-3 col-sm-6  partner-logo">
 
                   <img src="/usbank_logo.png" />
                 </div>
-                <div class="col-md-2 col-sm-6 partner-logo">
+                <div className="col-md-2 col-sm-6 partner-logo">
                   <img src="/remn_logo.png" />
                 </div>
-                <div class="col-md-2 col-sm-6  partner-logo">
+                <div className="col-md-2 col-sm-6  partner-logo">
 
                     <img src="/pf_logo.png" />
                 </div>
-                <div class="col-md-2 col-sm-6 partner-logo">
+                <div className="col-md-2 col-sm-6 partner-logo">
 
                     <img src="/interfirst.png" />
                 </div>
-                <div class="col-md-3 col-sm-6 col-sm-offset-3 col-md-offset-0 partner-logo">
+                <div className="col-md-3 col-sm-6 col-sm-offset-3 col-md-offset-0 partner-logo">
 
                     <img src="/union_bank_logo.png" />
                 </div>
                 </div>
               </div>
-              <div class="col-md-1 col-sm-12 col-sm-offset-4 col-md-offset-0 header-logo asseen">
-                <div class="text-uppercase"><p>And many</p><p>more</p></div>
+              <div className="col-md-1 col-sm-12 col-sm-offset-4 col-md-offset-0 header-logo asseen">
+                <div className="text-uppercase"><p>And many</p><p>more</p></div>
               </div>
             </div>
         </div>
-        <hr/>
+    
       </div>
 
     );
