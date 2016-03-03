74d69f30214555336b91591bbbaf80454dddfe71
Linh Nguyen <linhvn09@gmail.com>
2016-03-03 17:00:56 +0700

    update test cases for loan member management controller

--- spec/controllers/admin/loan_member_managements_controller_spec.rb
+++ spec/controllers/admin/loan_member_managements_controller_spec.rb
@@ -5,13 +5,25 @@ describe Admins::LoanMemberManagementsController do
 
   describe "#create" do
     context "when valid params" do
-      let(:loan_member_params) { FactoryGirl.attributes_for(:loan_member).merge({first_name: "Jon", last_name: "Smith", email: Faker::Internet.email, password: "secret-password"}) }
+      before do
+        @loan_member_params = { first_name: "Jon", last_name: "Smith",
+          email: Faker::Internet.email, password: "secret-password",
+          company_name: "Green Valley",
+          company_address: "Sillicon Valley",
+          company_phone_number: "(134)-345-678",
+          company_nmls: "33883",
+          nmls_id: "334545"
+        }
+      end
       it "returns successful message" do
-        post :create, loan_member: loan_member_params
+        post :create, loan_member: @loan_member_params
         expect(JSON.parse(response.body)["message"]).to eq("Created sucessfully")
       end
+      it "changes User data table by 1" do
+        expect{post :create, loan_member: @loan_member_params }.to change(User, :count).by 1
+      end
       it "changes LoanMember data table by 1" do
-        expect{post :create, loan_member: loan_member_params }.to change(LoanMember, :count).by 1
+        expect{post :create, loan_member: @loan_member_params }.to change(LoanMember, :count).by 1
       end
     end
   end
