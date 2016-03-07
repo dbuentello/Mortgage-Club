299a5b31ff0902743120c283caf670f47a43f44c
Linh Nguyen <linhvn09@gmail.com>
2016-03-07 11:57:39 +0700

    refactor code

--- spec/services/borrower_services/assign_secondary_borrower_to_loan_spec.rb
+++ spec/services/borrower_services/assign_secondary_borrower_to_loan_spec.rb
@@ -17,20 +17,20 @@ describe BorrowerServices::AssignSecondaryBorrowerToLoan do
     allow_any_instance_of(Digest::MD5).to receive(:hexdigest).and_return "8820245fb6"
   end
 
-  describe "the method for assigning secondary borrower for a loan" do
-    let!(:loan) { FactoryGirl.create(:loan_with_all_associations) }
-    it "calls CoBorrowerMailer with proper params" do
-      message_delivery = instance_double(ActionMailer::MessageDelivery)
-      expect(CoBorrowerMailer).to receive(:notify_being_added).with(loan.id, {is_new_user: false, default_password: nil}).and_return(message_delivery)
-      expect(message_delivery).to receive(:deliver_later)
-      BorrowerServices::AssignSecondaryBorrowerToLoan.new(loan, @secondary_params, nil).call
+  describe "#call" do
+    context "with valid params" do
+      let!(:loan) { FactoryGirl.create(:loan_with_all_associations) }
+      it "calls CoBorrowerMailer with proper params" do
+        message_delivery = instance_double(ActionMailer::MessageDelivery)
+        expect(CoBorrowerMailer).to receive(:notify_being_added).with(loan.id, {is_new_user: false, default_password: nil}).and_return(message_delivery)
+        expect(message_delivery).to receive(:deliver_later)
+        described_class.new(loan, @secondary_params, nil).call
+      end
+
+      it "calls save params" do
+        expect_any_instance_of(Loan).to receive(:save).and_return true
+        described_class.new(loan, @secondary_params, nil).call
+      end
     end
-
-    it "calls save params" do
-      expect_any_instance_of(Loan).to receive(:save).and_return true
-      BorrowerServices::AssignSecondaryBorrowerToLoan.new(loan, @secondary_params, nil).call
-    end
-
-
   end
 end
