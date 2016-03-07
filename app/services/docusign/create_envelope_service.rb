c9a9260e8a2f357a898eac6b323b4811f85cf247
Tang Nguyen <nvtang.khtn@gmail.com>
2016-03-07 10:06:50 +0700

    refactor code

--- app/services/docusign/create_envelope_service.rb
+++ app/services/docusign/create_envelope_service.rb
@@ -17,21 +17,59 @@ module Docusign
     end
 
     def call(user, loan)
-      # create_document_by_adobe_field_names(loan)
+      create_document_by_adobe_field_names(loan)
       client = DocusignRest::Client.new
+      envelope = client.create_envelope_from_document(
+        {
+          status: "sent",
+          email: {
+            subject: "Electronic Signature Request from MortgageClub Corporation",
+            body: "As discussed, let's finish our contract by signing to this envelope. Thank you!"
+          },
+          files: [
+            {path: UNIFORM_OUTPUT_PATH},
+            {path: FORM_4506_OUTPUT_PATH},
+            {path: BORROWER_CERTIFICATION_OUTPUT_PATH}
+          ],
+          signers: build_signers(user, loan)
+        }
+      )
 
-      document_data = {
-        status: "sent",
-        email: {
-          subject: "Electronic Signature Request from MortgageClub Corporation",
-          body: "As discussed, let's finish our contract by signing to this envelope. Thank you!"
-        },
-        files: [
-          {path: UNIFORM_OUTPUT_PATH},
-          {path: FORM_4506_OUTPUT_PATH},
-          {path: BORROWER_CERTIFICATION_OUTPUT_PATH}
-        ],
-        signers: [
+      delete_temp_files
+      envelope
+    end
+
+    def create_document_by_adobe_field_names(loan)
+      generate_uniform(loan)
+      generate_form_4506
+      generate_form_certification
+    end
+
+    def generate_uniform(loan)
+      data = Docusign::Templates::UniformResidentialLoanApplication.new(loan).build
+      pdftk.get_field_names(UNIFORM_PATH)
+      pdftk.fill_form(UNIFORM_PATH, "tmp/uniform.pdf", data)
+    end
+
+    def generate_form_4506
+      pdftk.get_field_names(FORM_4506_PATH)
+      pdftk.fill_form(FORM_4506_PATH, "tmp/form4506t.pdf")
+    end
+
+    def generate_form_certification
+      pdftk.get_field_names(BORROWER_CERTIFICATION_PATH)
+      pdftk.fill_form(BORROWER_CERTIFICATION_PATH, "tmp/certification.pdf")
+    end
+
+    def delete_temp_files
+      File.delete(UNIFORM_OUTPUT_PATH)
+      File.delete(FORM_4506_OUTPUT_PATH)
+      File.delete(BORROWER_CERTIFICATION_OUTPUT_PATH)
+    end
+
+    def build_signers(user, loan)
+      signers =
+        [
           {
             embedded: true,
             name: "#{user.first_name} #{user.last_name}",
@@ -63,10 +101,9 @@ module Docusign
             ]
           }
         ]
-      }
 
       if loan.secondary_borrower
-        document_data[:signers] << {
+        signers << {
           embedded: true,
           name: "#{loan.secondary_borrower.user.first_name} #{loan.secondary_borrower.user.last_name}",
           email: loan.secondary_borrower.user.email,
@@ -83,34 +120,8 @@ module Docusign
           ]
         }
       end
-      byebug
-      envelope = client.create_envelope_from_document(document_data)
-      File.delete(UNIFORM_OUTPUT_PATH)
-      File.delete(FORM_4506_OUTPUT_PATH)
-      File.delete(BORROWER_CERTIFICATION_OUTPUT_PATH)
-      envelope
-    end
 
-    def create_document_by_adobe_field_names(loan)
-      generate_uniform(loan)
-      generate_form_4506
-      generate_form_certification
-    end
-
-    def generate_uniform(loan)
-      data = Docusign::Templates::UniformResidentialLoanApplication.new(loan).build
-      pdftk.get_field_names(UNIFORM_PATH)
-      pdftk.fill_form(UNIFORM_PATH, "tmp/uniform.pdf", data)
-    end
-
-    def generate_form_4506
-      pdftk.get_field_names(FORM_4506_PATH)
-      pdftk.fill_form(FORM_4506_PATH, "tmp/form4506t.pdf")
-    end
-
-    def generate_form_certification
-      pdftk.get_field_names(BORROWER_CERTIFICATION_PATH)
-      pdftk.fill_form(BORROWER_CERTIFICATION_PATH, "tmp/certification.pdf")
+      signers
     end
   end
 end
