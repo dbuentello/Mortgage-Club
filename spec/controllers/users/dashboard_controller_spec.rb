2177be30f41f9569ee0625a29050b88044cd01f0
Linh Nguyen <linhvn09@gmail.com>
2016-03-09 12:00:24 +0700

    correct words

--- spec/controllers/users/dashboard_controller_spec.rb
+++ spec/controllers/users/dashboard_controller_spec.rb
@@ -4,16 +4,16 @@ describe Users::DashboardController do
   include_context "signed in as borrower user of loan"
 
   describe "#show" do
-    context "with new loan" do
+    context "when new loan" do
       let!(:loan) { FactoryGirl.create(:loan) }
 
-      it "redirect to edit page" do
+      it "redirects to edit page" do
         get :show, id: loan.id
         expect(response.status).to eq(302)
       end
     end
 
-    context "with completed loan" do
+    context "when completed loan" do
       let!(:loan) { FactoryGirl.create(:loan_with_all_associations, status: :submitted) }
 
       it "renders template borrower_app" do
@@ -22,7 +22,7 @@ describe Users::DashboardController do
       end
     end
 
-    context "with the loan that not belongs to the current user" do
+    context "when the loan that not belongs to the current user" do
 
       before do
         @loan = FactoryGirl.create(:loan_with_all_associations)
