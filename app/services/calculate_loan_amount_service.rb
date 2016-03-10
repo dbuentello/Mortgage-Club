b674e1763641bf6fdb75b743f8db784710c0deb6
Cuong Vu <cuongvu0103@gmail.com>
2016-03-10 11:42:36 +0700

    Add service to calculate loan's amount

---
+++ app/services/calculate_loan_amount_service.rb
@@ -0,0 +1,23 @@
+class CalculateLoanAmountService
+  def self.call(loan)
+    if loan.refinance?
+      amount = calculate_loan_amount_for_refinance_loan(loan)
+    elsif loan.purchase?
+      amount = calculate_loan_amount_for_purchase_loan(loan)
+    end
+    amount
+  end
+
+  def self.calculate_loan_amount_for_refinance_loan(loan)
+    if loan.subject_property.mortgage_payment_liability
+      amount = loan.subject_property.mortgage_payment_liability.balance
+    else
+      amount = loan.subject_property.market_price.to_f * 0.75
+    end
+    amount
+  end
+
+  def self.calculate_loan_amount_for_purchase_loan(loan)
+    loan.subject_property.purchase_price.to_f - loan.down_payment
+  end
+end
