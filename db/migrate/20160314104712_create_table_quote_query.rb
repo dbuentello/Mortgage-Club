9762f4cba1f67d0618b5801855650e9d0b4bd636
Cuong Vu <cuongvu0103@gmail.com>
2016-03-15 09:44:50 +0700

    Add timestamp to table

--- db/migrate/20160314104712_create_table_quote_query.rb
+++ db/migrate/20160314104712_create_table_quote_query.rb
@@ -3,6 +3,7 @@ class CreateTableQuoteQuery < ActiveRecord::Migration
     create_table :quote_queries do |t|
       t.string :code_id, index: true
       t.text :query
+      t.timestamps null: false
     end
   end
 end
