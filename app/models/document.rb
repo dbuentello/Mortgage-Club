be0bdcba4acbfd952f16a28b862f099d7a1371e1
Linh Nguyen <linhvn09@gmail.com>
2016-03-15 11:59:57 +0700

    update document with I18n

--- app/models/document.rb
+++ app/models/document.rb
@@ -57,11 +57,11 @@ class Document < ActiveRecord::Base
     presence: true,
     content_type: {
       content_type: ALLOWED_MIME_TYPES,
-      message: ' allows MS Excel, MS Documents, MS Powerpoint, Rich Text, Text File and Images'
+      message: :content_type_invalid
     },
     size: {
       less_than_or_equal_to: 10.megabytes,
-      message: ' must be less than or equal to 10MB'
+      message: :size_too_large
     }
 
   validate :document_type_must_belong_to_proper_document
@@ -97,6 +97,6 @@ class Document < ActiveRecord::Base
     when "Closing"
       return if CLOSING_LIST.include? document_type
     end
-    errors.add(:document_type, "must belong to a proper document")
+    errors.add(:document_type, :needed_proper_document)
   end
 end
