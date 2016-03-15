5999411a4f627443f7ae0e7ee475104013c1f487
Linh Nguyen <linhvn09@gmail.com>
2016-03-15 17:37:09 +0700

    update lender document i18n

--- app/controllers/loan_members/lender_documents_controller.rb
+++ app/controllers/loan_members/lender_documents_controller.rb
@@ -3,7 +3,7 @@ class LoanMembers::LenderDocumentsController < LoanMembers::BaseController
   before_action :set_document, only: [:download, :destroy]
 
   def create
-    return render json: {message: t("messages.errors.object_not_found", object: "Template")}, status: 500 unless template = LenderTemplate.find_by_id(params[:template_id])
+    return render json: {message: t("errors.messages.template_not_found")}, status: 500 unless template = LenderTemplate.find_by_id(params[:template_id])
 
     service = LenderDocumentServices::UploadFile.new(
       loan: @loan,
@@ -19,10 +19,10 @@ class LoanMembers::LenderDocumentsController < LoanMembers::BaseController
         lender_documents: LoanMembers::LenderDocumentsPresenter.new(@loan.lender_documents).show,
         download_url: get_download_url(service.lender_document),
         remove_url: get_remove_url(service.lender_document),
-        message: t("messages.info.success", status: "created")
+        message: t("loan_members.lender_documents.create.add_success")
       }, status: 200
     else
-      render json: {message: t("messages.errors.failed", process: "upload document")}, status: 500
+      render json: {message: t("loan_members.lender_documents.create.add_failed")}, status: 500
     end
   end
 
@@ -33,9 +33,9 @@ class LoanMembers::LenderDocumentsController < LoanMembers::BaseController
 
   def destroy
     if @document.destroy
-      return render json: {message: t("messages.info.success", status: "removed")}, status: 200
+      return render json: {message: t("loan_members.lender_documents.create.add_success")}, status: 200
     else
-      return render json: {message: t("messages.errors.failed", process: "remove")}, status: 500
+      return render json: {message: t("loan_members.lender_documents.create.add_failed")}, status: 500
     end
   end
 
@@ -48,7 +48,7 @@ class LoanMembers::LenderDocumentsController < LoanMembers::BaseController
   private
 
   def set_document
-    return render json: {message: t("messages.errors.object_not_found", object: "File")}, status: 500 unless @document = LenderDocument.find_by_id(params[:id])
+    return render json: {message: t("errors.messages.file_not_found")}, status: 500 unless @document = LenderDocument.find_by_id(params[:id])
   end
 
   def get_download_url(document)
