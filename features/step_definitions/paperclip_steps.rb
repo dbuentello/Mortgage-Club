588a4054541fa6e06d0ddaac3e733850e6718200
Tang Nguyen <nvtang.khtn@gmail.com>
2016-03-15 17:02:52 +0700

    enable Style/MethodCallParentheses rubocop

--- features/step_definitions/paperclip_steps.rb
+++ features/step_definitions/paperclip_steps.rb
@@ -1,13 +1,13 @@
 require "cucumber/rspec/doubles"
 
 When(/^I mock S3 object for "(.*)"$/) do |filename|
-  s3_object = double()
+  s3_object = double
   allow(s3_object).to receive(:url_for).and_return(Rails.root.join "spec", "files", filename)
   allow_any_instance_of(Paperclip::Attachment).to receive(:s3_object).and_return(s3_object)
 end
 
 When(/^I mock S3 object for all files$/) do
-  s3_object = double()
+  s3_object = double
   allow(s3_object).to receive(:url_for).and_return(Rails.root.join "spec", "files", "sample.pdf")
   allow_any_instance_of(Paperclip::Attachment).to receive(:s3_object).and_return(s3_object)
 end
