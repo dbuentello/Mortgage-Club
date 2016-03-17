d7673d14e21b60e828e35b074bc5931dc42392c8
Linh Nguyen <linhvn09@gmail.com>
2016-03-17 11:53:09 +0700

    restructure

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,8 +18,19 @@
 //= require d3.min
 //= require d3.slider
 //= require auto-complete.min
-//= require zopim
-//= require init
 
 // Important to import jquery_ujs before bundle_BorrowerApp as that patches jquery xhr to use the authenticity token!
 //= require build/bundle_BorrowerApp
+
+// zopim
+window.$zopim||(function(d,s){var z=$zopim=function(c){z._.push(c)},$=z.s=
+d.createElement(s),e=d.getElementsByTagName(s)[0];z.set=function(o){z.set.
+_.push(o)};z._=[];z.set._=[];$.async=!0;$.setAttribute("charset","utf-8");
+$.src="//v2.zopim.com/?3iekAjgdsMVBdIaGinAceGBxY9VKQNLd";z.t=+new Date;$.
+type="text/javascript";e.parentNode.insertBefore($,e)})(document,"script");
+
+// General Config
+$(document).on('ready', function(event) {
+  $('.flashSection').delay(7000).fadeOut();
+  $('[data-toggle="tooltip"]').tooltip();
+});
