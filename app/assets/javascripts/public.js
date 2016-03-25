f1d024dd07afed215a33f53e7a6204792c5c0bc5
jamesnguyenvnn <james.nguyen.vnn@gmail.com>
2016-03-25 10:05:35 +0700

    remove unuse code

--- app/assets/javascripts/public.js
+++ app/assets/javascripts/public.js
@@ -60,12 +60,6 @@ adroll_pix_id = "I7ZUQKIJDNFZTKERETJRFK";
  ga('create', 'UA-74698103-1', 'auto');
  ga('send', 'pageview');
 
-// // start Mixpanel -->
-//  (function(e,b){if(!b.__SV){var a,f,i,g;window.mixpanel=b;b._i=[];b.init=function(a,e,d){function f(b,h){var a=h.split(".");2==a.length&&(b=b[a[0]],h=a[1]);b[h]=function(){b.push([h].concat(Array.prototype.slice.call(arguments,0)))}}var c=b;"undefined"!==typeof d?c=b[d]=[]:d="mixpanel";c.people=c.people||[];c.toString=function(b){var a="mixpanel";"mixpanel"!==d&&(a+="."+d);b||(a+=" (stub)");return a};c.people.toString=function(){return c.toString(1)+".people (stub)"};i="disable time_event track track_pageview track_links track_forms register register_once alias unregister identify name_tag set_config people.set people.set_once people.increment people.append people.union people.track_charge people.clear_charges people.delete_user".split(" ");
-//  for(g=0;g<i.length;g++)f(c,i[g]);b._i.push([a,e,d])};b.__SV=1.2;a=e.createElement("script");a.type="text/javascript";a.async=!0;a.src="undefined"!==typeof MIXPANEL_CUSTOM_LIB_URL?MIXPANEL_CUSTOM_LIB_URL:"file:"===e.location.protocol&&"//cdn.mxpnl.com/libs/mixpanel-2-latest.min.js".match(/^\/\//)?"https://cdn.mxpnl.com/libs/mixpanel-2-latest.min.js":"//cdn.mxpnl.com/libs/mixpanel-2-latest.min.js";f=e.getElementsByTagName("script")[0];f.parentNode.insertBefore(a,f)}})(document,window.mixpanel||[]);
-//  mixpanel.init("cdf4bdb6a5368a9426af0bec06e6c956");
-//  // end Mixpanel -->
-
 // General Setting
 $(document).ready(function() {
 
@@ -89,12 +83,10 @@ $(document).ready(function() {
     element: document.querySelector('#backtopBtn'),
     duration:2000,
   });
-
  $("[data-toggle='tooltip']").tooltip({
     placement: "left",
     trigger: "manual"
   }).tooltip("show");
-
  $("#login-btn").click(function(){
    mixpanel.track("Navbar-Login");
  });
@@ -107,10 +99,4 @@ $(document).ready(function() {
  $("#apply-btn").click(function(){
    mixpanel.track("Homepage-FindMyRatesTable");
  });
- // $(".select-btn").click(function(){
- //   console.log("click select help me choose");
- //   mixpanel.track("clickSelectHelpMeChoose");
- // });
-
-
 });
