1efe22ec67126b15836998e9dd57ee02ff31b15d
Tang Nguyen <nvtang.khtn@gmail.com>
2016-03-28 11:22:38 +0700

    update discovery service

--- app/services/clearbit_services/discovery.rb
+++ app/services/clearbit_services/discovery.rb
@@ -1,5 +1,5 @@
 module ClearbitServices
-  class DiscoveryApi
+  class Discovery
     attr_accessor :company_name, :company_info, :response
 
     def initialize(company_name)
@@ -33,24 +33,43 @@ module ClearbitServices
     def read_company_info(response_data)
       return unless response_data["results"].present?
 
-      read_phone_info(response_data)
-      read_address_info(response_data)
+      read_address_info(response_data["results"])
     end
 
-    def read_phone_info(response_data)
-      @company_info[:contact_phone_number] = format_phone(response_data["phone"])
-      @company_info[:contact_name] = "HR Department"
-      return if response_data["phone"].present?
+    def read_address_info(response_data)
+      company_has_address_count_max = get_company_has_address_count_max(response_data)
 
-      @company_info[:contact_name] = nil
-      return unless response_data["results"].last["site"].present?
-      return unless response_data["results"].last["site"]["phoneNumbers"].present?
-
-      @company_info[:contact_name] = "HR Department"
-      @company_info[:contact_phone_number] = format_phone(response_data["results"].last["site"]["phoneNumbers"][0])
+      update_phone_info(company_has_address_count_max[1][:phone])
+      update_address_info(company_has_address_count_max[1][:address])
     end
 
-    def read_address_info(address)
+    def get_company_contact(response_data)
+      company_hash = {}
+
+      response_data.each do |company|
+        company_response = company_hash[company["location"]]
+
+        if company_response.present?
+          company_response[:count] += 1
+        else
+          company_hash[company["location"]] = {
+            count: 1,
+            address: company["geo"]
+          }
+        end
+      end
+
+      company_hash.max_by(&:count)[1]
+    end
+
+    def update_phone_info(phone)
+      return unless phone
+
+      @company_info[:contact_name] = "HR Department"
+      @company_info[:contact_phone_number] = format_phone(phone)
+    end
+
+    def update_address_info(address)
       @company_info[:street_address] = [address["streetNumber"], address["streetName"]].compact.join(" ")
       @company_info[:city] = address["city"]
       @company_info[:zip] = address["postalCode"]
@@ -66,7 +85,7 @@ module ClearbitServices
       phone = phone.gsub(/\D/, '')
       phone = phone[1..-1] if phone.size == 11 && phone[0] == 1.to_s
 
-      "(#{phone[0,3]}) #{phone[3,3]}-#{phone[6,4]}"
+      "(#{phone[0, 3]}) #{phone[3, 3]}-#{phone[6, 4]}"
     end
   end
 end
