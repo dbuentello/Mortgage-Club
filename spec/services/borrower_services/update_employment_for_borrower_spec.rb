08535b8f432e61d3df005a1ad625a6a12a515169
Tang Nguyen <nvtang.khtn@gmail.com>
2016-03-28 16:27:36 +0700

    update spec

--- spec/services/borrower_services/update_employment_for_borrower_spec.rb
+++ spec/services/borrower_services/update_employment_for_borrower_spec.rb
@@ -38,9 +38,6 @@ describe BorrowerServices::UpdateEmploymentForBorrower do
         expect(borrower.employments.count).to eq(0)
       end
 
-      context "with company info valid" do
-      end
-
       context "without company info invalid" do
         it "creates new current employment for borrower" do
           personal_info = {
@@ -118,6 +115,51 @@ describe BorrowerServices::UpdateEmploymentForBorrower do
           expect(borrower.previous_employment.job_title).to eq(personal_info[:prev_job_info][:title])
           expect(borrower.previous_employment.duration).to eq(personal_info[:prev_job_info][:years])
           expect(borrower.previous_employment.employer_name).to eq(personal_info[:prev_job_info][:company_name])
+        end
+      end
+
+      context "with company info valid" do
+        it "saves address for current employment" do
+          personal_info = {
+            current_job_info: {
+              title: "Developer",
+              years: 5,
+              company_name: "Microsoft"
+            },
+            prev_job_info: {
+              title: nil,
+              years: nil,
+              company_name: nil
+            }
+          }
+          company_info = {
+            contact_name: "HR Hunter",
+            contact_phone_number: "(123) 456-7890",
+            city: "New York",
+            state: "LA",
+            street_address: "123 Silver Street",
+            zip: "95143"
+          }
+
+          allow_any_instance_of(FullContactServices::GetPersonalInfo).to receive(:call).and_return(personal_info)
+          allow_any_instance_of(ClearbitServices::Discovery).to receive(:call).and_return(company_info)
+
+          service.call
+
+          expect(borrower.employments.count).to eq(1)
+
+          expect(borrower.current_employment).not_to be_nil
+          expect(borrower.current_employment.job_title).to eq(personal_info[:current_job_info][:title])
+          expect(borrower.current_employment.duration).to eq(personal_info[:current_job_info][:years])
+          expect(borrower.current_employment.employer_name).to eq(personal_info[:current_job_info][:company_name])
+          expect(borrower.current_employment.employer_contact_name).to eq(company_info[:contact_name])
+          expect(borrower.current_employment.employer_contact_number).to eq(company_info[:contact_phone_number])
+
+          expect(borrower.current_employment.address).not_to be_nil
+          expect(borrower.current_employment.address.city).to eq(company_info[:city])
+          expect(borrower.current_employment.address.state).to eq(company_info[:state])
+          expect(borrower.current_employment.address.street_address).to eq(company_info[:street_address])
+          expect(borrower.current_employment.address.zip).to eq(company_info[:zip])
         end
       end
     end
