54585ea46d8ccfafc5985475e37f4debab7591fd
Cuong Vu <cuongvu0103@gmail.com>
2016-04-04 16:10:56 +0700

    Create controller SlackWebhook to handle requests from Slack

---
+++ app/controllers/slack_webhooks_controller.rb
@@ -0,0 +1,22 @@
+class SlackWebhooksController < ApplicationController
+  skip_before_action :authenticate_user!
+  skip_before_action :verify_authenticity_token, only: [:receive]
+  before_action :validate_slack_bot, only: [:receive]
+  SLACK_BOT_HEADER_VALUE = "MCsLACK!".freeze
+
+  def receive
+    output = SlackBotServices::Base.new(params).call
+
+    render json: {
+      speech: output,
+      displayText: output,
+      source: "MortgageClub"
+    }
+  end
+
+  private
+
+  def validate_slack_bot
+    return head :unauthorized if request.headers["HTTP_MORTGAGECLUB_SLACK"] != SLACK_BOT_HEADER_VALUE
+  end
+end
