e116b36b5c918d2986f1a75fc02faf7b639618a9
Tang Nguyen <nvtang.khtn@gmail.com>
2016-04-04 16:01:01 +0700

    add rspec

--- spec/services/document_services/upload_file_spec.rb
+++ spec/services/document_services/upload_file_spec.rb
@@ -5,7 +5,7 @@ describe DocumentServices::UploadFile do
   let(:user) { FactoryGirl.create(:user) }
 
   context "with valid params" do
-    let(:service) do
+    let!(:service) do
       file = File.new(Rails.root.join "spec", "files", "sample.pdf")
       uploaded_file = ActionDispatch::Http::UploadedFile.new(
         tempfile: file,
@@ -40,6 +40,64 @@ describe DocumentServices::UploadFile do
       it "uploads a document successfully" do
         expect(PDF_MINE_TYPES).to include(@document.attachment_content_type)
         expect(@document.description).to eq("This is a closing disclosure")
+      end
+    end
+
+    context "when document type is other document" do
+      context "when subjectable is borrower" do
+        let!(:borrower) { FactoryGirl.create(:borrower) }
+
+        it "creates a new borrower document" do
+          service.args[:subject_type] = "Borrower"
+          service.args[:subject_id] = borrower.id
+          service.args[:document_type] = "other_borrower_report"
+
+          service.call
+
+          expect(borrower.documents.size).to eq(1)
+        end
+      end
+
+      context "when subjectable is loan" do
+        let(:loan) { FactoryGirl.create(:loan) }
+
+        it "creates a new loan document" do
+          service.args[:subject_type] = "Loan"
+          service.args[:subject_id] = loan.id
+          service.args[:document_type] = "other_loan_report"
+
+          service.call
+
+          expect(loan.documents.size).to eq(1)
+        end
+      end
+
+      context "when subjectable is closing" do
+        let(:closing_other_documents) { FactoryGirl.create(:closing) }
+
+        it "creates a new closing document" do
+          service.args[:subject_type] = "Closing"
+          service.args[:subject_id] = closing_other_documents.id
+          service.args[:document_type] = "other_closing_report"
+
+          service.call
+
+          expect(closing_other_documents.documents.size).to eq(1)
+        end
+      end
+
+      context "when subjectable is property" do
+        let(:property) { FactoryGirl.create(:property) }
+
+        it "creates a new property document" do
+          service.args[:subject_type] = "Property"
+          service.args[:subject_id] = property.id
+          service.args[:document_type] = "other_property_report"
+
+          service.call
+
+          expect(property.documents.size).to eq(1)
+        end
       end
     end
   end
