eb6c5dded9050fdba2fa060a3d1e00bd45d23e19
Linh Nguyen <linhvn09@gmail.com>
2016-04-04 16:11:08 +0700

    update email content

--- app/mailers/mortgage_bot_mailer.rb
+++ app/mailers/mortgage_bot_mailer.rb
@@ -1,17 +1,15 @@
 class MortgageBotMailer < ActionMailer::Base
   default from: ENV['EMAIL_SENDER']
 
-  def inform_sign_up_information(sign_up_info, user_email, user_name)
+  def inform_sign_up_information(sign_up_info)
     @sign_up_info = sign_up_info
-    @user_email = user_email
-    @user_name = user_name
     mail(
       to: ENV["MORTGAGE_BOT_INFO_RECEIVER"],
-      subject: "New Signup Information from Our Mortgage Bot!"
+      subject: "A new user wants to apply for a mortgage"
     )
   end
 
-  def inform_rate_information(rate_information, user_email, user_name)
+  def inform_rate_information(rate_info)
     @rate_information = rate_information
     @user_email = user_email
     @user_name = user_name
