1cb7d6750e04a7cc6bcfce85eb70505c3620ac31
Linh Nguyen <linhvn09@gmail.com>
2016-04-05 09:37:33 +0700

    write test case for failed deleting

--- spec/controllers/admin/borrower_managements_controller_spec.rb
+++ spec/controllers/admin/borrower_managements_controller_spec.rb
@@ -4,12 +4,30 @@ describe Admins::BorrowerManagementsController do
   include_context "signed in as admin"
 
   describe "#destroy" do
-    context "when valid params" do
+    context "when params are valid" do
       let!(:borrower) { FactoryGirl.create(:borrower, :with_user) }
 
       it "returns status 200" do
         delete :destroy, id: borrower.id
         expect(response.status).to eq(200)
+      end
+
+      it "returns array of borrower" do
+        @expected = {
+          :borrowers  => []
+        }.to_json
+        delete :destroy, id: borrower.id
+        expect(response.body).to eq(@expected)
+      end
+    end
+
+    context "when params are invalid" do
+      let!(:borrower) { FactoryGirl.create(:borrower, :with_user) }
+
+      it "returns status 500" do
+        allow_any_instance_of(Borrower).to receive(:destroy_completely).and_return(false)
+        delete :destroy, id: borrower.id
+        expect(response.status).to eq(500)
       end
     end
   end
