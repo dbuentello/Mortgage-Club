620ab46718de74cf9d075d2cfbbbfc8aa8c61c66
Linh Nguyen <linhvn09@gmail.com>
2016-04-05 14:21:21 +0700

    remove wrong code

--- app/models/role.rb
+++ app/models/role.rb
@@ -11,7 +11,7 @@
 #
 
 class Role < ActiveRecord::Base
-  has_many :users, through: :users_roles, dependent: :destroy
+  has_many :users, through: :users_roles
 
   belongs_to :resource, polymorphic: true
 
