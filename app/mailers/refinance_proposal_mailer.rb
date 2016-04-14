0e7d172f4197c8bac281f749cf6808b2deb6e94b
Linh Nguyen <linhvn09@gmail.com>
2016-04-14 11:56:06 +0700

    [WIP] - mailer for refinace proposal

--- app/mailers/refinance_proposal_mailer.rb
+++ app/mailers/refinance_proposal_mailer.rb
@@ -3,13 +3,13 @@ class RefinanceProposalMailer < ActionMailer::Base
   def notify_refinance_benefit(benefit_info, user_name, user_email)
     @user_name = user_name
     @benefit_info = benefit_info
-    savings_in_1_year = @benefit_info["savings_in_1_year"]
-    new_interest_rate = @benefit_info["new_interest_rate"]
-    original_term = @benefit_info["original_term"]
+    savings_in_1_year = @benefit_info[:savings_in_1_year]
+    new_interest_rate = @benefit_info[:new_interest_rate]
+    original_term = @benefit_info[:original_term]
 
     mail(
       to: user_email,
-      subject: "Refinance your Wells Fargo mortgage and save $#{ savings_in_1_year }/year (new rate #{ new_interest_rate }%, #{ original_term }, no closing costs)"
+      subject: "Refinance your Wells Fargo mortgage and save $#{savings_in_1_year}/year (new rate #{new_interest_rate}%, #{original_term}, no closing costs)"
     )
   end
 end
