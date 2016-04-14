8e97776b9cfe7e189e9a48aeeab94ebd2ea501dd
Linh Nguyen <linhvn09@gmail.com>
2016-04-14 18:22:09 +0700

    restyle the refinance proposal email

--- app/mailers/refinance_proposal_mailer.rb
+++ app/mailers/refinance_proposal_mailer.rb
@@ -3,8 +3,8 @@ class RefinanceProposalMailer < ActionMailer::Base
   def notify_refinance_benefit(benefit_info, user_name, user_email)
     @user_name = user_name
     @benefit_info = benefit_info
-    savings_in_1_year = @benefit_info[:savings_in_1_year]
-    new_interest_rate = @benefit_info[:new_interest_rate]
+    savings_in_1_year = @benefit_info[:lower_rate_refinance][:savings_in_1_year]
+    new_interest_rate = @benefit_info[:lower_rate_refinance][:new_interest_rate]
     original_term = @benefit_info[:original_term]
 
     mail(
