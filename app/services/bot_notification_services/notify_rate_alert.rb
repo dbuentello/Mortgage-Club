adfce87caf1874776bc380f0fc313aa268954976
Cuong Vu <cuongvu0103@gmail.com>
2016-04-21 17:59:38 +0700

    Refactor code

--- app/services/bot_notification_services/notify_rate_alert.rb
+++ app/services/bot_notification_services/notify_rate_alert.rb
@@ -4,13 +4,10 @@ module BotNotificationServices
     extend ParseData
 
     def self.call(params)
-      output = {}
       if data = parsed_data(params)
         MortgageBotMailer.inform_rate_information(data, params[:source]).deliver_later
       end
-      output[:data] = "Awesome, we'll notify you when rates drop. Goodbye!"
-      output[:status_code] = 200
-      output.to_json
+      "Awesome, we'll notify you when rates drop. Goodbye!"
     end
   end
 end
--- app/services/bot_notification_services/notify_sign_up.rb
+++ app/services/bot_notification_services/notify_sign_up.rb
@@ -4,13 +4,10 @@ module BotNotificationServices
     extend ParseData
 
     def self.call(params)
-      output = {}
       if data = parsed_data(params)
         MortgageBotMailer.inform_sign_up_information(data, params[:source]).deliver_later
       end
-      output[:data] = "Awesome, our team will reach out to you shortly with the next steps."
-      output[:status_code] = 200
-      output.to_json
+      "Awesome, our team will reach out to you shortly with the next steps."
     end
   end
 end
