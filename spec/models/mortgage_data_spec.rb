0f1fa438c3bb2296ef7f3a7eb8db9d9c7a21d91d
Cuong Vu <cuongvu0103@gmail.com>
2016-04-26 15:55:24 +0700

    Update spec

--- spec/models/mortgage_data_spec.rb
+++ spec/models/mortgage_data_spec.rb
@@ -2,7 +2,7 @@ require "rails_helper"
 
 describe MortgageData do
   describe ".search" do
-    context "Keyword matchs records" do
+    context "when keyword matches records" do
       let!(:mortgage_data) { FactoryGirl.create(:mortgage_data, property_address: "2309 Wilshire Boulevard, Santa Monica, CA") }
       let!(:mortgage_data_with_owner_name) { FactoryGirl.create(:mortgage_data, owner_name_1: "Taylor Swiff") }
       let!(:mortgage_data_with_another_owner_name) { FactoryGirl.create(:mortgage_data, owner_name_2: "Chris Hemsworth") }
@@ -15,7 +15,7 @@ describe MortgageData do
       end
     end
 
-    context "Keyword does not match any record" do
+    context "when keyword does not match any record" do
       let!(:mortgage_data) { FactoryGirl.create(:mortgage_data, property_address: "2309 Wilshire Boulevard, Santa Monica, CA", owner_name_1: "Jennifer Lawrence", owner_name_2: "Emma Lawrence") }
       let!(:mortgage_data_with_owner_name) { FactoryGirl.create(:mortgage_data, property_address: "N. La Jolla Ave. Hollywood", owner_name_1: "Robert Downey Jr.", owner_name_2: "Liam Hemsworth") }
       let!(:mortgage_data_with_another_owner_name) { FactoryGirl.create(:mortgage_data, property_address: "Ozeta Terrace Hollywood", owner_name_1: "Lucia Ronnie", owner_name_2: "Chris Hemsworth") }
