fa72a322852a0ae0a9e8ecf5b155730c053f2070
Linh Nguyen <linhvn09@gmail.com>
2016-04-27 17:13:36 +0700

    update test case to check save data and update data

--- spec/services/fred_economic_services/crawl_avg_rates_spec.rb
+++ spec/services/fred_economic_services/crawl_avg_rates_spec.rb
@@ -2,19 +2,31 @@ require "rails_helper"
 
 describe FredEconomicServices::CrawlAvgRates do
   describe ".call" do
-    it "crawls data from Fred Economic Data 3 times to create new data" do
+    it "crawls data from Fred Economic Data 3 times" do
       VCR.use_cassette("get Fred Economic rate data") do
         expect(described_class).to receive(:crawl_data).exactly(3).times
         described_class.call
       end
     end
+    it "saves data to Fred Economic" do
+      VCR.use_cassette("get Fred Economic rate data") do
+        expect { described_class.call }.to change(FredEconomic, :count).by(2353)
+      end
+    end
   end
 
   describe ".update" do
-    it "also crawls from Fred Economic Data 3 times to create or update data" do
+
+    it "crawls from Fred Economic Data 3 times" do
       VCR.use_cassette("get or update Fred Economic rate data") do
         expect(described_class).to receive(:crawl_data).exactly(3).times
+        described_class.update
+      end
+    end
+    it "finds or creates Fred Economic data" do
+      VCR.use_cassette("get or update Fred Economic rate data") do
         described_class.call
+        expect { described_class.update }.to change(FredEconomic, :count).by(0)
       end
     end
   end
