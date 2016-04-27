78484529cf4af20cdfe401a5810c36eebfaea6fb
Linh Nguyen <linhvn09@gmail.com>
2016-04-27 14:46:01 +0700

    add test case for query content

--- spec/services/loan_tek_services/get_quotes_for_facebook_bot_spec.rb
+++ spec/services/loan_tek_services/get_quotes_for_facebook_bot_spec.rb
@@ -33,4 +33,37 @@ describe LoanTekServices::GetQuotesForFacebookBot do
       end
     end
   end
+
+  describe "#query_content" do
+    let(:params) do
+      {
+        "parameters" => {
+          "zipcode" => "90212",
+          "credit_score" => "730",
+          "purpose" => "purchase",
+          "property_value" => "560000",
+          "usage" => "vacation_home",
+          "property_type" => "sfh",
+          "down_payment" => "100000",
+          "mortgage_balance" => "300000"
+        }
+      }
+    end
+    let(:query_content) do
+      {
+        zip_code: 90212,
+        credit_score: 730,
+        mortgage_purpose: "purchase",
+        property_value: 560000,
+        property_usage: "vacation_home",
+        property_type: "sfh",
+        down_payment: 100000,
+        mortgage_balance: 300000
+      }
+    end
+
+    it "returns query info users input" do
+      expect(described_class.new(params).query_content).to eq(query_content.to_json)
+    end
+  end
 end
