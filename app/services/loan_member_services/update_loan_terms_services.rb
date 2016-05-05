d18c1d96d5c3cd340a4ca4235a11e9e1816470be
Linh Nguyen <linhvn09@gmail.com>
2016-05-05 17:18:37 +0700

    update test case for update loan service

--- app/services/loan_member_services/update_loan_terms_services.rb
+++ app/services/loan_member_services/update_loan_terms_services.rb
@@ -12,17 +12,20 @@ module LoanMemberServices
 
     def update_loan
       loan = Loan.find(loan_id)
-      return loan.update(@loan_terms_params) if update_property
-      false
+      create_or_update_property
+      loan.update(@loan_terms_params)
+      logger.error(loan.errors.full_messages) unless loan.update(@loan_terms_params)
     end
 
-    def update_property
-      property = Property.find(@property_id)
+    def create_or_update_property
+      property = Property.find(@property_id) if @property_id
+      property = Property.create(@property_params.merge(is_subject: true)) unless property
       return property.update(@property_params) if create_or_update_address(property)
-      false
+      true
     end
 
     def create_or_update_address(property)
+      return true unless @address_params
       if @address_params[:id].present?
         address = Address.find(@address_id)
         return address.update(@address_params.except(:id))
