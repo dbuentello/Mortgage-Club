036cb850218a8f4d7dfc2e175e5cf8c43d3e9ea5
Linh Nguyen <linhvn09@gmail.com>
2016-05-05 15:29:13 +0700

    clean up code

--- app/controllers/loan_members/loans_controller.rb
+++ app/controllers/loan_members/loans_controller.rb
@@ -34,28 +34,6 @@ class LoanMembers::LoansController < LoanMembers::BaseController
     end
   end
 
-  def update_loan_terms_old
-
-    loan = Loan.find(loan_id)
-    if update_property
-      if loan.update(loan_terms_params)
-        loan = loan.reload
-        property = loan.subject_property
-        respond_to do |format|
-          format.json { render json: {message: t("loan_members.loans.update.success"), loan: loan, property: property, address: property.address} }
-        end
-      else
-        respond_to do |format|
-          format.json { render json: {message: t("loan_members.loans.update.failed")}, status: 500 }
-        end
-      end
-    else
-      respond_to do |format|
-        format.json { render json: {message: t("loan_members.loans.update.failed")}, status: 500 }
-      end
-    end
-  end
-
   private
 
   def property_id
@@ -65,33 +43,17 @@ class LoanMembers::LoansController < LoanMembers::BaseController
   def address_id
     params[:address][:id]
   end
+
   def loan_terms_params
     params.require(:loan).permit(Loan.get_editable_attributes)
   end
 
   def property_params
-    params.require(:property).permit(Property::PERMITTED_ATTRS).reject{ |_, value| value.blank? }
+    params.require(:property).permit(Property::PERMITTED_ATTRS).reject { |_, value| value.blank? }
   end
 
   def address_params
-    params.require(:address).permit(Address::PERMITTED_ATTRS).reject{ |_, value| value.blank? }
-  end
-
-  def update_property
-    property = Property.find(property_params[:id])
-    create_or_update_address(property)
-    property.update(property_params)
-  end
-
-  def create_or_update_address(property)
-    address_params = params.require(:address).permit(Address::PERMITTED_ATTRS).reject { |_, value| value.blank? }
-    if address_params[:id].present?
-      address = Address.find(address_params[:id])
-      address.update(address_params.except(:id))
-    else
-      property.build_address(address_params.except(:id))
-      property.address.save
-    end
+    params.require(:address).permit(Address::PERMITTED_ATTRS).reject { |_, value| value.blank? }
   end
 
   def loan_params
