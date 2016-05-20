290f0adb470957cb82d07e63c1d4290e5510cb0a
Cuong Vu <cuongvu0103@gmail.com>
2016-05-20 16:20:25 +0700

    Initialize service to export XML Mismo [ci skip]

---
+++ app/services/export_xml_mismo_service.rb
@@ -0,0 +1,15 @@
+class ExportXmlMismoService
+  def initialize(loan, borrower)
+  end
+
+  def call
+    build_data_information
+    build_additional_case_data
+  end
+
+  def build_data_information
+  end
+
+  def build_additional_case_data
+  end
+end
