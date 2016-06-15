a836c7f466657c6c55d40f307252ab106f8a5635
jamesnguyenvnn <james.nguyen.vnn@gmail.com>
2016-06-15 15:56:46 +0700

    add new layout

---
+++ app/assets/javascripts/landing.js
@@ -0,0 +1,15 @@
+//= require jquery
+//= require jquery_ujs
+//= require bootstrap-sprockets
+//= require landing/plugins
+//= require landing/functions
+//= require landing/jquery.themepunch.tools.min
+//= require landing/jquery.themepunch.revolution.min
+//= require landing/revolution.extension.video.min
+//= require landing/revolution.extension.slideanims.min
+//= require landing/revolution.extension.actions.min
+//= require landing/revolution.extension.layeranimation.min
+//= require landing/revolution.extension.kenburn.min
+//= require landing/revolution.extension.navigation.min
+//= require landing/revolution.extension.migration.min
+//= require landing/revolution.extension.parallax.min
