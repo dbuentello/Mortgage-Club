a931912f1d85f7aadb378816530dd8cc2e4e99b3
jamesnguyenvnn <james.nguyen.vnn@gmail.com>
2016-06-30 23:47:27 +0700

    add config for capybara

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -34,9 +34,16 @@ RSpec.configure do |config|
 end
 
 Capybara.javascript_driver = :poltergeist
-
+Capybara.default_driver = :poltergeist
 Capybara.register_driver :poltergeist do |app|
-  Capybara::Poltergeist::Driver.new(app, {js_errors: false, timeout: 120})
+  options = {
+        :js_errors => false,
+        :timeout => 120,
+        :debug => false,
+        :phantomjs_options => ['--load-images=no', '--disk-cache=false'],
+        :inspector => true,
+    }
+  Capybara::Poltergeist::Driver.new(app, options)
 end
 
 RSpec.configure do |config|
