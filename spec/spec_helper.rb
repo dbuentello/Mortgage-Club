d96ec7fad1140effebc6078a0c7495c30a655ee6
jamesnguyenvnn <james.nguyen.vnn@gmail.com>
2016-07-01 15:59:06 +0700

    add debug

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -24,7 +24,7 @@ require 'capybara/poltergeist'
 require 'rspec/retry'
 
 Capybara.javascript_driver = :poltergeist
-options = {js_errors: false, timeout: 180, phantomjs_logger: StringIO.new, logger: nil, phantomjs_options: ['--load-images=no', '--ignore-ssl-errors=yes']}
+options = {js_errors: false, debug: true, timeout: 180, phantomjs_logger: StringIO.new, logger: nil, phantomjs_options: ['--load-images=no', '--ignore-ssl-errors=yes']}
 Capybara.register_driver(:poltergeist) do |app|
   Capybara::Poltergeist::Driver.new app, options
 end
