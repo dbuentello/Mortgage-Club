e01c61c66c74e8aeb0de8980bea3ea6332d15638
jamesnguyenvnn <james.nguyen.vnn@gmail.com>
2016-08-04 16:38:09 +0700

    rspec for lender docusign form

--- spec/factories/lender_docusign_forms.rb
+++ spec/factories/lender_docusign_forms.rb
@@ -1,7 +1,10 @@
 FactoryGirl.define do
-  factory :lender_docusign_form do
-    description "MyString"
-    sign_position "MyString"
-    lender nil
+  factory :lender_docusign_form do |f|
+    f.description "MyString"
+    f.sign_position "MyString"
+    lender
+    f.attachment { File.new(Rails.root.join 'spec', 'files', 'sample.png') }
+    f.doc_order 4
+    f.spouse_signed true
   end
 end
