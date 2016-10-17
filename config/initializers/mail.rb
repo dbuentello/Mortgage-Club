8eeccd01a8b5cac2442c73cccb6172fca8fb1346
Tang Nguyen <nvtang.khtn@gmail.com>
2016-10-17 14:00:33 +0700

    config sendgrid api

--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -1,11 +1,24 @@
+# unless Rails.env.test?
+#   ActionMailer::Base.smtp_settings = {
+#     address: 'in-v3.mailjet.com',
+#     port: 587,
+#     enable_starttls_auto: true,
+#     user_name: ENV["MAILJET_USERNAME"],
+#     password: ENV["MAILJET_PASSWORD"],
+#     authentication: 'plain'
+#   }
+
+#   ActionMailer::Base.delivery_method = :smtp
+#   ActionMailer::Base.default charset: "utf-8"
+# end
 unless Rails.env.test?
   ActionMailer::Base.smtp_settings = {
-    address: 'in-v3.mailjet.com',
+    user_name: 'nhokjoy',
+    password: 'tang14121992',
+    address: 'smtp.sendgrid.net',
     port: 587,
-    enable_starttls_auto: true,
-    user_name: ENV["MAILJET_USERNAME"],
-    password: ENV["MAILJET_PASSWORD"],
-    authentication: 'plain'
+    authentication: :plain,
+    enable_starttls_auto: true
   }
 
   ActionMailer::Base.delivery_method = :smtp
