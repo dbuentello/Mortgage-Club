e64705f104c4d1518ec7ba821be48ee932d8f692
Tang Nguyen <nvtang.khtn@gmail.com>
2016-11-01 13:01:22 +0700

    fix rubocop

--- app/mailers/share_rate_mailer.rb
+++ app/mailers/share_rate_mailer.rb
@@ -1,6 +1,20 @@
 class ShareRateMailer < ActionMailer::Base
   def email_me(params, current_user)
-    unless params[:body]
+    if params[:body]
+      if params[:body].include? "[first_name]"
+        body = params[:body].gsub! "[first_name]", params[:first_name]
+      else
+        body = params[:body]
+      end
+
+      mail(
+        from: "#{current_user} <#{current_user.email}>",
+        to: params[:email],
+        subject: params[:subject],
+        body: body,
+        content_type: "text/html"
+      )
+    else
       @first_name = params[:first_name]
       @rate = params[:rate]
       @code = params[:code_id]
@@ -10,7 +24,7 @@ class ShareRateMailer < ActionMailer::Base
       @current_user = current_user
 
       if current_user && current_user.has_role?(:loan_member)
-        @email_from = current_user.email.present? ? "#{current_user.to_s} <#{current_user.email}>" : "Billy Tran <billy@mortgageclub.co>"
+        @email_from = current_user.email.present? ? "#{current_user} <#{current_user.email}>" : "Billy Tran <billy@mortgageclub.co>"
         @email = current_user.email.present? ? current_user.email : "billy@mortgageclub.co"
         @phone = current_user.loan_member.phone_number.present? ? current_user.loan_member.phone_number : "(650) 787-7799"
       else
@@ -23,20 +37,6 @@ class ShareRateMailer < ActionMailer::Base
         from: @email_from,
         to: params[:email],
         subject: "Your rate quote from MortgageClub"
-      )
-    else
-      if params[:body].include? "[first_name]"
-        body = params[:body].gsub! "[first_name]", params[:first_name]
-      else
-        body = params[:body]
-      end
-
-      mail(
-        from: "#{current_user.to_s} <#{current_user.email}>",
-        to: params[:email],
-        subject: params[:subject],
-        body: body,
-        content_type: "text/html"
       )
     end
   end
